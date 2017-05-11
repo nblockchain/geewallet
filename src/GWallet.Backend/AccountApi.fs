4cfa08dfa4038e342acb2f66bb7f8fa454876c9b
Andres G. Aragoneses <knocte@gmail.com>
2017-05-11 20:04:34 +0800

    Print balance of the ETH account

--- src/GWallet.Backend/AccountApi.fs
+++ src/GWallet.Backend/AccountApi.fs
@@ -1,6 +1,8 @@
 ﻿namespace GWallet.Backend
 
 open System
+
+open Nethereum.Web3
 open Nethereum.Core.Signing.Crypto
 
 module AccountApi =
@@ -8,6 +10,13 @@ module AccountApi =
     // TODO: propose this func to NEthereum's EthECKey class as method name GetPrivateKeyAsHexString()
     let ToHexString(byteArray: byte array) =
         BitConverter.ToString(byteArray).Replace("-", String.Empty)
+
+    let GetBalance(account: Account) =
+        // https://www.myetherapi.com/
+        let web3 = Web3("https://api.myetherapi.com/eth")
+        let balanceTask = web3.Eth.GetBalance.SendRequestAsync(account.PublicAddress)
+        balanceTask.Wait()
+        UnitConversion.Convert.FromWei(balanceTask.Result.Value, UnitConversion.EthUnit.Ether)
 
     let CreateOrGetMainAccount(currency): Account =
         let maybeAccount = Config.GetMainAccount(currency)
--- src/GWallet.Frontend.Console/Program.fs
+++ src/GWallet.Frontend.Console/Program.fs
@@ -8,7 +8,8 @@ let main argv =
     let account = AccountApi.CreateOrGetMainAccount(ethCurrency)
 
     Console.WriteLine("** STATUS **")
-    let ethStatus = sprintf "%s: Address=[%s]" (ethCurrency.ToString()) account.PublicAddress
+    let ethStatus = sprintf "%s: Address=[%s] Balance=[%s]"
+                        (ethCurrency.ToString()) account.PublicAddress (AccountApi.GetBalance(account).ToString())
     Console.WriteLine(ethStatus)
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
