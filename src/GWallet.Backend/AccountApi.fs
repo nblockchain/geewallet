94511ab9f5343f51a0ed630d8342a53ea42c07f1
Andres G. Aragoneses <knocte@gmail.com>
2017-05-15 23:19:33 +0800

    Support for sending ether transactions

--- src/GWallet.Backend/AccountApi.fs
+++ src/GWallet.Backend/AccountApi.fs
@@ -2,10 +2,15 @@
 
 open System
 open System.Linq
+open System.Numerics
 
 open Nethereum.Web3
 open Nethereum.Core.Signing.Crypto
 open Nethereum.KeyStore
+open NBitcoin.Crypto
+
+
+exception InsufficientFunds
 
 module AccountApi =
 
@@ -14,18 +19,22 @@ module AccountApi =
 
     let private PUBLIC_WEB3_API_ETC = "https://mewapi.epool.io"
 
+    let private ethWeb3 = Web3(PUBLIC_WEB3_API_ETH)
+    let private etcWeb3 = Web3(PUBLIC_WEB3_API_ETC)
+
+    let private keyStoreService = KeyStoreService()
+
     // TODO: propose this func to NEthereum's EthECKey class as method name GetPrivateKeyAsHexString()
     let ToHexString(byteArray: byte array) =
         BitConverter.ToString(byteArray).Replace("-", String.Empty)
 
     let GetBalance(account: Account) =
-        let web3ApiUrl =
+        let web3 =
             match account.Currency with
-            | Currency.ETH -> PUBLIC_WEB3_API_ETH
-            | Currency.ETC -> PUBLIC_WEB3_API_ETC
+            | Currency.ETH -> ethWeb3
+            | Currency.ETC -> etcWeb3
             | _ -> failwith("currency unknown")
 
-        let web3 = Web3(web3ApiUrl)
         let balanceTask = web3.Eth.GetBalance.SendRequestAsync(account.PublicAddress)
         balanceTask.Wait()
         UnitConversion.Convert.FromWei(balanceTask.Result.Value, UnitConversion.EthUnit.Ether)
@@ -39,6 +48,46 @@ module AccountApi =
                     yield account
         }
 
+    let private GAS_COST_FOR_A_NORMAL_ETHER_TRANSACTION = 21000
+
+    let SendPayment (account: Account) (destination: string) (amount: decimal) (password: string) =
+        let web3 =
+            match account.Currency with
+            | Currency.ETH -> ethWeb3
+            | Currency.ETC -> etcWeb3
+            | _ -> failwith("currency unknown")
+        let gasPriceTask = web3.Eth.GasPrice.SendRequestAsync()
+
+        let transCountTask = web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(account.PublicAddress)
+
+        let privKeyInBytes = keyStoreService.DecryptKeyStoreFromJson(password, account.Json)
+        let privKeyInHexString = ToHexString(privKeyInBytes)
+        let amountInWei = UnitConversion.Convert.ToWei(amount, UnitConversion.EthUnit.Ether)
+
+        transCountTask.Wait()
+        let transCount = transCountTask.Result
+
+        gasPriceTask.Wait()
+        let gasPrice = gasPriceTask.Result
+        let trans = web3.OfflineTransactionSigning.SignTransaction(privKeyInHexString,
+                                                                   destination,
+                                                                   amountInWei,
+                                                                   transCount.Value,
+                                                                   gasPrice.Value,
+                                                                   BigInteger(GAS_COST_FOR_A_NORMAL_ETHER_TRANSACTION))
+        if not (web3.OfflineTransactionSigning.VerifyTransaction(trans)) then
+            failwith "Transaction could not be verified?"
+
+        let insufficientFundsMsg = "Insufficient funds"
+        try
+            let sendRawTransTask = web3.Eth.Transactions.SendRawTransaction.SendRequestAsync("0x" + trans)
+            sendRawTransTask.Wait()
+            let txId = sendRawTransTask.Result
+            txId
+        with
+        | ex when ex.Message.StartsWith(insufficientFundsMsg) || ex.InnerException.Message.StartsWith(insufficientFundsMsg) ->
+            raise (InsufficientFunds)
+
     let Create currency password =
         let privateKey = EthECKey.GenerateKey()
         let privateKeyAsBytes = EthECKey.GetPrivateKeyAsBytes(privateKey)
@@ -50,7 +99,6 @@ module AccountApi =
             else
                 privateKeyAsBytes
 
-        let keyStoreService = KeyStoreService()
         let publicAddress = EthECKey.GetPublicAddress(privateKey)
 
         let accountSerializedJson = keyStoreService.EncryptAndGenerateDefaultKeyStoreAsJson(password, privateKeyTrimmed, publicAddress)
--- src/GWallet.Frontend.Console/Program.fs
+++ src/GWallet.Frontend.Console/Program.fs
@@ -8,6 +8,7 @@ type Options =
     | Exit          = 0
     | Refresh       = 1
     | CreateAccount = 2
+    | SendPayment   = 3
 
 let ConvertPascalCaseToSentence(pascalCaseElement: string) =
     Regex.Replace(pascalCaseElement, "[a-z][A-Z]",
@@ -27,22 +28,28 @@ let rec FindMatchingOption<'T> (optIntroduced, allOptions: ('T*int) list): 'T =
             else
                 FindMatchingOption(optIntroduced, tail)
 
-let rec AskOption(): Options =
+let rec AskOption(numAccounts: int): Options =
     Console.WriteLine()
     Console.WriteLine("Available options:")
 
     // TODO: move these 2 lines below to FSharpUtil?
     let allOptions = Enum.GetValues(typeof<Options>).Cast<Options>() |> List.ofSeq
-    let allOptionsMappedToTheirIntValues = List.map (fun x -> (x, int x)) allOptions
 
-    for option in allOptions do
-        Console.WriteLine(sprintf "%d: %s" (int option) (ConvertPascalCaseToSentence (option.ToString())))
+    let allOptionsAvailable =
+        seq {
+            for option in allOptions do
+                if not (option = Options.SendPayment && numAccounts = 0) then
+                    Console.WriteLine(sprintf "%d: %s"
+                                          (int option)
+                                          (ConvertPascalCaseToSentence (option.ToString())))
+                    yield option, int option
+        } |> List.ofSeq
     Console.Write("Choose option to perform: ")
     let optIntroduced = System.Console.ReadLine()
     try
-        FindMatchingOption(optIntroduced, allOptionsMappedToTheirIntValues)
+        FindMatchingOption(optIntroduced, allOptionsAvailable)
     with
-    | :? NoOptionFound -> AskOption()
+    | :? NoOptionFound -> AskOption(numAccounts)
 
 // taken from InfraLib
 let ConsoleReadPasswordLine() =
@@ -67,18 +74,21 @@ let ConsoleReadPasswordLine() =
     ConsoleReadPasswordLineInternal(String.Empty)
 
 
-let rec AskPassword(): string =
+let rec AskPassword(repeat: bool): string =
     Console.WriteLine()
 
-    Console.Write("Write a password to unlock your account: ")
+    Console.Write("Write the password to unlock your account: ")
     let password = ConsoleReadPasswordLine()
-    Console.Write("Repeat the password: ")
-    let password2 = ConsoleReadPasswordLine()
-    if (password <> password2) then
-        Console.Error.WriteLine("Passwords are not the same, please try again.")
-        AskPassword()
-    else
+    if not repeat then
         password
+    else
+        Console.Write("Repeat the password: ")
+        let password2 = ConsoleReadPasswordLine()
+        if (password <> password2) then
+            Console.Error.WriteLine("Passwords are not the same, please try again.")
+            AskPassword(repeat)
+        else
+            password
 
 let rec AskCurrency(): Currency =
     Console.WriteLine()
@@ -100,29 +110,85 @@ let DisplayStatus() =
     Console.WriteLine("** STATUS **")
     let accounts = AccountApi.GetAllAccounts()
     if (accounts.Any()) then
-        for account in accounts do
-            let status = sprintf "%s: Address=[%s] Balance=[%s]"
-                            (account.Currency.ToString())
-                            account.PublicAddress
-                            (AccountApi.GetBalance(account).ToString())
+        for i = 0 to accounts.Count() - 1 do
+            let account = accounts.ElementAt(i)
+            let accountInfo = sprintf "Account %d:%sCurrency=[%s] Address=[%s]"
+                                  (i+1) Environment.NewLine
+                                  (account.Currency.ToString())
+                                  account.PublicAddress
+            Console.WriteLine(accountInfo)
+            let status = sprintf "Balance=[%s]" (AccountApi.GetBalance(account).ToString())
             Console.WriteLine(status)
+
+            Console.WriteLine()
     else
         Console.WriteLine("No accounts have been created so far.")
+    accounts.Count()
 
-let rec PerformOptions() =
-    match AskOption() with
+let rec AskAccount(): Account =
+    let allAccounts = AccountApi.GetAllAccounts()
+    Console.Write("Write the account number: ")
+    let accountNumber = Console.ReadLine()
+    match Int32.TryParse(accountNumber) with
+    | false, _ -> AskAccount()
+    | true, accountParsed ->
+        let theAccountChosen =
+            try
+                allAccounts.ElementAt(accountParsed - 1)
+            with
+            | _ -> AskAccount()
+        theAccountChosen
+
+let ETHEREUM_ADDRESSES_LENGTH = 42
+let rec AskDestination() =
+    Console.Write("Destination address: ")
+    let destAddress = Console.ReadLine()
+    if not (destAddress.StartsWith("0x")) then
+        Console.Error.WriteLine("Error: destination address should start with '0x', please try again.")
+        AskDestination()
+    else if (destAddress.Length <> ETHEREUM_ADDRESSES_LENGTH) then
+        Console.Error.WriteLine(
+            sprintf "Error: destination address should have a length of %d characters, please try again."
+                ETHEREUM_ADDRESSES_LENGTH)
+        AskDestination()
+    else
+        destAddress
+
+let rec AskAmount() =
+    Console.Write("Amount of ether: ")
+    let amount = Console.ReadLine()
+    match Decimal.TryParse(amount) with
+    | (false, _) ->
+        Console.Error.WriteLine("Error: please enter a numeric amount")
+        AskAmount()
+    | (true, parsedAdmount) ->
+        parsedAdmount
+
+let rec PerformOptions(numAccounts: int) =
+    match AskOption(numAccounts) with
     | Options.Exit -> exit 0
     | Options.CreateAccount ->
         let currency = AskCurrency()
-        let password = AskPassword()
+        let password = AskPassword true
         let account = AccountApi.Create currency password
         Console.WriteLine("Account created: " + account.PublicAddress)
     | Options.Refresh -> ()
+    | Options.SendPayment ->
+        let account = AskAccount()
+        let destination = AskDestination()
+        let amount = AskAmount()
+        let password = AskPassword false
+        try
+            let txId = AccountApi.SendPayment account destination amount password
+            Console.WriteLine(sprintf "Transaction successful, its ID is:%s%s" Environment.NewLine txId)
+        with
+        | :? InsufficientFunds ->
+            Console.Error.WriteLine("Insufficient funds")
     | _ -> failwith "Unreachable"
 
 let rec ProgramMainLoop() =
-    DisplayStatus()
-    PerformOptions()
+    let numAccounts = DisplayStatus()
+    PerformOptions(numAccounts)
     ProgramMainLoop()
 
 [<EntryPoint>]
