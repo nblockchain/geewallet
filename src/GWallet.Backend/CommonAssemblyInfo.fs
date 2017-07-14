6ee345119a71b92edab88ec4cbbc04a8e9ea35a9
Andres G. Aragoneses <knocte@gmail.com>
2017-07-14 22:40:12 +0800

    Share AssemblyVersion between all projs and change it to 0.1 (not 1.0)

---
+++ src/GWallet.Backend/CommonAssemblyInfo.fs
@@ -0,0 +1,24 @@
+﻿
+namespace GWallet
+
+open System.Reflection
+
+[<assembly: AssemblyCompany("NBlockchain")>]
+[<assembly: AssemblyProduct("GWallet")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do
+    ()
--- src/GWallet.Frontend.Console/AssemblyInfo.fs
+++ src/GWallet.Frontend.Console/AssemblyInfo.fs
@@ -10,9 +10,6 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitle("GWallet.Frontend.Console")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("GWallet.Frontend.Console")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -23,19 +20,6 @@ open System.Runtime.InteropServices
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("8413eef5-69f5-499f-ae01-754e9541ef90")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
 
 do
     ()
