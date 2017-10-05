86a26dd4b78b816621a7a93347c38280a28101ac
Andres G. Aragoneses <knocte@gmail.com>
2017-10-05 17:01:30 +0800

    Backend: try to make Currency->JSon conversion Portable-compatible
    Apparently .NETCore or the Portable class libs don't have
    System.ComponentModel.TypeConverter :(
    
    This is the workaround recommended in:
    https://stackoverflow.com/a/40480742

--- src/GWallet.Backend/Currency.fs
+++ src/GWallet.Backend/Currency.fs
@@ -1,15 +1,16 @@
 ﻿namespace GWallet.Backend
 
-open System.ComponentModel
+open System
+open System.Linq
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
 
 // this attribute below is for Json.NET (Newtonsoft.Json) to be able to deserialize this as a dict key
-[<TypeConverter(typeof<StringTypeConverter>)>]
+[<JsonConverter(typeof<CurrencyConverter>)>]
 type Currency =
     | ETH
     | ETC
-    static member ToStrings() =
-        Microsoft.FSharp.Reflection.FSharpType.GetUnionCases(typeof<Currency>)
-            |> Array.map (fun info -> info.Name)
     static member GetAll(): seq<Currency> =
         FSharpUtil.GetAllElementsFromDiscriminatedUnion<Currency>()
     override self.ToString() =
@@ -17,12 +18,25 @@ type Currency =
 
 // the reason we have used "and" is because of the circular reference
 // between StringTypeConverter and Currency
-and private StringTypeConverter() =
-    inherit TypeConverter()
-    override this.CanConvertFrom(context, sourceType) =
-        sourceType = typeof<string> || base.CanConvertFrom(context, sourceType)
-    override this.ConvertFrom(context, culture, value) =
-        match value with
-        | :? string as stringValue ->
-            Seq.find (fun cur -> cur.ToString() = stringValue) (Currency.GetAll()) :> obj
-        | _ -> base.ConvertFrom(context, culture, value)
+and private CurrencyConverter() =
+    inherit JsonConverter()
+
+    override this.CanConvert(objectType): bool =
+        objectType = typedefof<Currency>
+
+    override this.ReadJson(reader: JsonReader, objectType: Type, existingValue: Object, serializer: JsonSerializer) =
+        if (reader.TokenType = JsonToken.Null) then
+            null
+        else
+            let token =
+                JToken.Load(reader)
+                      // not sure about the below way to convert to string, in stackoverflow it was a C# cast
+                      .ToString()
+            try
+                Currency.GetAll().First(fun currency -> currency.ToString() = token) :> Object
+            with ex -> raise(new Exception(sprintf "Currency not found: %s" token, ex))
+
+    override this.WriteJson(writer: JsonWriter, value: Object, serializer: JsonSerializer) =
+        let currency = value :?> Currency
+        writer.WriteValue(currency.ToString())
+
