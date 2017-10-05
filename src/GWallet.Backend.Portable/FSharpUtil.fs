a4d5ff191ef92dc4312a12d964f097844da6517c
Andres G. Aragoneses <knocte@gmail.com>
2017-10-05 18:12:40 +0800

    Backend.Portable: include a slightly smaller version of FSharpUtils
    So that Currency type can be built in Portable mode.

---
+++ src/GWallet.Backend.Portable/FSharpUtil.fs
@@ -0,0 +1,21 @@
+﻿namespace GWallet.Backend
+
+open System
+open System.Linq
+open Microsoft.FSharp.Reflection
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Converters
+
+module FSharpUtil =
+
+// http://stackoverflow.com/a/28466431/6503091
+    // will crash if 'T contains members which aren't only tags
+    let Construct<'T> (caseInfo: UnionCaseInfo) = FSharpValue.MakeUnion(caseInfo, [||]) :?> 'T
+
+    let GetUnionCaseInfoAndInstance<'T> (caseInfo: UnionCaseInfo) = (Construct<'T> caseInfo)
+
+    let GetAllElementsFromDiscriminatedUnion<'T>() =
+        FSharpType.GetUnionCases(typeof<'T>)
+        |> Seq.map GetUnionCaseInfoAndInstance<'T>
+
