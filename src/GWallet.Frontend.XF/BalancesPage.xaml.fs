e4e3b147fc3f86ed2c8fb63184375605677297df
Andres G. Aragoneses <knocte@gmail.com>
2017-12-18 02:37:51 +0800

    Frontend.XF: add ability to Receive funds
    The simplistic way to do this is a Receive button that
    just copies the address to the clipboard.

--- src/GWallet.Frontend.XF/BalancesPage.xaml.fs
+++ src/GWallet.Frontend.XF/BalancesPage.xaml.fs
@@ -1,9 +1,11 @@
 ﻿namespace GWallet.Frontend.XF
 
 open System
+open System.Threading.Tasks
 
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
+open Plugin.Clipboard
 
 open GWallet.Backend
 
@@ -23,7 +25,24 @@ type BalancesPage() =
                 | NotFresh(Cached(amount,_)) -> amount.ToString()
                 | Fresh(amount) -> amount.ToString()
             let accountBalance = Label()
+
             accountBalance.Text <- sprintf "%s %s" balanceAmount (account.Currency.ToString())
+            let receiveButton = Button()
+            receiveButton.Text <- "Receive"
+            receiveButton.Clicked.Subscribe(fun _ ->
+                CrossClipboard.Current.SetText account.PublicAddress
+                receiveButton.IsEnabled <- false
+                receiveButton.Text <- "Copied address to clipboard"
+                Task.Run(fun _ ->
+                    Task.Delay(TimeSpan.FromSeconds(2.0)).Wait()
+                    Device.BeginInvokeOnMainThread(fun _ ->
+                        receiveButton.Text <- "Receive"
+                        receiveButton.IsEnabled <- true
+                    )
+                ) |> ignore
+
+            ) |> ignore
             mainLayout.Children.Add(accountBalance)
+            mainLayout.Children.Add(receiveButton)
 
 
