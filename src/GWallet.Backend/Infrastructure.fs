4f45d970c296212a48bc0104610e7c9cfb239563
Andres G. Aragoneses <knocte@gmail.com>
2018-01-03 12:19:14 +0800

    Backend,Frontend: change Sentry nugetlib & upgrade to .NET4.6
    Android's project was not building because SharpRaven is not
    compatible with .NET Standard so we switch to using SharpRavenLight
    which it is, however the last version of it (the one that works for
    me, as the previous didn't) depends on .NET 4.6 (instead of 4.5.x).
    
    Build error was:
    System.IO.FileNotFoundException: Could not load assembly 'SharpRaven, Version=2.2.0.0, Culture=neutral, PublicKeyToken='. Perhaps it doesn't exist in the Mono for Android profile?
    File name: 'SharpRaven.dll'
      at Java.Interop.Tools.Cecil.DirectoryAssemblyResolver.Resolve (Mono.Cecil.AssemblyNameReference reference, Mono.Cecil.ReaderParameters parameters) [0x00099] in /Users/builder/data/lanes/5749/d8c6e504/source/xamarin-android/external/Java.Interop/src/Java.Interop.Tools.Cecil/Java.Interop.Tools.Cecil/DirectoryAssemblyResolver.cs:229
      at Java.Interop.Tools.Cecil.DirectoryAssemblyResolver.Resolve (Mono.Cecil.AssemblyNameReference reference) [0x00000] in /Users/builder/data/lanes/5749/d8c6e504/source/xamarin-android/external/Java.Interop/src/Java.Interop.Tools.Cecil/Java.Interop.Tools.Cecil/DirectoryAssemblyResolver.cs:179
      at Xamarin.Android.Tasks.ResolveAssemblies.AddAssemblyReferences (Java.Interop.Tools.Cecil.DirectoryAssemblyResolver resolver, System.Collections.Generic.ICollection`1[T] assemblies, Mono.Cecil.AssemblyDefinition assembly, System.Boolean topLevel) [0x0015c] in <acf4a3302c8d4a7eaf06af6dce1aafb2>:0
      at Xamarin.Android.Tasks.ResolveAssemblies.AddAssemblyReferences (Java.Interop.Tools.Cecil.DirectoryAssemblyResolver resolver, System.Collections.Generic.ICollection`1[T] assemblies, Mono.Cecil.AssemblyDefinition assembly, System.Boolean topLevel) [0x00166] in <acf4a3302c8d4a7eaf06af6dce1aafb2>:0
      at Xamarin.Android.Tasks.ResolveAssemblies.Execute (Java.Interop.Tools.Cecil.DirectoryAssemblyResolver resolver) [0x001c7] in <acf4a3302c8d4a7eaf06af6dce1aafb2>:0  (GWallet.Frontend.XamForms.Droid)

--- src/GWallet.Backend/Infrastructure.fs
+++ src/GWallet.Backend/Infrastructure.fs
@@ -2,13 +2,13 @@
 
 open System
 
-open SharpRaven
-open SharpRaven.Data
+open SharpRavenLight
+open SharpRavenLight.Data
 
 module Infrastructure =
 
     let private sentryUrl = "https://4d1c6170ee37412fab20f8c63a2ade24:fc5e2c50990e48929d190fc283513f87@sentry.io/187797"
-    let private ravenClient = RavenClient sentryUrl
+    let private ravenClient = RavenClientLight sentryUrl
 
     let public Report (ex: Exception) =
 
