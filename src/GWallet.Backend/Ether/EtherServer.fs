d0ccc5a1fb36aad3ec6b5fa878db0c3c209974a0
Andres G. Aragoneses <knocte@gmail.com>
2018-07-30 16:16:09 +0800

    Backend/Ether: add 2 more ETC public nodes (chainkorea & epool2)
    Extracted from here:
    https://github.com/MyCryptoHQ/MyCrypto/blob/develop/common/libs/nodes/configs.ts

--- src/GWallet.Backend/Ether/EtherServer.fs
+++ src/GWallet.Backend/Ether/EtherServer.fs
@@ -64,22 +64,26 @@ module Server =
     let private ethWeb3InfuraMyEtherWallet = SomeWeb3("https://mainnet.infura.io/mew")
 
     // TODO: add the one from https://etcchain.com/api/ too
-    let private etcWeb3ePoolIo = SomeWeb3("https://mewapi.epool.io")
+    let private etcWeb3ePoolIo1 = SomeWeb3("https://cry.epool.io")
+    let private etcWeb3ePoolIo2 = SomeWeb3("https://mewapi.epool.io")
     let private etcWeb3ZeroXInfraGeth = SomeWeb3("https://etc-geth.0xinfra.com")
     let private etcWeb3ZeroXInfraParity = SomeWeb3("https://etc-parity.0xinfra.com")
     let private etcWeb3CommonWealthGeth = SomeWeb3("https://etcrpc.viperid.online")
     // FIXME: the below one doesn't seem to work; we should include it anyway and make the algorithm discard it at runtime
     //let private etcWeb3CommonWealthMantis = SomeWeb3("https://etc-mantis.callisto.network")
     let private etcWeb3CommonWealthParity = SomeWeb3("https://etc-parity.callisto.network")
+    let private etcWeb3ChainKorea = SomeWeb3("https://node.classicexplorer.org/")
 
     let GetWeb3Servers (currency: Currency): list<SomeWeb3> =
         if currency = ETC then
             [
+                etcWeb3ePoolIo1;
+                etcWeb3ChainKorea;
                 etcWeb3CommonWealthParity;
                 etcWeb3CommonWealthGeth;
-                etcWeb3ePoolIo;
                 etcWeb3ZeroXInfraParity;
                 etcWeb3ZeroXInfraGeth;
+                etcWeb3ePoolIo2;
             ]
         elif (currency.IsEthToken() || currency = Currency.ETH) then
             [
