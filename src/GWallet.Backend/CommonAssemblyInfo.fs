e3ce25d48287d63c0670c74799b24538e7c873cf
Andres G. Aragoneses <knocte@gmail.com>
2018-08-15 17:33:24 +0800

    Backend: bump version to 0.4 (to differentiate from master)

--- src/GWallet.Backend/CommonAssemblyInfo.fs
+++ src/GWallet.Backend/CommonAssemblyInfo.fs
@@ -19,8 +19,8 @@ open System.Reflection
 // [<assembly: AssemblyVersion("1.0.*")>]
 
 
-[<assembly: AssemblyVersion("0.3.0.0")>]
-[<assembly: AssemblyFileVersion("0.3.0.0")>]
+[<assembly: AssemblyVersion("0.4.0.0")>]
+[<assembly: AssemblyFileVersion("0.4.0.0")>]
 
 do
     ()
