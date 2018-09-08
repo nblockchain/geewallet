f7b7a6ecf86118d74ed4c8962844fb46faad0571
Andres G. Aragoneses <knocte@gmail.com>
2018-09-08 18:13:47 +0800

    scripts: add a handy 'replace.fsx' script
    Given that replacing a string in all files of a subdirectory
    is a tricky thing to do in a crossplatform way (even the unix
    commands I found in stackoverflow were not portable from Linux
    to Mac), I'm writing here a handy F# script that can do this
    (even in Windows!). It will become handy when we want to do
    things like:
    
     - version bumps
     - upgrading Nethereum dependencies (doing it in the IDE is
       too tedious)
     - etc.

---
+++ scripts/replace.fsx
@@ -0,0 +1,42 @@
+#!/usr/bin/env fsharpi
+
+open System
+open System.IO
+#load "Infra.fs"
+open FSX.Infrastructure
+
+let rec ReplaceInDir (dir: DirectoryInfo) (oldString: string) (newString: string) =
+    let ReplaceInFile (file: FileInfo) (oldString: string) (newString: string) =
+        let oldText = File.ReadAllText file.FullName
+        let newText = oldText.Replace(oldString, newString)
+        if newText <> oldText then
+            File.WriteAllText(file.FullName, newText)
+
+    for file in dir.GetFiles() do
+        if (file.Extension.ToLower() <> "dll") &&
+           (file.Extension.ToLower() <> "exe") &&
+           (file.Extension.ToLower() <> "png") then
+            ReplaceInFile file oldString newString
+
+    for subFolder in dir.GetDirectories() do
+        if subFolder.Name <> ".git" then
+            ReplaceInDir subFolder oldString newString
+
+let args = Util.FsxArguments()
+let note = "NOTE: by default, some kind of files/folders will be excluded, e.g.: .git, *.dll, *.png, ..."
+if args.Length > 2 then
+    Console.Error.WriteLine "Can only pass two arguments: replace.fsx oldstring newstring"
+    Console.WriteLine note
+    Environment.Exit 1
+elif args.Length < 2 then
+    Console.Error.WriteLine "Need to pass two arguments: replace.fsx oldstring newstring"
+    Console.WriteLine note
+    Environment.Exit 1
+
+let oldString = args.[0]
+let newString = args.[1]
+
+let startDir = DirectoryInfo (Directory.GetCurrentDirectory())
+
+ReplaceInDir startDir oldString newString
+
