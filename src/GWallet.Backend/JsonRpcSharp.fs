0631661315abe46a42f8ed7f7582f641254ed205
Dave Falkner <ardave@gmail.com>
2018-09-08 15:15:54 +0800

    Backend/JsonRpc: retry in 2 more cases of failed DNS resolution
    Can't say for certain if it's my DNS provider or a Windows vs. Mac
    difference, but I've had both of the problems addressed here cause
    the test ``can connect to some electrum BTC servers`` to fail
    consistently, the two problems addressed being 1) Dns.GetHostEntry
    returning an empty AddressList for one of the IP Addressed in the
    servers config file and 2) a SocketException with SocketError.NoData.
    MSDN documentation describes this error as caused by:
    
    "The usual example for this is a host name-to-address translation
    attempt (using gethostbyname or WSAAsyncGetHostByName) which uses the
    DNS (Domain Name Server). An MX record is returned but no A record”
    indicating the host itself exists, but is not directly reachable."

--- src/GWallet.Backend/JsonRpcSharp.fs
+++ src/GWallet.Backend/JsonRpcSharp.fs
@@ -90,9 +90,12 @@ module JsonRpcSharp =
         let Read (stream: NetworkStream): string =
             ReadInternal stream [] DateTime.Now
 
-        let ResolveAsync (hostName: string) =
+        let ResolveAsync (hostName: string): Task<Option<IPAddress>> =
             // FIXME: loop over all addresses?
-            Task.Run(fun _ -> Dns.GetHostEntry(hostName).AddressList.[0])
+            Task.Run(fun _ ->
+                (Dns.GetHostEntry hostName).AddressList
+                    |> Array.tryHead
+            )
 
         let Connect(): System.Net.Sockets.TcpClient =
 
@@ -102,13 +105,18 @@ module JsonRpcSharp =
                     let resolveTask = ResolveAsync host
                     if not (resolveTask.Wait Config.DEFAULT_NETWORK_TIMEOUT) then
                         raise(ServerCannotBeResolvedException(exceptionMsg))
-                    resolveTask.Result
+                    match resolveTask.Result with
+                    | None ->
+                        raise <| ServerCannotBeResolvedException
+                                     (sprintf "DNS host entry lookup resulted in no records for %s" host)
+                    | Some ipAddress -> ipAddress
                 with
                 | ex ->
                     let socketException = FSharpUtil.FindException<SocketException>(ex)
                     if (socketException.IsNone) then
                         reraise()
                     if (socketException.Value.ErrorCode = int SocketError.HostNotFound ||
+                        socketException.Value.ErrorCode = int SocketError.NoData ||
                         socketException.Value.ErrorCode = int SocketError.TryAgain) then
                         raise(ServerCannotBeResolvedException(exceptionMsg, ex))
                     raise(UnhandledSocketException(socketException.Value.ErrorCode, ex))
