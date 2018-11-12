nblockchain/geewallet

commit 20e6ce556e2b7f390eff1d4ddf1229f2492841ea

scripts/bump.fsx: add a summary message hinting to push the tag

scripts/bump.fsx
@@ -38,3 +38,5 @@ Process.Execute (sprintf "git commit -m \"Bump version: %s -> %s\"" (fullVersion
                  false, true)
 Process.Execute (sprintf "git tag %s" (newFullVersion.ToString()),
                  false, true)
+Console.WriteLine sprintf "Version bumped. Remember to push via `git push <remote> <branch> && git push <remote> %s`"
+                          (newFullVersion.ToString())

commit 63be63378d104ae02668b1c8c7ffc80737d7be26

scripts/bump.fsx: fix build
Typo introduced in 20e6ce556e2b7f390eff1d4ddf1229f2492841ea

scripts/bump.fsx
@@ -38,5 +38,5 @@ Process.Execute (sprintf "git commit -m \"Bump version: %s -> %s\"" (fullVersion
                  false, true)
 Process.Execute (sprintf "git tag %s" (newFullVersion.ToString()),
                  false, true)
-Console.WriteLine sprintf "Version bumped. Remember to push via `git push <remote> <branch> && git push <remote> %s`"
-                          (newFullVersion.ToString())
+Console.WriteLine (sprintf "Version bumped. Remember to push via `git push <remote> <branch> && git push <remote> %s`"
+                           (newFullVersion.ToString()))