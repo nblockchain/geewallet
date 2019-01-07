nblockchain/geewallet

commit f9ceef3792171c8dc55ae4e6b787358686d80538

Backend: refactoring to bring more consistency
This way we remove the duplication of the File.ReadAllText
call (now AccountTypes.fs doesn't contain any more hardcoded
file access) and make both NormalAccount and ArchivedAccount
receive the same kind of func (with the same signature) in
their constructors.

src/GWallet.Backend/AccountTypes.fs
@@ -17,7 +17,7 @@ type NormalAccount(currency: Currency, accountFile: FileInfo, fromAccountFileToP
     member val AccountFile = accountFile with get
 
     member internal self.GetEncryptedPrivateKey() =
-        File.ReadAllText accountFile.FullName
+        fromAccountFileToPublicAddress accountFile
 
     interface IAccount with
         member val Currency = currency with get
@@ -29,12 +29,12 @@ type ReadOnlyAccount(currency: Currency, publicAddress: string) =
         member val Currency = currency with get
         member val PublicAddress = publicAddress with get
 
-type ArchivedAccount(currency: Currency, accountFile: FileInfo, fromUnencryptedPrivateKeyToPublicAddress: string -> string) =
+type ArchivedAccount(currency: Currency, accountFile: FileInfo, fromAccountFileToPublicAddress: FileInfo -> string) =
     member internal __.GetUnencryptedPrivateKey() =
-        File.ReadAllText accountFile.FullName
+        fromAccountFileToPublicAddress accountFile
 
     interface IAccount with
         member val Currency = currency with get
         member self.PublicAddress
             with get() =
-                fromUnencryptedPrivateKeyToPublicAddress (self.GetUnencryptedPrivateKey())
+                fromAccountFileToPublicAddress accountFile

src/GWallet.Backend/Account.fs
@@ -97,7 +97,7 @@ module Account =
             let allCurrencies = Currency.GetAll()
 
             for currency in allCurrencies do
-                let fromUnencryptedPrivateKeyToPublicAddress =
+                let fromUnencryptedPrivateKeyToPublicAddressFunc =
                     if currency.IsUtxo() then
                         UtxoCoin.Account.GetPublicAddressFromUnencryptedPrivateKey currency
                     elif currency.IsEtherBased() then
@@ -105,8 +105,12 @@ module Account =
                     else
                         failwith (sprintf "Unknown currency %A" currency)
 
+                let fromConfigAccountFileToPublicAddressFunc (accountConfigFile: FileInfo) =
+                    let privateKeyFromConfigFile = File.ReadAllText accountConfigFile.FullName
+                    fromUnencryptedPrivateKeyToPublicAddressFunc privateKeyFromConfigFile
+
                 for accountFile in Config.GetAllArchivedAccounts(currency) do
-                    let account = ArchivedAccount(currency, accountFile, fromUnencryptedPrivateKeyToPublicAddress)
+                    let account = ArchivedAccount(currency, accountFile, fromConfigAccountFileToPublicAddressFunc)
                     let maybeBalance = GetUnconfirmedPlusConfirmedBalance account Mode.Fast
                     yield async {
                         let! unconfirmedBalance = maybeBalance
@@ -213,6 +217,7 @@ module Account =
                 Ether.Account.GetPublicAddressFromUnencryptedPrivateKey
             else
                 failwith (sprintf "Unknown currency %A" currency)
+
         let fromConfigFileToPublicAddressFunc (accountConfigFile: FileInfo) =
             let privateKeyFromConfigFile = File.ReadAllText accountConfigFile.FullName
             fromUnencryptedPrivateKeyToPublicAddressFunc privateKeyFromConfigFile
@@ -224,7 +229,7 @@ module Account =
             ExtractPublicAddressFromConfigFileFunc = fromConfigFileToPublicAddressFunc
         }
         let newAccountFile = Config.AddArchivedAccount conceptAccount
-        ArchivedAccount(currency, newAccountFile, fromUnencryptedPrivateKeyToPublicAddressFunc)
+        ArchivedAccount(currency, newAccountFile, fromConfigFileToPublicAddressFunc)
 
     let Archive (account: NormalAccount)
                 (password: string)

commit d5c96562b04a6b9e11dabbccf0581bbfee84ed84

Backend: fix bug introduced in recent refactoring
Introduced in:
https://github.com/diginex/geewallet/commit/f9ceef3792171c8dc55ae4e6b787358686d80538

It was preventing to send transactions.

src/GWallet.Backend/AccountTypes.fs
@@ -40,7 +40,7 @@ type NormalAccount(currency: Currency, accountFile: FileRepresentation,
     inherit BaseAccount(currency, accountFile, fromAccountFileToPublicAddress)
 
     member internal self.GetEncryptedPrivateKey() =
-        fromAccountFileToPublicAddress accountFile
+        accountFile.Content()
 
     override this.Kind = AccountKind.Normal
 
@@ -55,6 +55,6 @@ type ArchivedAccount(currency: Currency, accountFile: FileRepresentation,
     inherit BaseAccount(currency, accountFile, fromAccountFileToPublicAddress)
 
     member internal __.GetUnencryptedPrivateKey() =
-        fromAccountFileToPublicAddress accountFile
+        accountFile.Content()
 
     override this.Kind = AccountKind.Archived