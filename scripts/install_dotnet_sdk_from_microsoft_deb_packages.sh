c1a0e2b5c93d298eade577bf99fb37ff6bc95fc4
Andres G. Aragoneses <knocte@gmail.com>
2019-01-30 22:03:12 +0800

    gitlab-ci,scripts: extract a script for installing .NET SDK
    In the same vein as this change in master branch:
    8c1e6149b499a9441e5ae780ff5ce6c02745c91c

---
+++ scripts/install_dotnet_sdk_from_microsoft_deb_packages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# TODO: check for .NET SDK in the configure.fsx file as well
+
+# taken from https://www.microsoft.com/net/download/linux-package-manager/ubuntu18-04/sdk-current
+apt install -y wget
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt install -y apt-transport-https
+apt update
+apt install -y dotnet-sdk-2.1
+
