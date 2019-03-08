f53fa2ab52d389fdf5c601eebf47c19a072d495e
Andres G. Aragoneses <knocte@gmail.com>
2019-03-08 18:58:58 +0800

    Backend.Tests: create electrum integration test focusing specific server
    We're still trying to catch the origin of this problem[1],
    which seems only to happen with one server[2]. So we add this
    test focusing on this specific server, to run 10 times, to see
    if we are more likely to catch it.
    
    [1] 5cf89aa021ceb56fb15bc8ec7eccd566d9cefc8a
    [2] https://sentry.io/organizations/nblockchain/issues/910256913/activity/

--- src/GWallet.Backend.Tests/ElectrumIntegrationTests.fs
+++ src/GWallet.Backend.Tests/ElectrumIntegrationTests.fs
@@ -101,28 +101,37 @@ type ElectrumIntegrationTests() =
     let rec AtLeastNJobsWork(jobs: List<Async<Option<ElectrumServer>>>) (minimumCountNeeded: uint32): unit =
         match jobs with
         | [] ->
-            Assert.Fail ("Not enough servers were reached")
+            if minimumCountNeeded > 0u then
+                Assert.Fail (sprintf "Not enough servers were reached. Required: %i" minimumCountNeeded)
         | head::tail ->
             match head |> Async.RunSynchronously with
             | None ->
                 AtLeastNJobsWork tail minimumCountNeeded
             | Some _ ->
-                let newCount = (minimumCountNeeded - 1u)
-                if newCount <> 0u then
+                if minimumCountNeeded = 0u then
+                    ()
+                else
+                    let newCount = (minimumCountNeeded - 1u)
                     AtLeastNJobsWork tail newCount
 
-    let CheckElectrumServersConnection electrumServers
+    let TestElectrumServersConnections (electrumServers: seq<_>)
                                        currency
                                        (query: ElectrumServer->Async<'T>)
                                        (assertion: 'T->unit)
+                                       (atLeast: uint32)
                                            =
+        if not (electrumServers.Any()) then
+            failwith "list received shouldn't be empty"
         let reachServerJobs = seq {
             for electrumServer in electrumServers do
                 yield CheckServerIsReachable electrumServer currency query assertion None
         }
         AtLeastNJobsWork (reachServerJobs |> List.ofSeq)
                          // more than one
-                         2u
+                         atLeast
+
+    let CheckElectrumServersConnection a b c d =
+        TestElectrumServersConnections a b c d 2u
 
     let UtxosAssertion (utxos: array<BlockchainScripthashListUnspentInnerResult>) =
         // if these ancient addresses get withdrawals it would be interesting in the crypto space...
@@ -179,6 +188,7 @@ type ElectrumIntegrationTests() =
         CheckElectrumServersConnection ElectrumServerSeedList.DefaultBtcList currency
                                        (ElectrumClient.GetUnspentTransactionOutputs argument) UtxosAssertion
 
+
     [<Test>]
     member __.``should not get empty/null response from electrum BTC servers``() =
         let currency = Currency.BTC
@@ -188,4 +198,19 @@ type ElectrumIntegrationTests() =
 
         CheckElectrumServersConnection ElectrumServerSeedList.DefaultBtcList currency
                                        (ElectrumClient.GetBlockchainTransaction argument) TxAssertion
+
+    [<Test>]
+    member __.``should not get empty/null response from electrum BTC servers (specific rebel server)``() =
+        let currency = Currency.BTC
+
+        // some random existing transaction
+        let argument = "2f309ef555110ab4e9c920faa2d43e64f195aa027e80ec28e1d243bd8929a2fc"
+
+        let sameServerManyTimes =
+            Seq.replicate 10 (List.find(fun s -> s.Fqdn = "E-X.not.fyi") ElectrumServerSeedList.DefaultBtcList)
+
+        TestElectrumServersConnections sameServerManyTimes
+                                       currency
+                                       (ElectrumClient.GetBlockchainTransaction argument) TxAssertion
+                                       0u
 
