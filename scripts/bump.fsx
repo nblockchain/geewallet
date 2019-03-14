6b4bed6f3b9ac877d675a4adfc34d96034eea9cd
Andres G. Aragoneses <knocte@gmail.com>
2019-03-14 15:54:55 +0800

    scripts/bump.fsx: use odd minorRevisions for unstable, even for stable
    From now on, we will only tags versions whose minorRevision is even
    (e.g. 0.4.1.20, 0.3.2.88), and leave odd versioms for unreleased and
    untagged versions (e.g. 0.4.1.21, 0.3.2.89).
    
    This is important because we package some artifacts with the version
    number in the artifact name, and it will allow us to know if the
    artifact (e.g. a zip file) comes from a released version (even number)
    or from an odd version (which means it's from some commit that was
    pushed in between the previous and the next even version). So if you
    see an artifact such as 0.3.3.3, it means you cannot determine exactly
    to which commit hash it exactly belongs, but if it's 0.3.3.4, you know
    that this version number maps to a git tag in the repository (and thus
    a commit hash).

--- scripts/bump.fsx
+++ scripts/bump.fsx
@@ -5,55 +5,82 @@ open System.IO
 #load "Infra.fs"
 open FSX.Infrastructure
 
-let rootDir = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, ".."))
-let fullVersion = Misc.GetCurrentVersion(rootDir)
-let androidVersion = fullVersion.MinorRevision
+let IsStableRevision revision =
+    (int revision % 2) = 0
 
-let newFullVersion,newVersion =
-    if Util.FsxArguments().Length > 0 then
-        if Util.FsxArguments().Length > 1 then
-            Console.Error.WriteLine "Only one argument supported, not more"
-            Environment.Exit 1
-            failwith "Unreachable"
+let Bump(toStable: bool): Version*Version =
+    let rootDir = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, ".."))
+    let fullVersion = Misc.GetCurrentVersion(rootDir)
+    let androidVersion = fullVersion.MinorRevision
+
+    if toStable && IsStableRevision androidVersion then
+        failwith "bump script expects you to be in unstable version currently, but we found a stable"
+    if (not toStable) && (not (IsStableRevision androidVersion)) then
+        failwith "sanity check failed, post-bump should happen in a stable version"
+
+    let newFullVersion,newVersion =
+        if Util.FsxArguments().Length > 0 then
+            if Util.FsxArguments().Length > 1 then
+                Console.Error.WriteLine "Only one argument supported, not more"
+                Environment.Exit 1
+                failwith "Unreachable"
+            else
+                let full = Version(Util.FsxArguments().Head)
+                full,full.MinorRevision
         else
-            let full = Version(Util.FsxArguments().Head)
-            full,full.MinorRevision
-    else
-        let newVersion = androidVersion + 1s
-        let full = Version(sprintf "%i.%i.%i.%i"
-                                   fullVersion.Major
-                                   fullVersion.Minor
-                                   fullVersion.Build
-                                   newVersion)
-        full,newVersion
+            let newVersion = androidVersion + 1s
+            let full = Version(sprintf "%i.%i.%i.%i"
+                                       fullVersion.Major
+                                       fullVersion.Minor
+                                       fullVersion.Build
+                                       newVersion)
+            full,newVersion
 
-let replaceScript = Path.Combine(__SOURCE_DIRECTORY__, "replace.fsx")
-Process.SafeExecute (sprintf "%s %s %s"
-                         replaceScript
-                         (fullVersion.ToString())
-                         (newFullVersion.ToString()),
-                     Echo.Off) |> ignore
-// to replace Android's versionCode attrib in AndroidManifest.xml
-Process.SafeExecute (sprintf "%s versionCode=\\\"%s\\\" versionCode=\\\"%s\\\""
-                         replaceScript
-                         (androidVersion.ToString())
-                         (newVersion.ToString()),
-                     Echo.Off) |> ignore
+    let replaceScript = Path.Combine(__SOURCE_DIRECTORY__, "replace.fsx")
+    Process.SafeExecute (sprintf "%s %s %s"
+                             replaceScript
+                             (fullVersion.ToString())
+                             (newFullVersion.ToString()),
+                         Echo.Off) |> ignore
+    // to replace Android's versionCode attrib in AndroidManifest.xml
+    Process.SafeExecute (sprintf "%s versionCode=\\\"%s\\\" versionCode=\\\"%s\\\""
+                             replaceScript
+                             (androidVersion.ToString())
+                             (newVersion.ToString()),
+                         Echo.Off) |> ignore
 
-Process.SafeExecute (sprintf "git add src/GWallet.Backend.Tests/*.json",
-                     Echo.Off) |> ignore
-Process.SafeExecute (sprintf "git add src/GWallet.Backend/Properties/CommonAssemblyInfo.fs",
-                     Echo.Off) |> ignore
-Process.SafeExecute (sprintf "git add src/GWallet.Frontend.XF.Android/Properties/AndroidManifest.xml",
-                     Echo.Off) |> ignore
-Process.SafeExecute (sprintf "git commit -m \"Bump version: %s -> %s\"" (fullVersion.ToString()) (newFullVersion.ToString()),
-                     Echo.Off) |> ignore
+    fullVersion,newFullVersion
 
 
-Process.Execute (sprintf "git tag --delete %s" (newFullVersion.ToString()),
-                 Echo.Off) |> ignore
-Process.SafeExecute (sprintf "git tag %s" (newFullVersion.ToString()),
+let GitCommit (fullVersion: Version) (newFullVersion: Version) =
+    Process.SafeExecute (sprintf "git add src/GWallet.Backend.Tests/*.json",
+                         Echo.Off) |> ignore
+    Process.SafeExecute (sprintf "git add src/GWallet.Backend/Properties/CommonAssemblyInfo.fs",
+                         Echo.Off) |> ignore
+    Process.SafeExecute (sprintf "git add src/GWallet.Frontend.XF.Android/Properties/AndroidManifest.xml",
+                         Echo.Off) |> ignore
+
+    let commitMessage = sprintf "Bump version: %s -> %s" (fullVersion.ToString()) (newFullVersion.ToString())
+    let finalCommitMessage =
+        if IsStableRevision fullVersion.MinorRevision then
+            sprintf "(Post)%s" commitMessage
+        else
+            commitMessage
+    Process.SafeExecute (sprintf "git commit -m \"%s\"" finalCommitMessage,
+                         Echo.Off) |> ignore
+
+let GitTag newFullVersion =
+    Process.Execute (sprintf "git tag --delete %s" (newFullVersion.ToString()),
                      Echo.Off) |> ignore
+    Process.SafeExecute (sprintf "git tag %s" (newFullVersion.ToString()),
+                         Echo.Off) |> ignore
+
+let fullUnstableVersion,newFullStableVersion = Bump true
+GitCommit fullUnstableVersion newFullStableVersion
+GitTag newFullStableVersion
+
+let fullStableVersion,newFullUnstableVersion = Bump false
+GitCommit fullStableVersion newFullUnstableVersion
 
 Console.WriteLine (sprintf "Version bumped. Remember to push via `git push <remote> <branch> %s`"
-                           (newFullVersion.ToString()))
+                           (newFullStableVersion.ToString()))
