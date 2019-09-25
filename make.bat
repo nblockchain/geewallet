94ec3c6f14da338e760837aec4dd631a57664110
Andres G. Aragoneses <knocte@gmail.com>
2019-09-25 01:08:41 -0700

    Reuse fsi.exe finding across .bat files

--- make.bat
+++ make.bat
@@ -1,22 +1,3 @@
 @ECHO OFF
 
-REM please keep this file in sync with configure.bat
-
-SET BUILDTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET COMMUNITY="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET ENTERPRISE="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET FSXSCRIPT=scripts\make.fsx
-
-IF EXIST %ENTERPRISE% (
-    %ENTERPRISE% %FSXSCRIPT% %*
-) ELSE (
-    IF EXIST %COMMUNITY% (
-        %COMMUNITY% %FSXSCRIPT% %*
-    ) ELSE (
-    	IF EXIST %BUILDTOOLS% (
-    	    %BUILDTOOLS% %FSXSCRIPT% %*
-    	) ELSE (
-    	    ECHO fsi.exe not found, is F# installed?
-    	)
-    )
-)
+CALL scripts\fsi.bat scripts\make.fsx %*
