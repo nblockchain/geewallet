0f63ee5a1ab6b24864a2192d0942ddc83d67a935
Andres G. Aragoneses <knocte@gmail.com>
2019-09-30 20:29:57 +0800

    Backend.Tests: add a basic test for FSharpUtil.FindException too
    Yeah let's not be stingy.

--- src/GWallet.Backend.Tests/FSharpUtil.fs
+++ src/GWallet.Backend.Tests/FSharpUtil.fs
@@ -11,6 +11,17 @@ open GWallet.Backend
 type FSharpUtilCoverage() =
 
     [<Test>]
+    member __.``basic test``() =
+        let innerEx = TaskCanceledException "bar"
+        let wrapperEx = Exception("foo", innerEx)
+        let childFound = FSharpUtil.FindException<TaskCanceledException> wrapperEx
+        match childFound with
+        | None -> failwith "should find through inner classes"
+        | Some ex ->
+            Assert.That(Object.ReferenceEquals(ex, innerEx), Is.True)
+            Assert.That(Object.ReferenceEquals(ex.InnerException, null))
+
+    [<Test>]
     member __.``it works with inherited classes (UnexpectedTaskCanceledException is child of TaskCanceledException)``() =
         let innerEx = TaskCanceledException "bar"
         let inheritedEx = UnexpectedTaskCanceledException("foo", innerEx)
