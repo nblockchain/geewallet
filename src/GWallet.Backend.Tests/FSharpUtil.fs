143337e66e865daf70196860e5fd6a064fd5627f
Andres G. Aragoneses <knocte@gmail.com>
2019-09-30 20:25:03 +0800

    Backend.Tests: add a test for FSharpUtil.FindException
    To make sure it works with inherited classes.

---
+++ src/GWallet.Backend.Tests/FSharpUtil.fs
@@ -0,0 +1,22 @@
+﻿namespace GWallet.Backend.Tests
+
+open System
+open System.Threading.Tasks
+
+open NUnit.Framework
+
+open GWallet.Backend
+
+[<TestFixture>]
+type FSharpUtilCoverage() =
+
+    [<Test>]
+    member __.``it works with inherited classes (UnexpectedTaskCanceledException is child of TaskCanceledException)``() =
+        let innerEx = TaskCanceledException "bar"
+        let inheritedEx = UnexpectedTaskCanceledException("foo", innerEx)
+        let parentFound = FSharpUtil.FindException<TaskCanceledException> inheritedEx
+        match parentFound with
+        | None -> failwith "should work with derived classes"
+        | Some ex ->
+            Assert.That(Object.ReferenceEquals(ex, inheritedEx), Is.True)
+            Assert.That(Object.ReferenceEquals(ex.InnerException, innerEx))
