27ea4026d1cc1f1376d201f94a2c2f59aa21a0bb
Andres G. Aragoneses <knocte@gmail.com>
2019-10-16 14:59:08 +0800

    Backend: Infrastructure code cleanup
    Less duplication and more behaviour consistency

--- src/GWallet.Backend/Infrastructure.fs
+++ src/GWallet.Backend/Infrastructure.fs
@@ -10,10 +10,18 @@ module Infrastructure =
     let private sentryUrl = "https://4d1c6170ee37412fab20f8c63a2ade24:fc5e2c50990e48929d190fc283513f87@sentry.io/187797"
     let private ravenClient = RavenClient(sentryUrl, Release = VersionHelper.CURRENT_VERSION)
 
-    let internal ReportError (errorMessage: string) =
-        ravenClient.Capture (SentryEvent (SentryMessage (errorMessage), Level = ErrorLevel.Error)) |> ignore
+    let private ReportInner (sentryEvent: SentryEvent) =
+        ravenClient.Capture sentryEvent |> ignore
 
-    let private ReportInner (ex: Exception) (errorLevel: ErrorLevel) =
+    let internal ReportError (errorMessage: string) =
+#if DEBUG
+        failwith errorMessage
+#else
+        let sentryEvent =  SentryEvent(SentryMessage errorMessage, Level = ErrorLevel.Error)
+        ReportInner sentryEvent
+#endif
+
+    let private Report (ex: Exception) (errorLevel: ErrorLevel) =
 
         // TODO: log this in a file (log4net?), as well as printing to the console, before sending to sentry
         Console.Error.WriteLine ex
@@ -22,14 +30,14 @@ module Infrastructure =
         raise ex
 #else
         let ev = SentryEvent(ex, Level = errorLevel)
-        ravenClient.Capture ev |> ignore
+        ReportInner ev
 #endif
 
     let ReportWarning (ex: Exception) =
-        ReportInner ex ErrorLevel.Warning
+        Report ex ErrorLevel.Warning
 
     let ReportCrash (ex: Exception) =
-        ReportInner ex ErrorLevel.Fatal
+        Report ex ErrorLevel.Fatal
 
     let private OnUnhandledException (sender: obj) (args: UnhandledExceptionEventArgs) =
         ReportCrash (args.ExceptionObject :?> Exception)
