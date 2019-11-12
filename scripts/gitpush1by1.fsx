a709bc7bbc959cdee236c833ef95eff5c8e2da2e
Andres G. Aragoneses <knocte@gmail.com>
2019-11-12 13:07:00 +0800

    scripts/gitpush1by1.fsx: improve algo to work when rewriting history
    Before this, when local tip and remote tip didn't match, it would fall
    into an infinite loop.

--- scripts/gitpush1by1.fsx
+++ scripts/gitpush1by1.fsx
@@ -36,19 +36,35 @@ let GetRemotes () =
     let gitRemote = Process.SafeExecute({ Command = "git"; Arguments = "remote" }, Echo.Off)
     Misc.CrossPlatformStringSplitInLines gitRemote.Output.StdOut
 
-let FindUnpushedCommits (lastCommitHashInRemote: string) =
-    let rec findUnpushedCommits commitsFoundSoFar currentSkipCount =
+let FindUnpushedCommits (remoteName: string) (remoteBranch: string) =
+    let rec findUnpushedCommits localCommitsWalkedSoFar currentSkipCount remoteCommits =
+        let rec findIntersection localCommits (remoteCommits: List<string>) =
+            match localCommits with
+            | [] -> None
+            | head::tail ->
+                if remoteCommits.Contains head then
+                    Some tail
+                else
+                    findIntersection tail remoteCommits
+
         Console.WriteLine "Walking tree..."
         let currentHash = Process.SafeExecute({ Command = "git";
                                                 Arguments = sprintf "log -1 --skip=%i --format=format:%%H"
                                                                     currentSkipCount },
                                               Echo.Off).Output.StdOut.Trim()
-        if currentHash = lastCommitHashInRemote then
-            commitsFoundSoFar
-        else
-            findUnpushedCommits (currentHash::commitsFoundSoFar) (currentSkipCount + 1)
+        let newRemoteCommits =
+            (GetLastNthCommitFromRemoteBranch remoteName remoteBranch currentSkipCount)::remoteCommits
 
-    findUnpushedCommits List.empty 0
+        let newLocalCommitsWalkedSoFar = currentHash::localCommitsWalkedSoFar
+        match findIntersection newLocalCommitsWalkedSoFar newRemoteCommits with
+        | Some theCommitsToPush ->
+            theCommitsToPush
+        | None ->
+            findUnpushedCommits newLocalCommitsWalkedSoFar
+                                (currentSkipCount + 1u)
+                                newRemoteCommits
+
+    findUnpushedCommits List.empty 0u List.empty
 
 let GetLastCommits (count: UInt32) =
     let rec getLastCommits commitsFoundSoFar currentSkipCount currentCount =
@@ -92,8 +108,7 @@ let currentBranch = Git.GetCurrentBranch()
 let commitsToBePushed =
     match maybeNumberOfCommits with
     | None ->
-        let lastCommitHashOfCurrentBranchInRemote = GetLastNthCommitFromRemoteBranch remote currentBranch 0u
-        let commitsToPush = FindUnpushedCommits lastCommitHashOfCurrentBranchInRemote
+        let commitsToPush = FindUnpushedCommits remote currentBranch
         if commitsToPush.Length = 0 then
             Console.Error.WriteLine (sprintf "Current branch '%s' in remote '%s' is already up to date. Force push by specifying number of commits as 2nd argument?"
                                              currentBranch remote)
