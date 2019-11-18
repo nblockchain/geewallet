nblockchain/geewallet

commit 54b2a367a114856d5bdcbfef344c94cf39c58d78

Backend: overhaul FaultTolerantParallelClient's cancellation
Hopefully the last attempt to fix the weird cancellation bug [1][2]
(that currently doesn't crash the app anymore, but generates a warning
and marks the balance with '?' to mean 'unknown').

This change makes it so that there's independent cancellationTokenSources
for each job, instead of having one shared across the FaultTPClient.Query
work, because I suspected that one job running HttpClient and facing the
TCE (to denote a timeout) could make the other jobs be cancelled as well.

[1] https://gitlab.com/knocte/geewallet/issues/125
[2] https://github.com/dotnet/corefx/issues/20296 ?

src/GWallet.Backend.Tests/AsyncCancellation.fs
@@ -96,7 +96,7 @@ type FaultTolerantParallelClientAsyncCancellation() =
         let client = FaultTolerantParallelClient<ServerDetails, SomeExceptionDuringParallelWork>
                          dummy_func_to_not_save_server_because_it_is_irrelevant_for_this_test
 
-        let externalCancellationSource = new CancellationTokenSource()
+        let externalCancellationSource = CustomCancelSource()
         let task = client.QueryWithCancellation externalCancellationSource settings allFuncs
                        |> Async.StartAsTask
 
@@ -121,7 +121,7 @@ type FaultTolerantParallelClientAsyncCancellation() =
     member __.``external cancellation source can come already canceled``() =
         let someLongTime = TimeSpan.FromSeconds 1.0
 
-        let externalCancellationSource = new CancellationTokenSource()
+        let externalCancellationSource = CustomCancelSource()
         externalCancellationSource.Cancel()
 
         let job1 = async {
@@ -161,11 +161,12 @@ type FaultTolerantParallelClientAsyncCancellation() =
         // to make sure the exception happened
         Assert.That(result.IsNone, Is.True)
 
+(*
     [<Test>]
     member __.``external cancellation source can come already disposed``() =
         let someLongTime = TimeSpan.FromSeconds 1.0
 
-        let externalCancellationSource = new CancellationTokenSource()
+        let externalCancellationSource = CustomCancelSource()
         externalCancellationSource.Dispose()
 
         let job1 = async {
@@ -204,15 +205,16 @@ type FaultTolerantParallelClientAsyncCancellation() =
 
         // to make sure the exception happened
         Assert.That(result.IsNone, Is.True)
+        *)
 
 
     [<Test>]
     member __.``external cancellation source can come already canceled&disposed``() =
         let someLongTime = TimeSpan.FromSeconds 1.0
 
-        let externalCancellationSource = new CancellationTokenSource()
+        let externalCancellationSource = CustomCancelSource()
         externalCancellationSource.Cancel()
-        externalCancellationSource.Dispose()
+        //externalCancellationSource.Dispose()
 
         let job1 = async {
             return 1
@@ -259,7 +261,7 @@ type FaultTolerantParallelClientAsyncCancellation() =
         let mutable longFuncFinishedExecution = false
         let mutable job1started = false
         let mutable job2started = false
-        let cancelSource = new CancellationTokenSource()
+        let cancelSource = CustomCancelSource()
 
         let SomeMethodAsync(cancelToken: CancellationToken): Task<unit> =
             let job = async {
@@ -374,7 +376,7 @@ type FaultTolerantParallelClientAsyncCancellation() =
     member __.``cancellationSource is disposed after FaultTolerantParallelClient finishes executing``() =
         let someLongTime = TimeSpan.FromSeconds 1.0
 
-        let externalCancellationSource = new CancellationTokenSource()
+        let externalCancellationSource = CustomCancelSource()
 
         let mutable longFuncFinishedExecution = false
         let job1 =

src/GWallet.Backend/Account.fs
@@ -10,7 +10,7 @@ module Account =
 
     let private GetShowableBalanceInternal (account: IAccount)
                                            (mode: ServerSelectionMode)
-                                           (cancelSourceOption: Option<CancellationTokenSource>)
+                                           (cancelSourceOption: Option<CustomCancelSource>)
                                                : Async<Option<decimal>> =
         match account with
         | :? UtxoCoin.IUtxoAccount as utxoAccount ->
@@ -26,7 +26,7 @@ module Account =
 
     let GetShowableBalance (account: IAccount)
                            (mode: ServerSelectionMode)
-                           (cancelSourceOption: Option<CancellationTokenSource>)
+                           (cancelSourceOption: Option<CustomCancelSource>)
                                : Async<MaybeCached<decimal>> =
         async {
             if Config.NoNetworkBalanceForDebuggingPurposes then
@@ -98,7 +98,7 @@ module Account =
                 EtherPublicAddress = etherPublicAddress
             }
 
-    let GetArchivedAccountsWithPositiveBalance (cancelSourceOption: Option<CancellationTokenSource>)
+    let GetArchivedAccountsWithPositiveBalance (cancelSourceOption: Option<CustomCancelSource>)
                                                    : Async<seq<ArchivedAccount*decimal>> =
         let asyncJobs = seq<Async<ArchivedAccount*Option<decimal>>> {
             let allCurrencies = Currency.GetAll()

src/GWallet.Backend/Ether/EtherAccount.fs
@@ -38,7 +38,7 @@ module internal Account =
     let private GetBalance (account: IAccount)
                            (mode: ServerSelectionMode)
                            (balType: BalanceType)
-                           (cancelSourceOption: Option<CancellationTokenSource>)
+                           (cancelSourceOption: Option<CustomCancelSource>)
                                = async {
         let! balance =
             if (account.Currency.IsEther()) then
@@ -53,7 +53,7 @@ module internal Account =
     let private GetBalanceFromServer (account: IAccount)
                                      (balType: BalanceType)
                                      (mode: ServerSelectionMode)
-                                     (cancelSourceOption: Option<CancellationTokenSource>)
+                                     (cancelSourceOption: Option<CustomCancelSource>)
                                          : Async<Option<decimal>> =
         async {
             try
@@ -66,7 +66,7 @@ module internal Account =
 
     let internal GetShowableBalance (account: IAccount)
                                     (mode: ServerSelectionMode)
-                                    (cancelSourceOption: Option<CancellationTokenSource>)
+                                    (cancelSourceOption: Option<CustomCancelSource>)
                                         : Async<Option<decimal>> =
         let getBalanceWithoutCaching(maybeUnconfirmedBalanceTaskAlreadyStarted: Option<Task<Option<decimal>>>)
                 : Async<Option<decimal>> =

src/GWallet.Backend/Ether/EtherServer.fs
@@ -471,7 +471,7 @@ module Server =
                         (address: string)
                         (balType: BalanceType)
                         (mode: ServerSelectionMode)
-                        (cancelSourceOption: Option<CancellationTokenSource>)
+                        (cancelSourceOption: Option<CustomCancelSource>)
                                      : Async<decimal> =
         async {
             let web3Funcs =
@@ -531,7 +531,7 @@ module Server =
                         (address: string)
                         (balType: BalanceType)
                         (mode: ServerSelectionMode)
-                        (cancelSourceOption: Option<CancellationTokenSource>)
+                        (cancelSourceOption: Option<CustomCancelSource>)
                             : Async<decimal> =
         async {
             let web3Funcs =

src/GWallet.Backend/UtxoCoin/UtxoCoinAccount.fs
@@ -194,7 +194,7 @@ module Account =
 
     let private GetBalances (account: IUtxoAccount)
                             (mode: ServerSelectionMode)
-                            (cancelSourceOption: Option<CancellationTokenSource>)
+                            (cancelSourceOption: Option<CustomCancelSource>)
                                 : Async<BlockchainScripthahsGetBalanceInnerResult> =
         let scriptHashHex = GetElectrumScriptHashFromPublicAddress account.Currency account.PublicAddress
 
@@ -213,7 +213,7 @@ module Account =
 
     let private GetBalancesFromServer (account: IUtxoAccount)
                                       (mode: ServerSelectionMode)
-                                      (cancelSourceOption: Option<CancellationTokenSource>)
+                                      (cancelSourceOption: Option<CustomCancelSource>)
                                          : Async<Option<BlockchainScripthahsGetBalanceInnerResult>> =
         async {
             try
@@ -226,7 +226,7 @@ module Account =
 
     let internal GetShowableBalance (account: IUtxoAccount)
                                     (mode: ServerSelectionMode)
-                                    (cancelSourceOption: Option<CancellationTokenSource>)
+                                    (cancelSourceOption: Option<CustomCancelSource>)
                                         : Async<Option<decimal>> =
         async {
             let! maybeBalances = GetBalancesFromServer account mode cancelSourceOption

commit b1b4088bb79c8fa73c3d21b56c91e8f2a94166bd

Backend: finish un-breaking some async-cancellation unit tests
They were also broken in 54b2a367a114856d5bdcbfef344c94cf39c58d78

src/GWallet.Backend.Tests/AsyncCancellation.fs
@@ -161,13 +161,13 @@ type FaultTolerantParallelClientAsyncCancellation() =
         // to make sure the exception happened
         Assert.That(result.IsNone, Is.True)
 
-(*
+
     [<Test>]
     member __.``external cancellation source can come already disposed``() =
         let someLongTime = TimeSpan.FromSeconds 1.0
 
         let externalCancellationSource = new CustomCancelSource()
-        externalCancellationSource.Dispose()
+        (externalCancellationSource:>IDisposable).Dispose()
 
         let job1 = async {
             return 1
@@ -198,14 +198,13 @@ type FaultTolerantParallelClientAsyncCancellation() =
 
             with
             | ex ->
-                let resException = FSharpUtil.FindException<ResourceUnavailabilityException> ex
+                let resException = FSharpUtil.FindException<TaskCanceledException> ex
                 let exDetails = ex.ToString()
                 Assert.That(resException.IsSome, Is.True, exDetails)
                 None
 
         // to make sure the exception happened
         Assert.That(result.IsNone, Is.True)
-        *)
 
 
     [<Test>]
@@ -214,7 +213,7 @@ type FaultTolerantParallelClientAsyncCancellation() =
 
         let externalCancellationSource = new CustomCancelSource()
         externalCancellationSource.Cancel()
-        //externalCancellationSource.Dispose()
+        (externalCancellationSource:>IDisposable).Dispose()
 
         let job1 = async {
             return 1
@@ -245,7 +244,7 @@ type FaultTolerantParallelClientAsyncCancellation() =
 
             with
             | ex ->
-                let resException = FSharpUtil.FindException<ResourceUnavailabilityException> ex
+                let resException = FSharpUtil.FindException<TaskCanceledException> ex
                 let exDetails = ex.ToString()
                 Assert.That(resException.IsSome, Is.True, exDetails)
                 None