9085957fb50931325d4f8f1b5bcc3e68bc8b6f17
Andres G. Aragoneses <knocte@gmail.com>
2019-12-11 09:52:20 +0800

    Frontend.XF: remove another XamarinForms workaround (for macOS)
    Now that we switched to Xamarin.Forms 4.x, this seems to be fixed.

--- src/GWallet.Frontend.XF/FrontendHelpers.fs
+++ src/GWallet.Frontend.XF/FrontendHelpers.fs
@@ -251,14 +251,9 @@ module FrontendHelpers =
         Device.BeginInvokeOnMainThread(fun _ ->
             NavigationPage.SetHasNavigationBar(newPage, false)
 
-            //workaround for https://github.com/xamarin/Xamarin.Forms/issues/4030 FIXME: remove it when bug is fixed
-            if Device.RuntimePlatform = Device.macOS then
-                currentPage.Navigation.PushAsync newPage
-                    |> DoubleCheckCompletionNonGeneric
-            else
-                currentPage.Navigation.InsertPageBefore(newPage, currentPage)
-                currentPage.Navigation.PopAsync()
-                    |> DoubleCheckCompletion
+            currentPage.Navigation.InsertPageBefore(newPage, currentPage)
+            currentPage.Navigation.PopAsync()
+                |> DoubleCheckCompletion
         )
 
     let ChangeTextAndChangeBack (button: Button) (newText: string) =
