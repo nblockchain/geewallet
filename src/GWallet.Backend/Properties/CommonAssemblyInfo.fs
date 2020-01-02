405a250b07ffaf8e94f6ee37bff61fffda191c6c
Andres G. Aragoneses <knocte@gmail.com>
2020-01-02 23:04:21 +0800

    (Post)Bump version: 0.3.2.6 -> 0.3.2.7

--- src/GWallet.Backend/Properties/CommonAssemblyInfo.fs
+++ src/GWallet.Backend/Properties/CommonAssemblyInfo.fs
@@ -18,8 +18,8 @@ open System.Reflection
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
 
-[<assembly: AssemblyVersion("0.3.2.6")>]
-[<assembly: AssemblyFileVersion("0.3.2.6")>]
+[<assembly: AssemblyVersion("0.3.2.7")>]
+[<assembly: AssemblyFileVersion("0.3.2.7")>]
 
 do
     ()
