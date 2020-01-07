844af8c3a3dac0c70829d0707a7019593fc374d6
Andres G. Aragoneses <knocte@gmail.com>
2020-01-08 01:10:48 +0800

    configure.sh: fix "$1: unbound variable" error if F#/fsx is not present
    I guess I made this mistake in 0326aa3564edd4c5a3d14ae6c09c0dbde3253f48

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ if ! which fsharpi >/dev/null 2>&1; then
     if ! which fsx >/dev/null 2>&1; then
         echo "$FSX_CHECK_MSG" $'not found\n'
 
-        echo "$1" $'failed, please install "fsharpi" or "fsx" first'
+        echo "$0" $'failed, please install "fsharpi" or "fsx" first'
         exit 1
     else
         echo "$FSX_CHECK_MSG" $'found'
@@ -23,7 +23,7 @@ else
         if ! which fsx >/dev/null 2>&1; then
             echo "$FSX_CHECK_MSG" $'not found\n'
 
-            echo "$1" $'failed, please install "fsx" first'
+            echo "$0" $'failed, please install "fsx" first'
             exit 1
         else
             echo "$FSX_CHECK_MSG" $'found'
