e56bd4a5728472dbba37278de97fd2797f6134d1
Andres G. Aragoneses <knocte@gmail.com>
2020-01-11 01:48:09 +0800

    scripts: make mono-stable installation cross-distro

--- scripts/install_mono_from_microsoft_deb_packages.sh
+++ scripts/install_mono_from_microsoft_deb_packages.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+source /etc/os-release
+
 # required by apt-key
 apt install -y gnupg2
 # required by apt-update when pulling from mono-project.com
@@ -8,7 +10,7 @@ apt install -y ca-certificates
 
 # taken from http://www.mono-project.com/download/stable/#download-lin
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb https://download.mono-project.com/repo/ubuntu stable-$UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 apt update
 apt install -y mono-devel fsharp
 mono --version
