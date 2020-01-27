nblockchain/geewallet

commit 06a87500bee676bf8b805356194578933a49b436

Backend: upgrade JsonRpcSharp dependency (with overhauled TcpClient)
The old JsonRpcSharp.TcpClient was either hanging or taking ages to
query certain Stratum operations such as the ones needed to estimate
fees. With this new version, even if it's not cancellable-friendly
yet, it could solve the hangs or long-waits seen in Android..

This also should remove the need for horrible workarounds like
the "Flaky*" ones I'm removing here.

src/GWallet.Backend/Config.fs
@@ -73,9 +73,7 @@ module Config =
 
     // TODO: make the tests instantiate Legacy or nonLegacyTcpClient themselves and test both from them
     let NewUtxoTcpClientDisabled =
-
-        //in macOS, even if using Mono >5.18.0.240, we still get the same issue of receiving an empty string from the
-        //"blockchain.scripthash.listunspent" stratum API, like: https://gitlab.com/DiginexGlobal/geewallet/issues/54
+        //in macOS, even if using Mono >5.18.0.240 (e.g. 6.6.0.155 at the time of writing), we still get hangs
         IsMacPlatform() ||
 
         //we need this check because older versions of Mono (such as 5.16, or Ubuntu 18.04 LTS's version: 4.6.2)

src/GWallet.Backend/JsonRpcSharp.fs
@@ -10,6 +10,7 @@ open System.Threading
 module JsonRpcSharpOld =
 
     exception ServerUnresponsiveException
+    exception NoResponseReceivedAfterRequestException
 
     type LegacyTcpClient  (resolveHostAsync: unit->Async<IPAddress>, port: uint32) =
         let rec WrapResult (acc: List<byte>): string =
@@ -30,7 +31,7 @@ module JsonRpcSharpOld =
             let timeIsUp (): bool =
                 if (List.Empty = acc) then
                     if (DateTime.UtcNow > initTime + DEFAULT_TIMEOUT_FOR_FIRST_DATA_AVAILABLE_SIGNAL_TO_HAPPEN) then
-                        raise JsonRpcSharp.TcpClient.NoResponseReceivedAfterRequestException
+                        raise NoResponseReceivedAfterRequestException
                     else
                         false
                 else

src/GWallet.Backend/JsonRpcTcpClient.fs
@@ -13,6 +13,9 @@ type ServerCannotBeResolvedException =
     new(message) = { inherit CommunicationUnsuccessfulException(message) }
     new(message:string, innerException: Exception) = { inherit CommunicationUnsuccessfulException(message, innerException) }
 
+type NonAbstractTcpClient(resolveHostJob, port, timeout) =
+    inherit JsonRpcSharp.TcpClient.JsonRpcClient(resolveHostJob, port, timeout)
+
 type JsonRpcTcpClient (host: string, port: uint32) =
 
     let ResolveAsync (hostName: string): Async<Option<IPAddress>> = async {
@@ -53,8 +56,8 @@ type JsonRpcTcpClient (host: string, port: uint32) =
             let tcpClient = JsonRpcSharpOld.LegacyTcpClient(ResolveHost, port)
             tcpClient.Request
         else
-            let tcpClient = JsonRpcSharp.TcpClient.TcpClient(ResolveHost, int port)
-            fun jsonRequest -> tcpClient.Request jsonRequest
+            let tcpClient = NonAbstractTcpClient(ResolveHost, int port, Config.DEFAULT_NETWORK_TIMEOUT)
+            fun jsonRequest -> tcpClient.RequestAsync jsonRequest
 
     member __.Host with get() = host
 
@@ -69,9 +72,12 @@ type JsonRpcTcpClient (host: string, port: uint32) =
         with
         | :? CommunicationUnsuccessfulException as ex ->
             return raise <| FSharpUtil.ReRaise ex
-        | :? JsonRpcSharpOld.ServerUnresponsiveException as ex ->
+        | :? JsonRpcSharp.TcpClient.CommunicationUnsuccessfulException as ex ->
+            return raise <| CommunicationUnsuccessfulException(ex.Message, ex)
+
+        | :? JsonRpcSharpOld.NoResponseReceivedAfterRequestException as ex ->
             return raise <| ServerTimedOutException(exceptionMsg, ex)
-        | :? JsonRpcSharp.TcpClient.NoResponseReceivedAfterRequestException as ex ->
+        | :? JsonRpcSharpOld.ServerUnresponsiveException as ex ->
             return raise <| ServerTimedOutException(exceptionMsg, ex)
 
         // FIXME: we should log this one on Sentry as a warning because it's really strange, I bet it's a bug

src/GWallet.Backend/GWallet.Backend.fsproj
@@ -201,7 +201,7 @@
       <HintPath>..\..\packages\Microsoft.Extensions.Logging.Abstractions.1.0.2\lib\netstandard1.1\Microsoft.Extensions.Logging.Abstractions.dll</HintPath>
     </Reference>
     <Reference Include="JsonRpcSharp.TcpClient">
-      <HintPath>..\..\packages\JsonRpcSharp.0.10.0-date20191211-1221.git-469146b\lib\net46\JsonRpcSharp.TcpClient.dll</HintPath>
+      <HintPath>..\..\packages\JsonRpcSharp.0.90.0-date20200125-1141.git-db16aef\lib\net46\JsonRpcSharp.TcpClient.dll</HintPath>
     </Reference>
     <Reference Include="System.IO.Pipes">
       <HintPath>..\..\packages\System.IO.Pipes.4.3.0\lib\net46\System.IO.Pipes.dll</HintPath>
@@ -213,49 +213,49 @@
       <HintPath>..\..\packages\System.Net.WebSockets.Client.4.3.2\lib\net46\System.Net.WebSockets.Client.dll</HintPath>
     </Reference>
     <Reference Include="JsonRpcSharp.Client">
-      <HintPath>..\..\packages\JsonRpcSharp.0.10.0-date20191211-1221.git-469146b\lib\net46\JsonRpcSharp.Client.dll</HintPath>
+      <HintPath>..\..\packages\JsonRpcSharp.0.90.0-date20200125-1141.git-db16aef\lib\net46\JsonRpcSharp.Client.dll</HintPath>
     </Reference>
     <Reference Include="JsonRpcSharp.IpcClient">
-      <HintPath>..\..\packages\JsonRpcSharp.0.10.0-date20191211-1221.git-469146b\lib\net46\JsonRpcSharp.IpcClient.dll</HintPath>
+      <HintPath>..\..\packages\JsonRpcSharp.0.90.0-date20200125-1141.git-db16aef\lib\net46\JsonRpcSharp.IpcClient.dll</HintPath>
     </Reference>
     <Reference Include="JsonRpcSharp.RpcClient">
-      <HintPath>..\..\packages\JsonRpcSharp.0.10.0-date20191211-1221.git-469146b\lib\net46\JsonRpcSharp.RpcClient.dll</HintPath>
+      <HintPath>..\..\packages\JsonRpcSharp.0.90.0-date20200125-1141.git-db16aef\lib\net46\JsonRpcSharp.RpcClient.dll</HintPath>
     </Reference>
     <Reference Include="JsonRpcSharp.WebSocketClient">
-      <HintPath>..\..\packages\JsonRpcSharp.0.10.0-date20191211-1221.git-469146b\lib\net46\JsonRpcSharp.WebSocketClient.dll</HintPath>
+      <HintPath>..\..\packages\JsonRpcSharp.0.90.0-date20200125-1141.git-db16aef\lib\net46\JsonRpcSharp.WebSocketClient.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.ABI">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.ABI.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.ABI.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.Accounts">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.Accounts.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.Accounts.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.Contracts">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.Contracts.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.Contracts.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.Hex">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.Hex.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.Hex.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.KeyStore">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.KeyStore.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.KeyStore.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.RLP">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.RLP.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.RLP.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.RPC">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.RPC.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.RPC.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.Signer">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.Signer.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.Signer.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.StandardTokenEIP20">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.StandardTokenEIP20.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.StandardTokenEIP20.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.Util">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.Util.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.Util.dll</HintPath>
     </Reference>
     <Reference Include="Nethereum.Web3">
-      <HintPath>..\..\packages\Nethereum.0.10.0-date20191211-1241.git-be6d117\lib\net46\Nethereum.Web3.dll</HintPath>
+      <HintPath>..\..\packages\Nethereum.0.10.0-date20200125-1201.git-5da787e\lib\net46\Nethereum.Web3.dll</HintPath>
     </Reference>
     <Reference Include="HtmlAgilityPack">
       <HintPath>..\..\packages\HtmlAgilityPack.1.11.9\lib\Net45\HtmlAgilityPack.dll</HintPath>

src/GWallet.Backend/UtxoCoin/StratumClient.fs
@@ -107,10 +107,6 @@ type public ElectrumServerReturningInternalErrorException(message: string, code:
                                                           originalRequest: string, originalResponse: string) =
     inherit ElectrumServerReturningErrorException(message, code, originalRequest, originalResponse)
 
-// TODO: send a warning to sentry when this exception happens
-type FlakyJsonRpcSharpClientException (message: string) =
-    inherit CommunicationUnsuccessfulException(message)
-
 type StratumClient (jsonRpcClient: JsonRpcTcpClient) =
 
     let Serialize(req: Request): string =
@@ -121,14 +117,10 @@ type StratumClient (jsonRpcClient: JsonRpcTcpClient) =
     member private self.Request<'R> (jsonRequest: string): Async<'R*string> = async {
         let! rawResponse = jsonRpcClient.Request jsonRequest
 
-
         // FIXME: we should actually fix this bug in JsonRpcSharp (https://github.com/nblockchain/JsonRpcSharp/issues/9)
         if String.IsNullOrEmpty rawResponse then
-            return raise <|
-                FlakyJsonRpcSharpClientException(
-                    sprintf "Server '%s' returned a null/empty JSON response to the request '%s'"
-                            jsonRpcClient.Host jsonRequest)
-
+            return failwithf "Server '%s' returned a null/empty JSON response to the request '%s'??"
+                             jsonRpcClient.Host jsonRequest
 
         try
             return (StratumClient.Deserialize<'R> rawResponse, rawResponse)
@@ -210,15 +202,10 @@ type StratumClient (jsonRpcClient: JsonRpcTcpClient) =
         let! resObj, rawResponse = self.Request<ServerVersionResult> json
 
         if Object.ReferenceEquals (resObj, null) then
-            failwithf "resObj is null? raw response was %s" rawResponse
+            failwithf "resObj is null?? raw response was %s" rawResponse
 
         if Object.ReferenceEquals (resObj.Result, null) then
-            if rawResponse.Replace("\r\n", "\n").Replace("\n", " ").Trim() =
-                      json.Replace("\r\n", "\n").Replace("\n", " ").Trim() then
-                // FIXME: remove this HACK when we upgrade JsonRpcSharp with this work: https://gitlab.com/nblockchain/TcpEchoSharp/merge_requests/1
-                raise <| FlakyJsonRpcSharpClientException("resObj.Result was null because the response was the request(!?)")
-            else
-                failwithf "resObj.Result is null? raw response was %s" rawResponse
+            failwithf "resObj.Result is null?? raw response was %s" rawResponse
 
         // resObj.Result.[0] is e.g. "ElectrumX 1.4.3"
         // e.g. "1.1"

src/GWallet.Backend/packages.config
@@ -5,14 +5,14 @@
   <package id="FSharp.Core" version="4.7.0" targetFramework="net461" />
   <package id="FSharp.Data" version="3.0.0" targetFramework="net46" />
   <package id="HtmlAgilityPack" version="1.11.9" targetFramework="net46" />
-  <package id="JsonRpcSharp" version="0.10.0-date20191211-1221.git-469146b" targetFramework="net461" />
+  <package id="JsonRpcSharp" version="0.90.0-date20200125-1141.git-db16aef" targetFramework="net461" />
   <package id="Microsoft.CSharp" version="4.0.1" targetFramework="net46" />
   <package id="Microsoft.Extensions.Logging.Abstractions" version="1.0.2" targetFramework="net46" />
   <package id="Microsoft.NETCore.Platforms" version="1.1.0" targetFramework="net46" />
   <package id="Microsoft.Win32.Primitives" version="4.3.0" targetFramework="net46" />
   <package id="NBitcoin" version="4.2.8" targetFramework="net461" />
   <package id="NBitcoin.Altcoins" version="1.0.3" targetFramework="net452" />
-  <package id="Nethereum" version="0.10.0-date20191211-1241.git-be6d117" targetFramework="net461" />
+  <package id="Nethereum" version="0.10.0-date20200125-1201.git-5da787e" targetFramework="net461" />
   <package id="NETStandard.Library" version="1.6.1" targetFramework="net46" />
   <package id="Newtonsoft.Json" version="11.0.2" targetFramework="net46" />
   <package id="SharpRaven" version="2.4.0" targetFramework="net46" />

commit d5be98787ee26b13feffc3ffcfef5b331fcc31cb

Backend: fix async/socket hangs
After much testing, I realised when were the hangs really happening
(that were introduced in 06a87500bee676bf8b805356194578933a49b436):

-> FSharpUtil.WithTimeout discarding an async job which underneath:
-> was within an Async.Sleep call that somehow seemed to never finish.

I still don't understand how FSharpUtil.WithTimeout, coded the way
it was (using Async.StartChild with a timeout parameter) made the
code hang (will probably keep investigating), but I found that replacing
it with an implementation that uses Async.Choice instead makes the
hang go away. I also found out that my implementation of the helper
AsyncExtensions.WhenAny suffered a similar problem so I changed it too
to use Async.Choice.

Even if I hadn't found that the problem with Async.Choice doesn't happen,
the timeouts needed changing because we were passing a long-style timeout
setting (30secs) to the lower-level code that deals with sockets. So then
the upper-layer (or lower-caller down the stack) was "killing" threads
(with WithTimeout) that could be still in the process of communicating
properly (or that should have been killed/discarded by the lower-level
code itself earlier). We can even see that in the old legacy JsonRpcShap
client we had different levels of timeouts, so I introduced a new global
Config timeout in this commit and reused it in this legacy client too, to
make sure what I mean is clear.

This should eliminate all problems we were having on macOS with the async
JsonRpcSharp, plus I made the GetMonoVersion() logic more readable, to
make sure we're definitely not using the legacyUtxo client in Windows
either.

src/GWallet.Backend/Config.fs
@@ -72,17 +72,17 @@ module Config =
                 simpleVersion |> Some
 
     // TODO: make the tests instantiate Legacy or nonLegacyTcpClient themselves and test both from them
-    let NewUtxoTcpClientDisabled =
-        //in macOS, even if using Mono >5.18.0.240 (e.g. 6.6.0.155 at the time of writing), we still get hangs
-        IsMacPlatform() ||
-
+    let LegacyUtxoTcpClientEnabled =
         //we need this check because older versions of Mono (such as 5.16, or Ubuntu 18.04 LTS's version: 4.6.2)
         //don't work with the new TCP client, only the legacy one works
-        (Option.exists (fun monoVersion -> monoVersion < Version("5.18.0.240")) (GetMonoVersion()))
+        match GetMonoVersion() with
+        | None -> false
+        | Some monoVersion -> monoVersion < Version("5.18.0.240")
 
     // FIXME: make FaultTolerantParallelClient accept funcs that receive this as an arg, maybe 2x-ing it when a full
     //        round of failures has happened, as in, all servers failed
     let internal DEFAULT_NETWORK_TIMEOUT = TimeSpan.FromSeconds 30.0
+    let internal DEFAULT_NETWORK_CONNECT_TIMEOUT = TimeSpan.FromSeconds 5.0
 
     let internal NUMBER_OF_RETRIES_TO_SAME_SERVERS = 3u
 

src/GWallet.Backend/FSharpUtil.fs
@@ -5,6 +5,10 @@ open System.Threading.Tasks
 open System.Runtime.ExceptionServices
 open Microsoft.FSharp.Reflection
 
+type Result<'Val, 'Err when 'Err :> Exception> =
+    | Error of 'Err
+    | Value of 'Val
+
 module FSharpUtil =
 
     type internal ResultWrapper<'T>(value : 'T) =
@@ -53,25 +57,20 @@ module FSharpUtil =
 
         // like Async.Choice, but with no need for Option<T> types
         let WhenAny<'T>(jobs: seq<Async<'T>>): Async<'T> =
-            let wrap job =
+            let wrap (job: Async<'T>): Async<Option<'T>> =
                 async {
                     let! res = job
-                    return! RaiseResult <| ResultWrapper res
+                    return Some res
                 }
 
             async {
-                try
-                    do!
-                        jobs
-                        |> Seq.map wrap
-                        |> Async.Parallel
-                        |> Async.Ignore
-
-                    // unreachable
-                    return failwith "No successful result?"
-                with
-                | :? ResultWrapper<'T> as ex ->
-                    return ex.Value
+                let wrappedJobs = jobs |> Seq.map wrap
+                let! combinedRes = Async.Choice wrappedJobs
+                match combinedRes with
+                | Some x ->
+                    return x
+                | None ->
+                    return failwith "unreachable"
             }
 
         // a mix between Async.WhenAny and Async.Choice
@@ -108,13 +107,29 @@ module FSharpUtil =
     let ListIntersect<'T> (list1: List<'T>) (list2: List<'T>) (offset: uint32): List<'T> =
         ListIntersectInternal list1 list2 offset [] 1
 
-    let WithTimeout (timeSpan: TimeSpan) (operation: Async<'R>): Async<Option<'R>> = async {
-        let! child = Async.StartChild (operation, int timeSpan.TotalMilliseconds)
-        try
-            let! result = child
-            return Some result
-        with :? TimeoutException ->
-            return None
+    let WithTimeout (timeSpan: TimeSpan) (job: Async<'R>): Async<Option<'R>> = async {
+        let read = async {
+            let! value = job
+            return value |> Value |> Some
+        }
+
+        let delay = async {
+            let total = int timeSpan.TotalMilliseconds
+            do! Async.Sleep total
+            return Some (Error(TimeoutException()))
+        }
+
+        let! dummyOption = Async.Choice([read; delay])
+        match dummyOption with
+        | Some theResult ->
+            match theResult with
+            | Value r ->
+                return Some r
+            | Error _ ->
+                return None
+        | None ->
+            // none of the jobs passed to Async.Choice returns None
+            return failwith "unreachable"
     }
 
     // FIXME: we should not need this workaround anymore when this gets addressed:

src/GWallet.Backend/FaultTolerantParallelClient.fs
@@ -81,10 +81,6 @@ type FaultTolerantParallelClientSettings<'R> =
         ExceptionHandler: Option<Exception->unit>
     }
 
-type Result<'Val, 'Err when 'Err :> Exception> =
-    | Error of 'Err
-    | Value of 'Val
-
 type MutableStateUnsafeAccessor<'T>(initialState: 'T) =
     let mutable state = initialState
     member this.Value

src/GWallet.Backend/JsonRpcSharp.fs
@@ -26,7 +26,7 @@ module JsonRpcSharpOld =
 
         let DEFAULT_TIMEOUT_FOR_FIRST_DATA_AVAILABLE_SIGNAL_TO_HAPPEN = Config.DEFAULT_NETWORK_TIMEOUT
         let DEFAULT_TIMEOUT_FOR_SUBSEQUENT_DATA_AVAILABLE_SIGNAL_TO_HAPPEN = TimeSpan.FromMilliseconds(500.0)
-        let DEFAULT_TIME_TO_WAIT_BETWEEN_DATA_GAPS = TimeSpan.FromMilliseconds(1.0)
+        let DEFAULT_TIME_TO_WAIT_BETWEEN_DATA_GAPS = Config.DEFAULT_NETWORK_CONNECT_TIMEOUT
         let rec ReadInternal (stream: NetworkStream) acc (initTime: DateTime): string =
             let timeIsUp (): bool =
                 if (List.Empty = acc) then

src/GWallet.Backend/JsonRpcTcpClient.fs
@@ -52,11 +52,11 @@ type JsonRpcTcpClient (host: string, port: uint32) =
     }
 
     let rpcTcpClientInnerRequest =
-        if Config.NewUtxoTcpClientDisabled then
+        if Config.LegacyUtxoTcpClientEnabled then
             let tcpClient = JsonRpcSharpOld.LegacyTcpClient(ResolveHost, port)
             tcpClient.Request
         else
-            let tcpClient = NonAbstractTcpClient(ResolveHost, int port, Config.DEFAULT_NETWORK_TIMEOUT)
+            let tcpClient = NonAbstractTcpClient(ResolveHost, int port, Config.DEFAULT_NETWORK_CONNECT_TIMEOUT)
             fun jsonRequest -> tcpClient.RequestAsync jsonRequest
 
     member __.Host with get() = host

src/GWallet.Backend/UtxoCoin/UtxoCoinServer.fs
@@ -37,7 +37,7 @@ module Server =
                     {
                         ServerSelectionMode = mode
                         ConsistencyConfig = consistencyConfig
-                        ReportUncanceledJobs = (not Config.NewUtxoTcpClientDisabled)
+                        ReportUncanceledJobs = (not Config.LegacyUtxoTcpClientEnabled)
                     }
         }
 

src/GWallet.Backend.Tests/ElectrumIntegrationTests.fs
@@ -175,7 +175,7 @@ type ElectrumIntegrationTests() =
     member __.``configuration is correct for this client``() =
         let shouldUseLegacyClient =
             if Config.IsMacPlatform() then
-                true
+                false
             elif Environment.OSVersion.Platform = PlatformID.Unix then
                 match Config.GetMonoVersion() with
                 | None ->
@@ -189,7 +189,7 @@ type ElectrumIntegrationTests() =
             else
                 false
 
-        Assert.That(Config.NewUtxoTcpClientDisabled, Is.EqualTo shouldUseLegacyClient)
+        Assert.That(Config.LegacyUtxoTcpClientEnabled, Is.EqualTo shouldUseLegacyClient)
 
     [<Test>]
     member __.``can connect (just check balance) to some electrum BTC servers``() =