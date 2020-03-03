5af7a4023a6321ae4e95bee20f3a77ea4547e337
Andres G. Aragoneses <knocte@gmail.com>
2020-03-04 01:04:47 +0800

    Frontend.XF: forced shutdown in case "main-throwing" didn't crash
    Because it seems the Async.Start approach didn't work:
    c80e2440a462989deb980df50cd80edfe7821711

--- src/GWallet.Frontend.XF/FrontendHelpers.fs
+++ src/GWallet.Frontend.XF/FrontendHelpers.fs
@@ -232,7 +232,16 @@ module FrontendHelpers =
             if shouldCrash then
                 Device.BeginInvokeOnMainThread(fun _ ->
                     raise ex
+                    // this is just in case the raise(throw) doesn't really tear down the program:
+                    Console.Error.WriteLine("FATAL PROBLEM: " + ex.ToString())
+                    Console.Error.WriteLine "MANUAL FORCED SHUTDOWN NOW"
+                    Device.PlatformServices.QuitApplication()
                 )
+                raise ex
+                // this is just in case the raise(throw) doesn't really tear down the program:
+                Console.Error.WriteLine("FATAL PROBLEM: " + ex.ToString())
+                Console.Error.WriteLine "MANUAL FORCED SHUTDOWN NOW"
+                Device.PlatformServices.QuitApplication()
 
     // when running Task<unit> or Task<T> where we want to ignore the T, we should still make sure there is no exception,
     // & if there is, bring it to the main thread to fail fast, report to Sentry, etc, otherwise it gets ignored
