f6eb26c544b2bc9935e195e5118b67ea083aaf1a
Andres G. Aragoneses <knocte@gmail.com>
2020-06-02 00:13:23 +0800

    Backend/Ether: catch/retry yet another possible -32000 error
    https://sentry.io/organizations/nblockchain/issues/1699499127/

--- src/GWallet.Backend/Ether/EtherServer.fs
+++ src/GWallet.Backend/Ether/EtherServer.fs
@@ -171,12 +171,15 @@ module Server =
         match maybeRpcResponseEx with
         | Some rpcResponseEx ->
             if rpcResponseEx.RpcError <> null then
-                if (rpcResponseEx.RpcError.Code = int RpcErrorCode.StatePruningNodeOrMissingTrieNode) then
+                if rpcResponseEx.RpcError.Code = int RpcErrorCode.StatePruningNodeOrMissingTrieNodeOrHeaderNotFound then
                     if (not (rpcResponseEx.RpcError.Message.Contains "pruning=archive")) &&
+                       (not (rpcResponseEx.RpcError.Message.Contains "header not found")) &&
                        (not (rpcResponseEx.RpcError.Message.Contains "missing trie node")) then
                         raise <| Exception(
-                                     SPrintF1 "Expecting 'pruning=archive' or 'missing trie node' in message of a %i code"
-                                                   (int RpcErrorCode.StatePruningNodeOrMissingTrieNode), rpcResponseEx)
+                                     SPrintF2 "Expecting 'pruning=archive' or 'missing trie node' or 'header not found' in message of a %d code, but got '%s'"
+                                             (int RpcErrorCode.StatePruningNodeOrMissingTrieNodeOrHeaderNotFound)
+                                             rpcResponseEx.RpcError.Message,
+                                     rpcResponseEx)
                     else
                         raise <| ServerMisconfiguredException(exMsg, rpcResponseEx)
                 if (rpcResponseEx.RpcError.Code = int RpcErrorCode.UnknownBlockNumber) then
