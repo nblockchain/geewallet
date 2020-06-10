91883ddaab61301236fc62397f2ddd88fb7a9316
Andres G. Aragoneses <knocte@gmail.com>
2020-06-10 23:57:55 +0800

    Try using a revision different than 0 to try to fix stable snap submit
    Mmm, seems we're gonna need to keep different version numbers for UWP and
    the SnapStore? (See eda05c3a4e2dbe55f5a6d7c66d58048ad052a00a )
    
    It threw the error below for version 0.2.10.0:
    
    Processing...\
    Processing...|
    Traceback (most recent call last):
      File "/snap/snapcraft/4862/bin/snapcraft", line 11, in <module>
        load_entry_point('snapcraft==4.0.4', 'console_scripts', 'snapcraft')()
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/click/core.py", line 829, in __call__
        return self.main(*args, **kwargs)
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/click/core.py", line 782, in main
        rv = self.invoke(ctx)
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/click/core.py", line 1259, in invoke
        return _process_result(sub_ctx.command.invoke(sub_ctx))
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/click/core.py", line 1066, in invoke
        return ctx.invoke(self.callback, **ctx.params)
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/click/core.py", line 610, in invoke
        return callback(*args, **kwargs)
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/snapcraft/cli/store.py", line 198, in upload
        snap_revision
      File "/snap/snapcraft/4862/lib/python3.6/site-packages/snapcraft/storeapi/v2/channel_map.py", line 276, in get_revision
        raise ValueError(f"No revision information for {revision_number!r}")
    ValueError: No revision information for 14
    You can find the traceback in file '/tmp/tmpasfpfwh2/trace.txt'.

--- src/GWallet.Backend/Properties/CommonAssemblyInfo.fs
+++ src/GWallet.Backend/Properties/CommonAssemblyInfo.fs
@@ -18,8 +18,8 @@ open System.Reflection
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
 
-[<assembly: AssemblyVersion("0.2.11.0")>]
-[<assembly: AssemblyFileVersion("0.2.11.0")>]
+[<assembly: AssemblyVersion("0.2.11.1")>]
+[<assembly: AssemblyFileVersion("0.2.11.1")>]
 
 do
     ()
