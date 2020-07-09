04c97c36d8a3c729144a8743227bd4107dc2ca2a
Andrew Cann <shum@canndrew.org>
2020-07-09 18:52:53 +0800

    Add regtest geewallet<->lnd channel opening test
    Add a test which uses the regtest network to test that geewallet can
    open a channel with lnd.
    
    To run this test locally you just need to have bitcoind,
    electrumx_server (the spesmilo version, which didn't drop bitcoin
    support) and lnd in your `$PATH`. It shouldn't interfere with any local
    config for these programs.

--- scripts/install_regtest_dependencies.sh
+++ scripts/install_regtest_dependencies.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 set -eux
 
-# install bitcoin core
+# Set up variables needed for the regtest test
+
+# This is needed to find lnd in PATH
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+# This is needed to convince electrum to run on CI as root
+export ALLOW_ROOT=1
+
+
+# Install bitcoin core
 apt install -y curl tar gzip
 curl -OJL https://bitcoin.org/bin/bitcoin-core-0.20.0/bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
 tar -C /usr/local --strip-components 1 -xzvf bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
 
-# install electrumx
+# Install electrumx
 apt install -y curl unzip python3-pip
 curl -L https://github.com/spesmilo/electrumx/archive/1.15.0.zip -o electrumx-1.15.0.zip
 unzip electrumx-1.15.0.zip
 pip3 install ./electrumx-1.15.0
 
-# install lnd
-apt install -y curl tar gzip unzip make
+# Install lnd
+apt install -y curl tar gzip unzip make git
 curl -OJL https://golang.org/dl/go1.14.4.linux-amd64.tar.gz
 tar -C /usr/local -xzvf go1.14.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-curl -L https://github.com/lightningnetwork/lnd/archive/v0.10.3-beta.zip -o lnd-0.10.3-beta.zip
-unzip lnd-0.10.3-beta.zip
-pushd lnd-0.10.3-beta
+git clone https://github.com/lightningnetwork/lnd
+pushd lnd
+git checkout v0.10.3-beta
 make
 make install
 popd
