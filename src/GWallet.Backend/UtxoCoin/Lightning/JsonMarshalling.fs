905332d84112ee47c2832267414f4b3b0c0d0a69
Andrew Cann <shum@canndrew.org>
2020-07-10 14:28:05 +0800

    Add support for monohop unidirectional payments
    Co-authored-by: Andres G. Aragoneses <knocte@gmail.com>

--- src/GWallet.Backend/UtxoCoin/Lightning/JsonMarshalling.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/JsonMarshalling.fs
@@ -6,39 +6,35 @@ open System.Net
 open DotNetLightning.Serialize
 
 open GWallet.Backend
+open FSharpUtil
 
 open Newtonsoft.Json
 
-
 module JsonMarshalling =
-
-    type FeatureBitJsonConverter() =
+    type internal FeatureBitJsonConverter() =
         inherit JsonConverter<FeatureBit>()
 
-        override this.ReadJson(reader: JsonReader, _: Type, _: FeatureBit, _: bool, serializer: JsonSerializer): FeatureBit =
+        override self.ReadJson(reader: JsonReader, _: Type, _: FeatureBit, _: bool, serializer: JsonSerializer) =
             let serializedFeatureBit = serializer.Deserialize<string> reader
-            let parsed = FeatureBit.TryParse serializedFeatureBit
-            match parsed with
-            | Ok featureBit -> featureBit
-            | _ -> failwith "error decoding feature bit"
+            UnwrapResult (FeatureBit.TryParse serializedFeatureBit) "error decoding feature bit"
 
-        override this.WriteJson(writer: JsonWriter, state: FeatureBit, serializer: JsonSerializer) =
+        override self.WriteJson(writer: JsonWriter, state: FeatureBit, serializer: JsonSerializer) =
             serializer.Serialize(writer, state.ToString())
 
-    type IPAddressJsonConverter() =
+    type internal IPAddressJsonConverter() =
         inherit JsonConverter<IPAddress>()
 
-        override this.ReadJson(reader: JsonReader, _: Type, _: IPAddress, _: bool, serializer: JsonSerializer) =
+        override self.ReadJson(reader: JsonReader, _: Type, _: IPAddress, _: bool, serializer: JsonSerializer) =
             let serializedIPAddress = serializer.Deserialize<string> reader
             IPAddress.Parse serializedIPAddress
 
-        override this.WriteJson(writer: JsonWriter, state: IPAddress, serializer: JsonSerializer) =
+        override self.WriteJson(writer: JsonWriter, state: IPAddress, serializer: JsonSerializer) =
             serializer.Serialize(writer, state.ToString())
 
-    type IPEndPointJsonConverter() =
+    type internal IPEndPointJsonConverter() =
         inherit JsonConverter<IPEndPoint>()
 
-        override this.ReadJson(reader: JsonReader, _: Type, _: IPEndPoint, _: bool, serializer: JsonSerializer) =
+        override __.ReadJson(reader: JsonReader, _: Type, _: IPEndPoint, _: bool, serializer: JsonSerializer) =
             assert (reader.TokenType = JsonToken.StartArray)
             reader.Read() |> ignore
             let ip = serializer.Deserialize<IPAddress> reader
@@ -48,13 +44,13 @@ module JsonMarshalling =
             reader.Read() |> ignore
             IPEndPoint (ip, port)
 
-        override this.WriteJson(writer: JsonWriter, state: IPEndPoint, serializer: JsonSerializer) =
+        override __.WriteJson(writer: JsonWriter, state: IPEndPoint, serializer: JsonSerializer) =
             writer.WriteStartArray()
             serializer.Serialize(writer, state.Address)
             serializer.Serialize(writer, state.Port)
             writer.WriteEndArray()
 
-    let LightningSerializerSettings: JsonSerializerSettings =
+    let internal SerializerSettings: JsonSerializerSettings =
         let settings = Marshalling.DefaultSettings ()
         let ipAddressConverter = IPAddressJsonConverter()
         let ipEndPointConverter = IPEndPointJsonConverter()
