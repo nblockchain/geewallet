1f28dc63f8f92e04d2115673128f7ae7cede7ef6
Andrew Cann <shum@canndrew.org>
2020-08-24 17:34:59 +0800

    Upgrade to DNL with serializable Commitments type
    This DNL removes the method-like-non-method members of Commitments which
    breaks json serialization as well as the chain hash endianess fix.
    
    Also remove custom serialization of Commitments since this is no longer
    broken on the DNL side.

--- src/GWallet.Backend.Tests.Unit/SerializedChannel.fs
+++ src/GWallet.Backend.Tests.Unit/SerializedChannel.fs
@@ -19,7 +19,41 @@ type SerializedChannelTest() =
       "fields": [
         {
           "commitments": {
-            "channelId": "2c0e386c42c30b0c4032e8406ab632d6916636df1e5edf66eb083badda023afa",
+            "localParams": {
+              "nodeId": "02323d281f323fc7513660602736a7bcd90d9d06057c9fa9e8d2deb0de5aeb2231",
+              "channelPubKeys": {
+                "fundingPubKey": "02fdcf55c55324c7ddf78e5491a2efb2745e465180ff1c905c171cb08293644bb4",
+                "revocationBasePubKey": "03408d1c4a0db3671695bc31b8a7528bdafc91af0a6a208507ef7bcdb2133d6397",
+                "paymentBasePubKey": "021ca7cbadc640a6aba7aa62e976a53a9d79bd8181aa30700c8c486580efa3365b",
+                "delayedPaymentBasePubKey": "02a6c7282ee4c98d7caea59a78b55e5d7a8797fdec9fa97107b39e48d234a37084",
+                "htlcBasePubKey": "031f8050b624096531a0c9f3bba7e5bc7c571ad3eaba1a7587202b067d3986cd81"
+              },
+              "dustLimitSatoshis": 200,
+              "maxHTLCValueInFlightMSat": 0.0000001,
+              "channelReserveSatoshis": 2000,
+              "htlcMinimumMSat": 0.00000001,
+              "toSelfDelay": 6,
+              "maxAcceptedHTLCs": 10,
+              "isFunder": true,
+              "defaultFinalScriptPubKey": "a914ee63c76cda9f5a4928a24710e3b950f2bed0bcc787",
+              "features": "10"
+            },
+            "remoteParams": {
+              "nodeId": "02323d281f323fc7513660602736a7bcd90d9d06057c9fa9e8d2deb0de5aeb2231",
+              "dustLimitSatoshis": 573,
+              "maxHTLCValueInFlightMSat": 0.00198,
+              "channelReserveSatoshis": 2000,
+              "htlcMinimumMSat": 0.00000000001,
+              "toSelfDelay": 144,
+              "maxAcceptedHTLCs": 483,
+              "paymentBasePoint": "034badd7bcd0cdaaae7a099d24e4966a42e9088b8fd9575435428e2ce52ce40132",
+              "fundingPubKey": "038bfc09a2b108e8f63f3377b9374bd4642e9e57c2b8e53b08f7ab18dee284eba4",
+              "revocationBasePoint": "03f1590ac91a6a78c4dc41b5d8caed115c51a9c1b87b6d43a73a5f5502a3ab20cf",
+              "delayedPaymentBasePoint": "0291698f62ba42f26f9fe61fd9509119ff671659410d8d3e3682860f7f5d760645",
+              "htlcBasePoint": "02579a84cb68f6fd41e864d6b5a9b130187584cd338bfd3fc45921b5acfee075fb",
+              "features": "000000101010001010100001",
+              "minimumDepth": 3
+            },
             "channelFlags": 0,
             "fundingScriptCoin": {
               "isP2SH": false,
@@ -33,12 +67,6 @@ type SerializedChannelTest() =
               },
               "amount": 200000,
               "scriptPubKey": "0020725a67d7c34f41ef89dd52a3171c4d13f6e1840186e5d7fb6693f3e179a36bf6"
-            },
-            "localChanges": {
-              "proposed": [],
-              "signed": [],
-              "acKed": [],
-              "all": []
             },
             "localCommit": {
               "index": 0,
@@ -77,32 +105,6 @@ type SerializedChannelTest() =
               },
               "pendingHTLCSuccessTxs": []
             },
-            "localNextHTLCId": 0,
-            "localParams": {
-              "nodeId": "02323d281f323fc7513660602736a7bcd90d9d06057c9fa9e8d2deb0de5aeb2231",
-              "channelPubKeys": {
-                "fundingPubKey": "02fdcf55c55324c7ddf78e5491a2efb2745e465180ff1c905c171cb08293644bb4",
-                "revocationBasePubKey": "03408d1c4a0db3671695bc31b8a7528bdafc91af0a6a208507ef7bcdb2133d6397",
-                "paymentBasePubKey": "021ca7cbadc640a6aba7aa62e976a53a9d79bd8181aa30700c8c486580efa3365b",
-                "delayedPaymentBasePubKey": "02a6c7282ee4c98d7caea59a78b55e5d7a8797fdec9fa97107b39e48d234a37084",
-                "htlcBasePubKey": "031f8050b624096531a0c9f3bba7e5bc7c571ad3eaba1a7587202b067d3986cd81"
-              },
-              "dustLimitSatoshis": 200,
-              "maxHTLCValueInFlightMSat": 0.0000001,
-              "channelReserveSatoshis": 2000,
-              "htlcMinimumMSat": 0.00000001,
-              "toSelfDelay": 6,
-              "maxAcceptedHTLCs": 10,
-              "isFunder": true,
-              "defaultFinalScriptPubKey": "a914ee63c76cda9f5a4928a24710e3b950f2bed0bcc787",
-              "features": "10"
-            },
-            "originChannels": {},
-            "remoteChanges": {
-              "proposed": [],
-              "signed": [],
-              "acKed": []
-            },
             "remoteCommit": {
               "index": 0,
               "spec": {
@@ -115,30 +117,28 @@ type SerializedChannelTest() =
               "txId": "2ce238b4fe973636e02004a6af2dd434a4221b1757ea62b0b0abac810674f3e8",
               "remotePerCommitmentPoint": "032451f412d0ff745d333c50a32d35444c8f88f24b6348c5bbf4f20a6df70ce2ae"
             },
+            "localChanges": {
+              "proposed": [],
+              "signed": [],
+              "acKed": [],
+              "all": []
+            },
+            "remoteChanges": {
+              "proposed": [],
+              "signed": [],
+              "acKed": []
+            },
+            "localNextHTLCId": 0,
+            "remoteNextHTLCId": 0,
+            "originChannels": {},
             "remoteNextCommitInfo": {
               "case": "Revoked",
               "fields": [
                 "0272cbf08b7f138e5d07cefa136f154ba619143c4c87b8333c14ef6057e7df7e1a"
               ]
             },
-            "remoteNextHTLCId": 0,
-            "remoteParams": {
-              "nodeId": "02323d281f323fc7513660602736a7bcd90d9d06057c9fa9e8d2deb0de5aeb2231",
-              "dustLimitSatoshis": 573,
-              "maxHTLCValueInFlightMSat": 0.00198,
-              "channelReserveSatoshis": 2000,
-              "htlcMinimumMSat": 0.00000000001,
-              "toSelfDelay": 144,
-              "maxAcceptedHTLCs": 483,
-              "paymentBasePoint": "034badd7bcd0cdaaae7a099d24e4966a42e9088b8fd9575435428e2ce52ce40132",
-              "fundingPubKey": "038bfc09a2b108e8f63f3377b9374bd4642e9e57c2b8e53b08f7ab18dee284eba4",
-              "revocationBasePoint": "03f1590ac91a6a78c4dc41b5d8caed115c51a9c1b87b6d43a73a5f5502a3ab20cf",
-              "delayedPaymentBasePoint": "0291698f62ba42f26f9fe61fd9509119ff671659410d8d3e3682860f7f5d760645",
-              "htlcBasePoint": "02579a84cb68f6fd41e864d6b5a9b130187584cd338bfd3fc45921b5acfee075fb",
-              "features": "000000101010001010100001",
-              "minimumDepth": 3
-            },
-            "remotePerCommitmentSecrets": []
+            "remotePerCommitmentSecrets": [],
+            "channelId": "2c0e386c42c30b0c4032e8406ab632d6916636df1e5edf66eb083badda023afa"
           },
           "shortChannelId": {
             "blockHeight": 109,
--- src/GWallet.Backend/UtxoCoin/Lightning/SerializedChannel.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/SerializedChannel.fs
@@ -12,73 +12,6 @@ open DotNetLightning.Transactions
 
 open GWallet.Backend.FSharpUtil
 
-type SerializedCommitmentSpec =
-    {
-        HTLCs: Map<HTLCId, DirectedHTLC>
-        FeeRatePerKw: FeeRatePerKw
-        ToLocal: LNMoney
-        ToRemote: LNMoney
-    }
-
-type SerializedCommitments =
-    {
-        ChannelId: ChannelIdentifier
-        ChannelFlags: uint8
-        FundingScriptCoin: ScriptCoin
-        LocalChanges: LocalChanges
-        LocalCommit: LocalCommit
-        LocalNextHTLCId: HTLCId
-        LocalParams: LocalParams
-        OriginChannels: Map<HTLCId, HTLCSource>
-        RemoteChanges: RemoteChanges
-        RemoteCommit: RemoteCommit
-        RemoteNextCommitInfo: RemoteNextCommitInfo
-        RemoteNextHTLCId: HTLCId
-        RemoteParams: RemoteParams
-        RemotePerCommitmentSecrets: RevocationSet
-    }
-
-type private CommitmentsJsonConverter() =
-    inherit JsonConverter<Commitments>()
-
-    override __.ReadJson(reader: JsonReader, _: Type, _: Commitments, _: bool, serializer: JsonSerializer) =
-        let serializedCommitments = serializer.Deserialize<SerializedCommitments> reader
-        let commitments: Commitments = {
-            RemotePerCommitmentSecrets = serializedCommitments.RemotePerCommitmentSecrets
-            RemoteParams = serializedCommitments.RemoteParams
-            RemoteNextHTLCId = serializedCommitments.RemoteNextHTLCId
-            RemoteNextCommitInfo = serializedCommitments.RemoteNextCommitInfo
-            RemoteCommit = serializedCommitments.RemoteCommit
-            RemoteChanges = serializedCommitments.RemoteChanges
-            OriginChannels = serializedCommitments.OriginChannels
-            LocalParams = serializedCommitments.LocalParams
-            LocalNextHTLCId = serializedCommitments.LocalNextHTLCId
-            LocalCommit = serializedCommitments.LocalCommit
-            LocalChanges = serializedCommitments.LocalChanges
-            FundingScriptCoin = serializedCommitments.FundingScriptCoin
-            ChannelId = serializedCommitments.ChannelId.DnlChannelId
-            ChannelFlags = serializedCommitments.ChannelFlags
-        }
-        commitments
-
-    override __.WriteJson(writer: JsonWriter, state: Commitments, serializer: JsonSerializer) =
-        serializer.Serialize(writer, {
-            ChannelId = ChannelIdentifier.FromDnl state.ChannelId
-            ChannelFlags = state.ChannelFlags
-            FundingScriptCoin = state.FundingScriptCoin
-            LocalChanges = state.LocalChanges
-            LocalCommit = state.LocalCommit
-            LocalNextHTLCId = state.LocalNextHTLCId
-            LocalParams = state.LocalParams
-            OriginChannels = state.OriginChannels
-            RemoteChanges = state.RemoteChanges
-            RemoteCommit = state.RemoteCommit
-            RemoteNextCommitInfo = state.RemoteNextCommitInfo
-            RemoteNextHTLCId = state.RemoteNextHTLCId
-            RemoteParams = state.RemoteParams
-            RemotePerCommitmentSecrets = state.RemotePerCommitmentSecrets
-        })
-
 type SerializedChannel =
     {
         ChannelIndex: int
@@ -93,8 +26,6 @@ type SerializedChannel =
     }
     static member LightningSerializerSettings: JsonSerializerSettings =
         let settings = JsonMarshalling.SerializerSettings
-        let commitmentsConverter = CommitmentsJsonConverter()
-        settings.Converters.Add commitmentsConverter
         settings
 
 
