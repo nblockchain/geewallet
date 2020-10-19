nblockchain/geewallet

commit c1a0ed6c12ce6736867409265c649eb095499bd4

Frontend.XF.*: make app a static singleton
Similarly to how it was done in the Xamarin.Essentials samples:
https://github.com/xamarin/Essentials/commit/696928bb76f3debfe2ce75361462646addb81f87

src/GWallet.Frontend.XF.Android/MainActivity.fs
@@ -3,15 +3,15 @@
 open System
 
 open Android.App
-open Android.Content
 open Android.Content.PM
-open Android.Runtime
-open Android.Views
-open Android.Widget
 open Android.OS
+
 open Xamarin.Forms.Platform.Android
 
 type Resources = GWallet.Frontend.XF.Android.Resource
+
+module AppSingleton =
+    let internal Instance = GWallet.Frontend.XF.App ()
 
 [<Activity (LaunchMode = LaunchMode.SingleTask, 
             Label = "geewallet", 
@@ -21,6 +21,7 @@ type Resources = GWallet.Frontend.XF.Android.Resource
             ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
 type MainActivity() =
     inherit FormsAppCompatActivity()
+
 
     override this.OnRequestPermissionsResult(requestCode: int, permissions: string[], grantResults: Permission[]) =
         ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -35,4 +36,4 @@ type MainActivity() =
 
         ZXing.Net.Mobile.Forms.Android.Platform.Init()
 
-        this.LoadApplication (new GWallet.Frontend.XF.App ())
+        this.LoadApplication AppSingleton.Instance

src/GWallet.Frontend.XF.Gtk/Program.fs
@@ -7,6 +7,9 @@ open Xamarin.Forms.Platform.GTK
 
 open GWallet.Backend.FSharpUtil.UwpHacks
 
+module AppSingleton =
+    let internal Instance = GWallet.Frontend.XF.App ()
+
 module Main =
 
     [<EntryPoint>]
@@ -20,9 +23,8 @@ module Main =
             failwith "The GTK frontend is only officially supported for the Linux OS"
 
         ZXing.Net.Mobile.Forms.GTK.Platform.Init()
-        let app = GWallet.Frontend.XF.App()
         use window = new FormsWindow()
-        window.LoadApplication(app)
+        window.LoadApplication AppSingleton.Instance
         window.SetApplicationTitle "geewallet"
         let snapEnvVar = Environment.GetEnvironmentVariable "SNAP"
         let logoFileName = "logo.png"

src/GWallet.Frontend.XF.Mac/AppDelegate.fs
@@ -1,11 +1,13 @@
 ﻿namespace GWallet.Frontend.XF.Mac
 
-open System
 open Foundation
 open AppKit
 
 open Xamarin.Forms
 open Xamarin.Forms.Platform.MacOS
+
+module AppSingleton =
+    let internal Instance = GWallet.Frontend.XF.App ()
 
 [<Register ("AppDelegate")>]
 type AppDelegate () =
@@ -27,6 +29,6 @@ type AppDelegate () =
 
         ZXing.Net.Mobile.Forms.macOS.Platform.Init()
 
-        this.LoadApplication(new GWallet.Frontend.XF.App())
+        this.LoadApplication AppSingleton.Instance
 
         base.DidFinishLaunching(notification)

src/GWallet.Frontend.XF.iOS/AppDelegate.fs
@@ -1,10 +1,13 @@
 ﻿namespace GWallet.Frontend.XF.iOS
 
-open System
 open UIKit
 open Foundation
+
 open Xamarin.Forms
 open Xamarin.Forms.Platform.iOS
+
+module AppSingleton =
+    let internal Instance = GWallet.Frontend.XF.App ()
 
 [<Register ("AppDelegate")>]
 type AppDelegate () =
@@ -15,7 +18,7 @@ type AppDelegate () =
 
         ZXing.Net.Mobile.Forms.iOS.Platform.Init()
 
-        this.LoadApplication (new GWallet.Frontend.XF.App())
+        this.LoadApplication AppSingleton.Instance
         base.FinishedLaunching(app, options)
 
 module Main =

src/GWallet.Frontend.XF.UWP/MainPage.xaml.cs
@@ -23,14 +23,15 @@ namespace GWallet.Frontend.XamForms.UWP
     /// </summary>
     public sealed partial class MainPage
     {
+        static GWallet.Frontend.XF.App formsApp;
+
         public MainPage()
         {
             this.InitializeComponent();
 
             ZXing.Net.Mobile.Forms.WindowsUniversal.ZXingScannerViewRenderer.Init();
 
-            var app = new GWallet.Frontend.XF.App();
-            LoadApplication(app);
+            LoadApplication(formsApp ??= new GWallet.Frontend.XF.App());
         }
     }
 }

commit ae69a2790060a3561c402b4f1723161b3bb285c1

Frontend.XF.*: call Xamarin.Forms.Init earlier
Xamarin.Forms.Init () was called from the entry point.
This does not work anymore and the application crashes on startup
with an exception. The reason is that the initialization of the AppSingleton
module, which happens before the entry point is executed, requires the Init()
call to be done already.

The call has been moved into a do-block inside the AppSingleton module, in
order to avoid this crash (regression caused by previous commit
c1a0ed6c12ce6736867409265c649eb095499bd4):

Unhandled Exception:
System.TypeInitializationException: The type initializer for '<StartupCode$geewallet>.$Program' threw an exception. ---> System.InvalidOperationException: You MUST call Xamarin.Forms.Init(); prior to using it.
  at Xamarin.Forms.Device.get_PlatformServices () [0x00007] in <9c7f7e2a0eb547108347ae7b430c0dfd>:0
  at Xamarin.Forms.Device.GetAssemblies () [0x00000] in <9c7f7e2a0eb547108347ae7b430c0dfd>:0
  at Xamarin.Forms.DependencyService.Initialize () [0x00021] in <9c7f7e2a0eb547108347ae7b430c0dfd>:0
  at Xamarin.Forms.DependencyService.Get[T] (Xamarin.Forms.DependencyFetchTarget fetchTarget) [0x00000] in <9c7f7e2a0eb547108347ae7b430c0dfd>:0
  at Xamarin.Forms.Application..ctor () [0x0002b] in <9c7f7e2a0eb547108347ae7b430c0dfd>:0
  at GWallet.Frontend.XF.App..ctor () [0x00000] in <5f6c7084303fdc7ea745038384706c5f>:0
  at <StartupCode$geewallet>.$Program..cctor () [0x00000] in <5f6c71dc944123fea7450383dc716c5f>:0
   --- End of inner exception stack trace ---
  at (wrapper managed-to-native) System.Object.__icall_wrapper_mono_generic_class_init(intptr)
  at GWallet.Frontend.XF.Gtk.Main.main (System.String[] argv) [0x00000] in <5f6c71dc944123fea7450383dc716c5f>:0
[ERROR] FATAL UNHANDLED EXCEPTION: System.TypeInitializationException: The type initializer for '<StartupCode$geewallet>.$Program' threw an exception. ---> System.InvalidOperationException: You MUST call Xamarin.Forms.Init(); prior to using it.

src/GWallet.Frontend.XF.Android/MainActivity.fs
@@ -6,11 +6,16 @@ open Android.App
 open Android.Content.PM
 open Android.OS
 
+open Xamarin.Forms
 open Xamarin.Forms.Platform.Android
 
 type Resources = GWallet.Frontend.XF.Android.Resource
 
 module AppSingleton =
+    // strangely enough, Android doesn't have an empty Init() method like other platforms...
+    //do
+    //    Xamarin.Forms.Forms.Init()
+
     let internal Instance = GWallet.Frontend.XF.App ()
 
 [<Activity (LaunchMode = LaunchMode.SingleTask, 

src/GWallet.Frontend.XF.Gtk/Program.fs
@@ -8,6 +8,9 @@ open Xamarin.Forms.Platform.GTK
 open GWallet.Backend.FSharpUtil.UwpHacks
 
 module AppSingleton =
+    do
+        Xamarin.Forms.Forms.Init()
+
     let internal Instance = GWallet.Frontend.XF.App ()
 
 module Main =
@@ -16,7 +19,6 @@ module Main =
     [<STAThread>]
     let main argv =
         Gtk.Application.Init()
-        Forms.Init()
 
         // TODO: detect Windows/UWP too
         if GWallet.Backend.Config.IsMacPlatform() then

src/GWallet.Frontend.XF.Mac/AppDelegate.fs
@@ -7,6 +7,9 @@ open Xamarin.Forms
 open Xamarin.Forms.Platform.MacOS
 
 module AppSingleton =
+    do
+        Xamarin.Forms.Forms.Init()
+
     let internal Instance = GWallet.Frontend.XF.App ()
 
 [<Register ("AppDelegate")>]

src/GWallet.Frontend.XF.iOS/AppDelegate.fs
@@ -7,6 +7,9 @@ open Xamarin.Forms
 open Xamarin.Forms.Platform.iOS
 
 module AppSingleton =
+    do
+        Xamarin.Forms.Forms.Init()
+
     let internal Instance = GWallet.Frontend.XF.App ()
 
 [<Register ("AppDelegate")>]