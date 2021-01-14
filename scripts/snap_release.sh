9e4057b9c52a28cff9b3750008c8150cc193e293
Andres G. Aragoneses <knocte@gmail.com>
2021-01-14 12:38:44 +0800

    scripts/snap_release.sh: add more echos and a clarifying comment
    Since this started failing on GithubCI, let's add some more bells&whistles
    for the next time it happens.

--- scripts/snap_release.sh
+++ scripts/snap_release.sh
@@ -10,6 +10,7 @@ if [ -e snapcraft.login ]; then
     echo "snapcraft.login found, skipping log-in"
 else
     if [[ $SNAPCRAFT_LOGIN ]]; then
+        echo "Automatic login about to begin..."
         echo "$SNAPCRAFT_LOGIN" > snapcraft.login
     else
         echo "No login details found, initiating manual logging-in..."
@@ -17,7 +18,9 @@ else
     fi
 fi
 
+# if this fails, use `snapcraft export-login` to generate a new token
 snapcraft login --with snapcraft.login
 
+echo "Login successfull. Upload starting..."
 # the 'stable' and 'candidate' channels require 'stable' grade in the yaml
 snapcraft push snap/*.snap --release=edge
