0768e7bd410d59acac88b555eb7f99f2f9a44ce8
Andrew Cann <shum@canndrew.org>
2021-01-18 20:17:03 +0800

    Backend(LN),Frontend.Console: recover funds from remotely force-closed chans
    When a remote peer force-closes a channel we can now recover the funds
    from it.

--- src/GWallet.Backend/UtxoCoin/Lightning/ChannelManagement.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/ChannelManagement.fs
@@ -1,5 +1,6 @@
 ﻿namespace GWallet.Backend.UtxoCoin.Lightning
 
+open System
 open System.IO
 
 open DotNetLightning.Channel
@@ -205,6 +206,48 @@ type ChannelStore(account: NormalUtxoAccount) =
                 serializedChannel.ChanState.Commitments
                 "A channel can only end up in the wallet if it has commitments."
         commitments.LocalCommit.PublishableTxs.CommitTx.Value.ToHex()
+
+    member self.CheckForClosingTx (channelId: ChannelIdentifier)
+                                      : Async<Option<string * Option<uint32>>> = async {
+        let serializedChannel = self.LoadChannel channelId
+        let commitments = ChannelSerialization.Commitments serializedChannel
+        let currency = self.Currency
+        let network = UtxoCoin.Account.GetNetwork currency
+        let fundingAddressString: string =
+            let fundingAddress: BitcoinAddress =
+                let fundingDestination: TxDestination =
+                    commitments.FundingScriptCoin.ScriptPubKey.GetDestination()
+                fundingDestination.GetAddress network
+            fundingAddress.ToString()
+        let scriptHash = Account.GetElectrumScriptHashFromPublicAddress currency fundingAddressString
+        let! historyList =
+            Server.Query
+                currency
+                (QuerySettings.Default ServerSelectionMode.Fast)
+                (ElectrumClient.GetBlockchainScriptHashHistory scriptHash)
+                None
+        let fundingTxId = TransactionIdentifier.FromHash (ChannelSerialization.Commitments serializedChannel).FundingScriptCoin.Outpoint.Hash
+
+        let closingTxItemOpt =
+            List.tryFind
+                (fun historyItem ->
+                    let thisTxId = TransactionIdentifier.FromHash <| uint256 historyItem.TxHash
+                    thisTxId <> fundingTxId
+                )
+                historyList
+
+        match closingTxItemOpt with
+        | None -> return None
+        | Some closingTxItem ->
+            let closingTxIdString = closingTxItem.TxHash
+            let closingTxHeightOpt =
+                let reportedHeight = closingTxItem.Height
+                if reportedHeight = 0u then
+                    None
+                else
+                    Some reportedHeight
+            return Some (closingTxIdString, closingTxHeightOpt)
+    }
 
 module ChannelManager =
     // difference from fee estimation in UtxoCoinAccount.fs: this is for P2WSH
