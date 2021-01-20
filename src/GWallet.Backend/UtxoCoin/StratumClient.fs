300843dd4c2da678d09c55e9fa6ce601c7819536
Andrew Cann <shum@canndrew.org>
2021-01-20 17:04:17 +0800

    Send electrum json-rpc params as arrays instead of objects
    electrs expects the params of a json-rpc call to be an array rather than
    a json object. This is why we were getting empty responses back from
    electrum servers.

--- src/GWallet.Backend/UtxoCoin/StratumClient.fs
+++ src/GWallet.Backend/UtxoCoin/StratumClient.fs
@@ -341,7 +341,7 @@ type StratumClient (jsonRpcClient: JsonRpcTcpClient) =
         let obj = {
             Id = 0;
             Method = "blockchain.transaction.get_merkle";
-            Params = Map.ofList ["tx_hash", txHash :> obj; "height", height :> obj]
+            Params = [txHash :> obj; height :> obj]
         }
         let json = Serialize obj
         async {
@@ -365,7 +365,7 @@ type StratumClient (jsonRpcClient: JsonRpcTcpClient) =
         let obj = {
             Id = 0
             Method = "blockchain.transaction.get"
-            Params = Map.ofList ["tx_hash", txHash :> obj; "verbose", true :> obj]
+            Params = [txHash :> obj; true :> obj]
         }
         let json = Serialize obj
         async {
