b577cd16ff906d968e56c7967c465206cda1e285
Andres G. Aragoneses <knocte@gmail.com>
2021-01-27 19:14:55 +0800

    Backend/Ether: refactor error handling code to improve readability
    So that there's less need for horizontal scrolling (and at the same
    time we mitigate some ugliness that fantomas will likely cause here
    soon when we use it, see [1]).
    
    [1] https://github.com/fsprojects/fantomas/issues/1391

--- src/GWallet.Backend/Ether/EtherServer.fs
+++ src/GWallet.Backend/Ether/EtherServer.fs
@@ -187,7 +187,8 @@ module Server =
         match maybeRpcResponseEx with
         | Some rpcResponseEx ->
             if rpcResponseEx.RpcError <> null then
-                if rpcResponseEx.RpcError.Code = int RpcErrorCode.StatePruningNodeOrMissingTrieNodeOrHeaderNotFound then
+                match rpcResponseEx.RpcError.Code with
+                | a when a = int RpcErrorCode.StatePruningNodeOrMissingTrieNodeOrHeaderNotFound ->
                     if not (err32kPossibleMessages.Any (fun msg -> rpcResponseEx.RpcError.Message.Contains msg)) then
                         raise <| Exception(
                                      SPrintF2 "Expecting 'pruning=archive' or 'missing trie node' or 'error: no suitable peers available' or 'header not found' in message of a %d code, but got '%s'"
@@ -196,13 +197,15 @@ module Server =
                                      rpcResponseEx)
                     else
                         raise <| ServerMisconfiguredException(exMsg, rpcResponseEx)
-                if (rpcResponseEx.RpcError.Code = int RpcErrorCode.UnknownBlockNumber) then
+                | b when b = int RpcErrorCode.UnknownBlockNumber ->
                     raise <| ServerMisconfiguredException(exMsg, rpcResponseEx)
-                if rpcResponseEx.RpcError.Code = int RpcErrorCode.GatewayTimeout then
+                | c when c = int RpcErrorCode.GatewayTimeout ->
                     raise <| ServerMisconfiguredException(exMsg, rpcResponseEx)
-                if rpcResponseEx.RpcError.Code = int RpcErrorCode.EmptyResponse then
+                | d when d = int RpcErrorCode.EmptyResponse ->
                     raise <| ServerMisconfiguredException(exMsg, rpcResponseEx)
-                raise <| Exception(SPrintF3 "RpcResponseException with RpcError Code <%i> and Message '%s' (%s)"
+                | _ ->
+                    raise
+                    <| Exception (SPrintF3 "RpcResponseException with RpcError Code <%i> and Message '%s' (%s)"
                                          rpcResponseEx.RpcError.Code
                                          rpcResponseEx.RpcError.Message
                                          rpcResponseEx.Message,
