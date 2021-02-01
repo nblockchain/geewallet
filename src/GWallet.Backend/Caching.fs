02b2a5227db091cc2159077778a978c728e26538
Andrew Cann <shum@canndrew.org>
2021-02-02 01:43:16 +0800

    Backend,GitLabCI: add new end2end pipelines (with regtest deps)
    The currentDir change in make.fsx is done so that the text search
    is not done in any of the files installed by these deps.
    
    The Backend fixes are three (which should have been done in the
    commit that added RegTest support[1] but the need for them was only
    exposed by failures in this end2end test).
    
    1) baseline fix: otherwise we would get this exception:
    
    "Merge&Save didn't happen before launching the FaultTolerantPClient?"
    
    2) averageFunc fix: otherwise we would get this exception:
    
    "numberOfParallelJobsAllowed should be equal or higher than
    minimumNumberOfResponses for the averageFunc"
    
    3) fee estimation fix: otherwise we would get this exception:
    
    "GWallet.Backend.ServerMisconfiguredException: Fee estimation
    returned a -1 error code"
    
    [1] 4f7350c6de7369b2d1ba32a7d733fc2a2ceda00d

--- src/GWallet.Backend/Caching.fs
+++ src/GWallet.Backend/Caching.fs
@@ -550,29 +550,12 @@ module Caching =
             )
 
         member __.GetServers (currency: Currency): seq<ServerDetails> =
-            if Config.BitcoinNet() = NBitcoin.Network.RegTest && currency = Currency.BTC then
-                let ipv6Localhost = "::1"
-                seq [
-                    {
-                        ServerInfo =
-                            {
-                                NetworkPath = ipv6Localhost
-                                ConnectionType =
-                                    {
-                                        Encrypted = false
-                                        Protocol = Tcp 50001u
-                                    }
-                            }
-                        CommunicationHistory = None
-                    }
-                ]
-            else
-                lock cacheFiles.ServerStats (fun _ ->
-                    match sessionServerRanking.TryFind currency with
-                    | None ->
-                        failwith <| SPrintF1 "Initialization of servers' cache failed? currency %A not found" currency
-                    | Some servers -> servers
-                )
+            lock cacheFiles.ServerStats (fun _ ->
+                match sessionServerRanking.TryFind currency with
+                | None ->
+                    failwith <| SPrintF1 "Initialization of servers' cache failed? currency %A not found" currency
+                | Some servers -> servers
+            )
 
         member __.ExportServers (): Option<string> =
             lock cacheFiles.ServerStats (fun _ ->
