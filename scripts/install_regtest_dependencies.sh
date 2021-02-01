22bcd79b5b74d79a38f1c47f1603c20a1c275138
Andrew Cann <shum@canndrew.org>
2021-02-01 15:12:24 +0800

    WIP [fix CI] - remove sudo from regtest install script

--- scripts/install_regtest_dependencies.sh
+++ scripts/install_regtest_dependencies.sh
@@ -27,8 +27,8 @@ apt install -y ca-certificates
 # Install "cryptoanarchy" debian repo. This contains packages for bitcoind, lnd and electrs
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
-gpg --export 3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C | sudo apt-key add -
-gpg --export BC528686B50D79E339D3721CEB3E94ADBE1229CF | sudo apt-key add -
+gpg --export 3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C | apt-key add -
+gpg --export BC528686B50D79E339D3721CEB3E94ADBE1229CF | apt-key add -
 echo 'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/debian/10/prod buster main' > /etc/apt/sources.list.d/microsoft.list
 echo 'deb https://deb.ln-ask.me/beta buster common local desktop' > /etc/apt/sources.list.d/cryptoanarchy.list
 apt update
