6625be98e7b31be6f53d5d9449b899b9a4e38f15
Andrew Cann <shum@canndrew.org>
2021-02-01 14:42:50 +0800

    WIP - attempt to fix CI

--- scripts/install_regtest_dependencies.sh
+++ scripts/install_regtest_dependencies.sh
@@ -10,24 +10,41 @@ export ALLOW_ROOT=1
 
 
 # Install bitcoin core
-apt install -y curl tar gzip
-curl -OJL https://bitcoin.org/bin/bitcoin-core-0.20.0/bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
-tar -C /usr/local --strip-components 1 -xzf bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
+# apt install -y curl tar gzip
+# curl -OJL https://bitcoin.org/bin/bitcoin-core-0.20.0/bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
+# tar -C /usr/local --strip-components 1 -xzf bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
 
 # Install electrumx
-apt install -y curl unzip python3-pip
-curl -L https://github.com/spesmilo/electrumx/archive/1.15.0.zip -o electrumx-1.15.0.zip
-unzip electrumx-1.15.0.zip
-pip3 install ./electrumx-1.15.0
+#apt install -y curl unzip python3-pip
+#curl -L https://github.com/spesmilo/electrumx/archive/1.15.0.zip -o electrumx-1.15.0.zip
+#unzip electrumx-1.15.0.zip
+#pip3 install ./electrumx-1.15.0
+
+# Install "cryptoanarchy" debian repo. This contains packages for bitcoind, lnd and electrs
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+gpg --export 3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C | sudo apt-key add -
+gpg --export BC528686B50D79E339D3721CEB3E94ADBE1229CF | sudo apt-key add -
+echo 'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/debian/10/prod buster main' > /etc/apt/sources.list.d/microsoft.list
+echo 'deb https://deb.ln-ask.me/beta buster common local desktop' > /etc/apt/sources.list.d/cryptoanarchy.list
+apt update
+
+# install bitcoin core
+apt install bitcoind
+
+# Install electrs
+apt install electrs
 
 # Install lnd
-apt install -y curl tar gzip unzip make git
-curl -OJL https://golang.org/dl/go1.14.4.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
-git clone https://github.com/lightningnetwork/lnd
-pushd lnd
-git checkout v0.10.3-beta
-make
-make install
-popd
+apt install lnd
 
+# Install lnd
+# apt install -y curl tar gzip unzip make git
+# curl -OJL https://golang.org/dl/go1.14.4.linux-amd64.tar.gz
+# tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
+# git clone https://github.com/lightningnetwork/lnd
+# pushd lnd
+# git checkout v0.10.3-beta
+# make
+# make install
+# popd
