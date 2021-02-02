a184cb95cb0beb9030fc4e5ed30f82fa729a61b0
Andrew Cann <shum@canndrew.org>
2021-02-02 16:20:39 +0800

    EndToEnd: deduplicate some code in AcceptChannel
    Add WalletInstance.AcceptChannelFromFunder method and use it to
    de-duplicate code in tests.

--- src/GWallet.Backend.Tests.EndToEnd/GeewalletToGeewalletFundee.fs
+++ src/GWallet.Backend.Tests.EndToEnd/GeewalletToGeewalletFundee.fs
@@ -23,46 +23,7 @@ type GeewalletToGeewalletFundee() =
     [<Test>]
     member __.``can send/receive monohop payments and close channel (fundee)``() = Async.RunSynchronously <| async {
         use! walletInstance = WalletInstance.New (Some Config.FundeeLightningIPEndpoint) (Some Config.FundeeAccountsPrivateKey)
-        let! pendingChannelRes =
-            Lightning.Network.AcceptChannel
-                walletInstance.Node
-
-        let (channelId, _fundingTxId) = UnwrapResult pendingChannelRes "OpenChannel failed"
-
-        let! lockFundingRes = Lightning.Network.LockChannelFunding walletInstance.Node channelId
-        UnwrapResult lockFundingRes "LockChannelFunding failed"
-
-        let channelInfo = walletInstance.ChannelStore.ChannelInfo channelId
-        match channelInfo.Status with
-        | ChannelStatus.Active -> ()
-        | status -> failwith (SPrintF1 "unexpected channel status. Expected Active, got %A" status)
-
-        if Money(channelInfo.Balance, MoneyUnit.BTC) <> Money(0.0m, MoneyUnit.BTC) then
-            failwith "incorrect balance after accepting channel"
-
-        let! receiveMonoHopPaymentRes =
-            Lightning.Network.ReceiveMonoHopPayment walletInstance.Node channelId
-        UnwrapResult receiveMonoHopPaymentRes "ReceiveMonoHopPayment failed"
-
-        let channelInfoAfterPayment0 = walletInstance.ChannelStore.ChannelInfo channelId
-        match channelInfo.Status with
-        | ChannelStatus.Active -> ()
-        | status -> failwith (SPrintF1 "unexpected channel status. Expected Active, got %A" status)
-
-        if Money(channelInfoAfterPayment0.Balance, MoneyUnit.BTC) <> Config.WalletToWalletTestPayment0Amount then
-            failwith "incorrect balance after receiving payment 0"
-
-        let! receiveMonoHopPaymentRes =
-            Lightning.Network.ReceiveMonoHopPayment walletInstance.Node channelId
-        UnwrapResult receiveMonoHopPaymentRes "ReceiveMonoHopPayment failed"
-
-        let channelInfoAfterPayment1 = walletInstance.ChannelStore.ChannelInfo channelId
-        match channelInfo.Status with
-        | ChannelStatus.Active -> ()
-        | status -> failwith (SPrintF1 "unexpected channel status. Expected Active, got %A" status)
-
-        if Money(channelInfoAfterPayment1.Balance, MoneyUnit.BTC) <> Config.WalletToWalletTestPayment0Amount + Config.WalletToWalletTestPayment1Amount then
-            failwith "incorrect balance after receiving payment 1"
+        let! channelId = walletInstance.AcceptChannelFromFunder()
 
         let! closeChannelRes = Lightning.Network.AcceptCloseChannel walletInstance.Node channelId
         match closeChannelRes with
