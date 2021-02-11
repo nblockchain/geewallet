043bff1785ef3a81c6ea4dca8622dd780377fccf
Andres G. Aragoneses <knocte@gmail.com>
2021-02-11 15:11:31 +0800

    Backend.Tests.End2End: separate types into their own files

---
+++ src/GWallet.Backend.Tests.End2End/ElectrumServer.fs
@@ -0,0 +1,35 @@
+﻿namespace GWallet.Backend.Tests.End2End
+
+open System
+open System.IO
+
+type ElectrumServer = {
+    DbDir: string
+    ProcessWrapper: ProcessWrapper
+} with
+    interface IDisposable with
+        member self.Dispose() =
+            self.ProcessWrapper.Process.Kill()
+            self.ProcessWrapper.WaitForExit()
+            Directory.Delete(self.DbDir, true)
+
+    static member Start(bitcoind: Bitcoind): ElectrumServer =
+        let dbDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName())
+        Directory.CreateDirectory dbDir |> ignore
+        let processWrapper =
+            ProcessWrapper.New
+                "electrumx_server"
+                ""
+                (Map.ofList <| [
+                    "SERVICES", "tcp://[::1]:50001";
+                    "COIN", "BitcoinSegwit";
+                    "NET", "regtest";
+                    "DAEMON_URL", bitcoind.RpcUrl;
+                    "DB_DIRECTORY", dbDir
+                ])
+                true
+        processWrapper.WaitForMessage (fun msg -> msg.EndsWith "TCP server listening on [::1]:50001")
+        {
+            DbDir = dbDir
+            ProcessWrapper = processWrapper
+        }
