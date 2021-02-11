35271be43cef646aa7555f726a846daede428961
Andres G. Aragoneses <knocte@gmail.com>
2021-02-11 23:22:36 +0800

    Backend.Tests.End2End: remove more duplicated code

--- src/GWallet.Backend.Tests.End2End/Lnd.fs
+++ src/GWallet.Backend.Tests.End2End/Lnd.fs
@@ -164,3 +164,31 @@ type Lnd = {
         | OpenChannelResult.Ok -> return Ok ()
         | err -> return Error err
     }
+
+    member self.FundByMining (bitcoind: Bitcoind)
+                             (address: BitcoinAddress)
+                                 : Async<unit> = async {
+        let! lndDepositAddress = self.GetDepositAddress()
+        let blocksMinedToLnd = BlockHeightOffset32 1u
+        bitcoind.GenerateBlocks blocksMinedToLnd lndDepositAddress
+
+        // Geewallet cannot use these outputs, even though they are encumbered with an output
+        // script from its wallet. This is because they come from coinbase. Coinbase outputs are
+        // the source of all bitcoin, and as of May 2020, Geewallet does not detect coins
+        // received straight from coinbase. In practice, this doesn't matter, since miners
+        // do not use Geewallet. If the coins were to be detected by geewallet,
+        // this test would still work. This comment is just here to avoid confusion.
+        let maturityDurationInNumberOfBlocks = BlockHeightOffset32 (uint32 NBitcoin.Consensus.RegTest.CoinbaseMaturity)
+        bitcoind.GenerateBlocks maturityDurationInNumberOfBlocks address
+
+        // We confirm the one block mined to LND, by waiting for LND to see the chain
+        // at a height which has that block matured. The height at which the block will
+        // be matured is 100 on regtest. Since we initialally mined one block for LND,
+        // this will wait until the block height of LND reaches 1 (initial blocks mined)
+        // plus 100 blocks (coinbase maturity). This test has been parameterized
+        // to use the constants defined in NBitcoin, but you have to keep in mind that
+        // the coinbase maturity may be defined differently in other coins.
+        do! self.WaitForBlockHeight (BlockHeight.Zero + blocksMinedToLnd + maturityDurationInNumberOfBlocks)
+        do! self.WaitForBalance (Money(50UL, MoneyUnit.BTC))
+    }
+
