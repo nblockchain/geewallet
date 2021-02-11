b8b09e74a448edeba18629279db62a90409a663c
Andres G. Aragoneses <knocte@gmail.com>
2021-02-11 23:22:36 +0800

    Backend.Tests.End2End: remove more duplicated code

--- src/GWallet.Backend.Tests.End2End/LN.fs
+++ src/GWallet.Backend.Tests.End2End/LN.fs
@@ -21,25 +21,6 @@ type LN() =
     let walletToWalletTestPayment0Amount = Money (0.01m, MoneyUnit.BTC)
     let walletToWalletTestPayment1Amount = Money (0.015m, MoneyUnit.BTC)
 
-    let fundeeAccountsPrivateKey =
-        // Note: The key needs to be hard-coded, as opposed to randomly
-        // generated, since it is used in two separate processes and must be
-        // the same in each process.
-        new Key (uint256.Parse("9d1ee30acb68716ed5f4e25b3c052c6078f1813f45d33a47e46615bfd05fa6fe").ToBytes())
-    let fundeeNodePubKey =
-        let extKey = WalletInstance.AccountPrivateKeyToNodeSecret fundeeAccountsPrivateKey
-        extKey.PrivateKey.PubKey
-    let fundeeLightningIPEndpoint = IPEndPoint (IPAddress.Parse "127.0.0.1", 9735)
-    let fundeeNodeEndpoint =
-        NodeEndPoint.Parse
-            Currency.BTC
-            (SPrintF3
-                "%s@%s:%d"
-                (fundeeNodePubKey.ToHex())
-                (fundeeLightningIPEndpoint.Address.ToString())
-                fundeeLightningIPEndpoint.Port
-            )
-
     [<Category "G2GChannelOpeningFunder">]
     [<Test>]
     member __.``can open channel with geewallet (funder)``() = Async.RunSynchronously <| async {
@@ -48,77 +29,9 @@ type LN() =
         use _electrumServer = ElectrumServer.Start bitcoind
         use! lnd = Lnd.Start bitcoind
 
-        // As explained in the other test, geewallet cannot use coinbase outputs.
-        // To work around that we mine a block to a LND instance and afterwards tell
-        // it to send funds to the funder geewallet instance
-        let! address = lnd.GetDepositAddress()
-        let blocksMinedToLnd = BlockHeightOffset32 1u
-        bitcoind.GenerateBlocks blocksMinedToLnd address
+        do! walletInstance.FundByMining bitcoind lnd
 
-        let maturityDurationInNumberOfBlocks = BlockHeightOffset32 (uint32 NBitcoin.Consensus.RegTest.CoinbaseMaturity)
-        bitcoind.GenerateBlocks maturityDurationInNumberOfBlocks walletInstance.Address
-
-        // We confirm the one block mined to LND, by waiting for LND to see the chain
-        // at a height which has that block matured. The height at which the block will
-        // be matured is 100 on regtest. Since we initialally mined one block for LND,
-        // this will wait until the block height of LND reaches 1 (initial blocks mined)
-        // plus 100 blocks (coinbase maturity). This test has been parameterized
-        // to use the constants defined in NBitcoin, but you have to keep in mind that
-        // the coinbase maturity may be defined differently in other coins.
-        do! lnd.WaitForBlockHeight (BlockHeight.Zero + blocksMinedToLnd + maturityDurationInNumberOfBlocks)
-        do! lnd.WaitForBalance (Money(50UL, MoneyUnit.BTC))
-
-        // fund geewallet
-        let geewalletAccountAmount = Money (25m, MoneyUnit.BTC)
-        let feeRate = FeeRatePerKw 2500u
-        let! _txid = lnd.SendCoins geewalletAccountAmount walletInstance.Address feeRate
-
-        // wait for lnd's transaction to appear in mempool
-        while bitcoind.GetTxIdsInMempool().Length = 0 do
-            Thread.Sleep 500
-
-        // We want to make sure Geewallet consideres the money received.
-        // A typical number of blocks that is almost universally considered
-        // 100% confirmed, is 6. Therefore we mine 7 blocks. Because we have
-        // waited for the transaction to appear in bitcoind's mempool, we
-        // can assume that the first of the 7 blocks will include the
-        // transaction sending money to Geewallet. The next 6 blocks will
-        // bury the first block, so that the block containing the transaction
-        // will be 6 deep at the end of the following call to generateBlocks.
-        // At that point, the 0.25 regtest coins from the above call to sendcoins
-        // are considered arrived to Geewallet.
-        let consideredConfirmedAmountOfBlocksPlusOne = BlockHeightOffset32 7u
-        bitcoind.GenerateBlocks consideredConfirmedAmountOfBlocksPlusOne walletInstance.Address
-
-        let fundingAmount = Money(0.1m, MoneyUnit.BTC)
-        let! transferAmount = async {
-            let! accountBalance = walletInstance.WaitForBalance fundingAmount
-            return TransferAmount (fundingAmount.ToDecimal MoneyUnit.BTC, accountBalance.ToDecimal MoneyUnit.BTC, Currency.BTC)
-        }
-        let! metadata = ChannelManager.EstimateChannelOpeningFee (walletInstance.Account :?> NormalUtxoAccount) transferAmount
-        let! pendingChannelRes =
-            Lightning.Network.OpenChannel
-                walletInstance.NodeServer.NodeClient
-                fundeeNodeEndpoint
-                transferAmount
-                metadata
-                walletInstance.Password
-        let pendingChannel = UnwrapResult pendingChannelRes "OpenChannel failed"
-        let minimumDepth = (pendingChannel :> IChannelToBeOpened).ConfirmationsRequired
-        let channelId = (pendingChannel :> IChannelToBeOpened).ChannelId
-        let! fundingTxIdRes = pendingChannel.Accept()
-        let _fundingTxId = UnwrapResult fundingTxIdRes "pendingChannel.Accept failed"
-        bitcoind.GenerateBlocks (BlockHeightOffset32 minimumDepth) walletInstance.Address
-
-        do! walletInstance.WaitForFundingConfirmed channelId
-
-        let! lockFundingRes = Lightning.Network.ConnectLockChannelFunding walletInstance.NodeServer.NodeClient channelId
-        UnwrapResult lockFundingRes "LockChannelFunding failed"
-
-        let channelInfo = walletInstance.ChannelStore.ChannelInfo channelId
-        match channelInfo.Status with
-        | ChannelStatus.Active -> ()
-        | status -> failwith (SPrintF1 "unexpected channel status. Expected Active, got %A" status)
+        let! _channelId,_fundingAmount = walletInstance.OpenChannelWithFundee bitcoind Config.FundeeNodeEndpoint
 
         return ()
     }
@@ -126,7 +39,7 @@ type LN() =
     [<Category "G2GChannelOpeningFundee">]
     [<Test>]
     member __.``can open channel with geewallet (fundee)``() = Async.RunSynchronously <| async {
-        use! walletInstance = WalletInstance.New (Some fundeeLightningIPEndpoint) (Some fundeeAccountsPrivateKey)
+        use! walletInstance = WalletInstance.New (Some Config.FundeeLightningIPEndpoint) (Some Config.FundeeAccountsPrivateKey)
         let! pendingChannelRes =
             Lightning.Network.AcceptChannel
                 walletInstance.NodeServer
@@ -152,80 +65,10 @@ type LN() =
         use _electrumServer = ElectrumServer.Start bitcoind
         use! lnd = Lnd.Start bitcoind
 
-        // As explained in the other test, geewallet cannot use coinbase outputs.
-        // To work around that we mine a block to a LND instance and afterwards tell
-        // it to send funds to the funder geewallet instance
-        let! address = lnd.GetDepositAddress()
-        let blocksMinedToLnd = BlockHeightOffset32 1u
-        bitcoind.GenerateBlocks blocksMinedToLnd address
+        do! walletInstance.FundByMining bitcoind lnd
 
-        let maturityDurationInNumberOfBlocks = BlockHeightOffset32 (uint32 NBitcoin.Consensus.RegTest.CoinbaseMaturity)
-        bitcoind.GenerateBlocks maturityDurationInNumberOfBlocks walletInstance.Address
-
-        // We confirm the one block mined to LND, by waiting for LND to see the chain
-        // at a height which has that block matured. The height at which the block will
-        // be matured is 100 on regtest. Since we initialally mined one block for LND,
-        // this will wait until the block height of LND reaches 1 (initial blocks mined)
-        // plus 100 blocks (coinbase maturity). This test has been parameterized
-        // to use the constants defined in NBitcoin, but you have to keep in mind that
-        // the coinbase maturity may be defined differently in other coins.
-        do! lnd.WaitForBlockHeight (BlockHeight.Zero + blocksMinedToLnd + maturityDurationInNumberOfBlocks)
-        do! lnd.WaitForBalance (Money(50UL, MoneyUnit.BTC))
-
-        // fund geewallet
-        let geewalletAccountAmount = Money (25m, MoneyUnit.BTC)
-        let feeRate = FeeRatePerKw 2500u
-        let! _txid = lnd.SendCoins geewalletAccountAmount walletInstance.Address feeRate
-
-        // wait for lnd's transaction to appear in mempool
-        while bitcoind.GetTxIdsInMempool().Length = 0 do
-            Thread.Sleep 500
-
-        // We want to make sure Geewallet consideres the money received.
-        // A typical number of blocks that is almost universally considered
-        // 100% confirmed, is 6. Therefore we mine 7 blocks. Because we have
-        // waited for the transaction to appear in bitcoind's mempool, we
-        // can assume that the first of the 7 blocks will include the
-        // transaction sending money to Geewallet. The next 6 blocks will
-        // bury the first block, so that the block containing the transaction
-        // will be 6 deep at the end of the following call to generateBlocks.
-        // At that point, the 0.25 regtest coins from the above call to sendcoins
-        // are considered arrived to Geewallet.
-        let consideredConfirmedAmountOfBlocksPlusOne = BlockHeightOffset32 7u
-        bitcoind.GenerateBlocks consideredConfirmedAmountOfBlocksPlusOne walletInstance.Address
-
-        let fundingAmount = Money(0.1m, MoneyUnit.BTC)
-        let! transferAmount = async {
-            let! accountBalance = walletInstance.WaitForBalance fundingAmount
-            return TransferAmount (fundingAmount.ToDecimal MoneyUnit.BTC, accountBalance.ToDecimal MoneyUnit.BTC, Currency.BTC)
-        }
-        let! metadata = ChannelManager.EstimateChannelOpeningFee (walletInstance.Account :?> NormalUtxoAccount) transferAmount
-        let! pendingChannelRes =
-            Lightning.Network.OpenChannel
-                walletInstance.NodeServer.NodeClient
-                fundeeNodeEndpoint
-                transferAmount
-                metadata
-                walletInstance.Password
-        let pendingChannel = UnwrapResult pendingChannelRes "OpenChannel failed"
-        let minimumDepth = (pendingChannel :> IChannelToBeOpened).ConfirmationsRequired
-        let channelId = (pendingChannel :> IChannelToBeOpened).ChannelId
-        let! fundingTxIdRes = pendingChannel.Accept()
-        let _fundingTxId = UnwrapResult fundingTxIdRes "pendingChannel.Accept failed"
-        bitcoind.GenerateBlocks (BlockHeightOffset32 minimumDepth) walletInstance.Address
-
-        do! walletInstance.WaitForFundingConfirmed channelId
-
-        let! lockFundingRes = Lightning.Network.ConnectLockChannelFunding walletInstance.NodeServer.NodeClient channelId
-        UnwrapResult lockFundingRes "LockChannelFunding failed"
-
+        let! channelId,fundingAmount = walletInstance.OpenChannelWithFundee bitcoind Config.FundeeNodeEndpoint
         let channelInfo = walletInstance.ChannelStore.ChannelInfo channelId
-        match channelInfo.Status with
-        | ChannelStatus.Active -> ()
-        | status -> failwith (SPrintF1 "unexpected channel status. Expected Active, got %A" status)
-
-        if Money(channelInfo.Balance, MoneyUnit.BTC) <> fundingAmount then
-            failwith "balance does not match funding amount"
 
         let! sendMonoHopPayment0Res =
             let transferAmount =
@@ -270,7 +113,7 @@ type LN() =
     [<Category "G2GMonoHopUnidirectionalPaymentsFundee">]
     [<Test>]
     member __.``can receive mono-hop unidirectional payments, with geewallet (fundee)``() = Async.RunSynchronously <| async {
-        use! walletInstance = WalletInstance.New (Some fundeeLightningIPEndpoint) (Some fundeeAccountsPrivateKey)
+        use! walletInstance = WalletInstance.New (Some Config.FundeeLightningIPEndpoint) (Some Config.FundeeAccountsPrivateKey)
         let! pendingChannelRes =
             Lightning.Network.AcceptChannel
                 walletInstance.NodeServer
@@ -323,81 +166,10 @@ type LN() =
         use _electrumServer = ElectrumServer.Start bitcoind
         use! lnd = Lnd.Start bitcoind
 
-        let! address = lnd.GetDepositAddress()
-        let blocksMinedToLnd = BlockHeightOffset32 1u
-        bitcoind.GenerateBlocks blocksMinedToLnd address
-
-        // Geewallet cannot use these outputs, even though they are encumbered with an output
-        // script from its wallet. This is because they come from coinbase. Coinbase outputs are
-        // the source of all bitcoin, and as of May 2020, Geewallet does not detect coins
-        // received straight from coinbase. In practice, this doesn't matter, since miners
-        // do not use Geewallet. If the coins were to be detected by geewallet,
-        // this test would still work. This comment is just here to avoid confusion.
-        let maturityDurationInNumberOfBlocks = BlockHeightOffset32 (uint32 NBitcoin.Consensus.RegTest.CoinbaseMaturity)
-        bitcoind.GenerateBlocks maturityDurationInNumberOfBlocks walletInstance.Address
-
-        // We confirm the one block mined to LND, by waiting for LND to see the chain
-        // at a height which has that block matured. The height at which the block will
-        // be matured is 100 on regtest. Since we initialally mined one block for LND,
-        // this will wait until the block height of LND reaches 1 (initial blocks mined)
-        // plus 100 blocks (coinbase maturity). This test has been parameterized
-        // to use the constants defined in NBitcoin, but you have to keep in mind that
-        // the coinbase maturity may be defined differently in other coins.
-        do! lnd.WaitForBlockHeight (BlockHeight.Zero + blocksMinedToLnd + maturityDurationInNumberOfBlocks)
-        do! lnd.WaitForBalance (Money(50UL, MoneyUnit.BTC))
-
-        // fund geewallet
-        let geewalletAccountAmount = Money (25m, MoneyUnit.BTC)
-        let feeRate = FeeRatePerKw 2500u
-        let! _txid = lnd.SendCoins geewalletAccountAmount walletInstance.Address feeRate
-
-        // wait for lnd's transaction to appear in mempool
-        while bitcoind.GetTxIdsInMempool().Length = 0 do
-            Thread.Sleep 500
-
-        // We want to make sure Geewallet consideres the money received.
-        // A typical number of blocks that is almost universally considered
-        // 100% confirmed, is 6. Therefore we mine 7 blocks. Because we have
-        // waited for the transaction to appear in bitcoind's mempool, we
-        // can assume that the first of the 7 blocks will include the
-        // transaction sending money to Geewallet. The next 6 blocks will
-        // bury the first block, so that the block containing the transaction
-        // will be 6 deep at the end of the following call to generateBlocks.
-        // At that point, the 0.25 regtest coins from the above call to sendcoins
-        // are considered arrived to Geewallet.
-        let consideredConfirmedAmountOfBlocksPlusOne = BlockHeightOffset32 7u
-        bitcoind.GenerateBlocks consideredConfirmedAmountOfBlocksPlusOne walletInstance.Address
+        do! walletInstance.FundByMining bitcoind lnd
 
         let! lndEndPoint = lnd.GetEndPoint()
-        let! transferAmount = async {
-            let amount = Money(0.002m, MoneyUnit.BTC)
-            let! accountBalance = walletInstance.WaitForBalance amount
-            return TransferAmount (amount.ToDecimal MoneyUnit.BTC, accountBalance.ToDecimal MoneyUnit.BTC, Currency.BTC)
-        }
-        let! metadata = ChannelManager.EstimateChannelOpeningFee (walletInstance.Account :?> NormalUtxoAccount) transferAmount
-        let! pendingChannelRes =
-            Lightning.Network.OpenChannel
-                walletInstance.NodeServer.NodeClient
-                lndEndPoint
-                transferAmount
-                metadata
-                walletInstance.Password
-        let pendingChannel = UnwrapResult pendingChannelRes "OpenChannel failed"
-        let minimumDepth = (pendingChannel :> IChannelToBeOpened).ConfirmationsRequired
-        let channelId = (pendingChannel :> IChannelToBeOpened).ChannelId
-        let! fundingTxIdRes = pendingChannel.Accept()
-        let _fundingTxId = UnwrapResult fundingTxIdRes "pendingChannel.Accept failed"
-        bitcoind.GenerateBlocks (BlockHeightOffset32 minimumDepth) walletInstance.Address
-
-        do! walletInstance.WaitForFundingConfirmed channelId
-
-        let! lockFundingRes = Lightning.Network.ConnectLockChannelFunding walletInstance.NodeServer.NodeClient channelId
-        UnwrapResult lockFundingRes "LockChannelFunding failed"
-
-        let channelInfo = walletInstance.ChannelStore.ChannelInfo channelId
-        match channelInfo.Status with
-        | ChannelStatus.Active -> ()
-        | status -> failwith (SPrintF1 "unexpected channel status. Expected Active, got %A" status)
+        let! _channelId, _fundingAmount = walletInstance.OpenChannelWithFundee bitcoind lndEndPoint
 
         return ()
     }
@@ -409,28 +181,7 @@ type LN() =
         use _electrumServer = ElectrumServer.Start bitcoind
         use! lnd = Lnd.Start bitcoind
 
-        let! address = lnd.GetDepositAddress()
-        let blocksMinedToLnd = BlockHeightOffset32 1u
-        bitcoind.GenerateBlocks blocksMinedToLnd address
-
-        // Geewallet cannot use these outputs, even though they are encumbered with an output
-        // script from its wallet. This is because they come from coinbase. Coinbase outputs are
-        // the source of all bitcoin, and as of May 2020, Geewallet does not detect coins
-        // received straight from coinbase. In practice, this doesn't matter, since miners
-        // do not use Geewallet. If the coins were to be detected by geewallet,
-        // this test would still work. This comment is just here to avoid confusion.
-        let maturityDurationInNumberOfBlocks = BlockHeightOffset32 (uint32 NBitcoin.Consensus.RegTest.CoinbaseMaturity)
-        bitcoind.GenerateBlocks maturityDurationInNumberOfBlocks walletInstance.Address
-
-        // We confirm the one block mined to LND, by waiting for LND to see the chain
-        // at a height which has that block matured. The height at which the block will
-        // be matured is 100 on regtest. Since we initialally mined one block for LND,
-        // this will wait until the block height of LND reaches 1 (initial blocks mined)
-        // plus 100 blocks (coinbase maturity). This test has been parameterized
-        // to use the constants defined in NBitcoin, but you have to keep in mind that
-        // the coinbase maturity may be defined differently in other coins.
-        do! lnd.WaitForBlockHeight (BlockHeight.Zero + blocksMinedToLnd + maturityDurationInNumberOfBlocks)
-        do! lnd.WaitForBalance (Money(50UL, MoneyUnit.BTC))
+        do! lnd.FundByMining bitcoind walletInstance.Address
 
         let acceptChannelTask = Lightning.Network.AcceptChannel walletInstance.NodeServer
         let openChannelTask = async {
