c800c98b3f874b2cdc271ccbe7ed58326df87940
Andres G. Aragoneses <knocte@gmail.com>
2021-03-11 14:38:56 +0800

    End2End: separate closing tests
    Recent commit[1] that added close-channel testing extended the existing
    opening-channel tests, but it's better to have them separate.
    
    [1] c533983a8534167ab29a24bbaee75774fb72c7a5

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -254,6 +254,7 @@ let RunTests (suite: string) =
         [
             "G2GChannelOpening"
             "G2GMonoHopUnidirectionalPayments"
+            "G2GChannelClosing"
         ]
 
     let runTwoProcessTest testAssembly testName =
