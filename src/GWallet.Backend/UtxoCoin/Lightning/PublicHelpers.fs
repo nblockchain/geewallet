c3ad2cd0aadca39ae46580f0e0fa5df7fe9d9bd2
Andres G. Aragoneses <knocte@gmail.com>
2021-05-15 12:37:52 +0800

    Backend(LN),End2End,Frontend.Console: simplify/refactor function

--- src/GWallet.Backend/UtxoCoin/Lightning/PublicHelpers.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/PublicHelpers.fs
@@ -52,7 +52,4 @@ module public Network =
     let public ReceiveLightningEvent (nodeServer: NodeServer) = nodeServer.ReceiveLightningEvent
     let public AcceptLockChannelFunding (nodeServer: NodeServer) = nodeServer.AcceptLockChannelFunding
 
-    let public CreateRecoveryTxForRemoteForceClose (node: Node) =
-        node.CreateRecoveryTxForRemoteForceClose
-
     let public EndPoint (nodeServer: NodeServer) = nodeServer.EndPoint
