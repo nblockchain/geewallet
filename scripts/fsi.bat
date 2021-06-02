8ef9f738c4b9bede41866be42c9f2c48e673a7a3
Andres G. Aragoneses <knocte@gmail.com>
2021-06-02 21:53:31 +0800

    GitHubCI: fix fsi location wrt last VM update
    See [1] ([2]).
    
    [1] https://gitlab.com/nblockchain/fsx/-/commit/9d6c512d6f97763a0ff94c0e76d4fcb0c34908f2
    [2] https://stackoverflow.com/questions/67800998/githubactions-ci-windows-vms-dont-have-fsi-exe-anymore-or-it-changed-location

--- scripts/fsi.bat
+++ scripts/fsi.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
 
-SET ENTERPRISE="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET COMMUNITY="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET BUILDTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
+SET ENTERPRISE="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
+SET ENTERPRISE_OLD="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
+SET COMMUNITY="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
+SET COMMUNITY_OLD="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
+SET BUILDTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
+SET BUILDTOOLS_OLD="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
 
 IF EXIST %ENTERPRISE% (
     SET RUNNER=%ENTERPRISE%
@@ -13,8 +16,20 @@ IF EXIST %ENTERPRISE% (
         IF EXIST %BUILDTOOLS% (
             SET RUNNER=%BUILDTOOLS%
         ) ELSE (
-            ECHO fsi.exe not found, is F# installed?
-            EXIT /b 1
+            IF EXIST %ENTERPRISE_OLD% (
+                SET RUNNER=%ENTERPRISE_OLD%
+            ) ELSE (
+                IF EXIST %COMMUNITY_OLD% (
+                    SET RUNNER=%COMMUNITY%
+                ) ELSE (
+                    IF EXIST %BUILDTOOLS_OLD% (
+                        SET RUNNER=%BUILDTOOLS_OLD%
+                    ) ELSE (
+                        ECHO fsi.exe not found, is F# installed?
+                        EXIT /b 1
+                    )
+                )
+            )
         )
     )
 )
