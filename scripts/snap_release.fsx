6205ba0ba9e19d3461d7bae7aefb1c49a76051a5
Andres G. Aragoneses <knocte@gmail.com>
2021-10-28 17:13:26 +0800

    GitHubCI: move snap builds to same workflow
    This way the snap package will be always created (regardless if
    we're on a git tag or not), and only published if a git tag is
    being pushed.
    
    This allows us to detect problems in the packaging process earlier
    than before.

--- scripts/snap_release.fsx
+++ scripts/snap_release.fsx
@@ -32,6 +32,19 @@ if not (snapSubfolder.EnumerateFiles().Any(fun file -> file.Name.EndsWith ".snap
     Console.Error.WriteLine "No snap package found."
     Environment.Exit 1
 
+Console.WriteLine "Checking if this is a tag commit..."
+let githubRef = Environment.GetEnvironmentVariable "GITHUB_REF"
+if String.IsNullOrEmpty githubRef then
+    failwith "GITHUB_REF var not found. Beware: manual logging for release has been disabled, only automated CI jobs can upload now"
+
+let tagsPrefix = "refs/tags/"
+if not (githubRef.StartsWith tagsPrefix) then
+    Console.WriteLine (sprintf "No tag being set (GITHUB_REF=%s), skipping release." githubRef)
+    Environment.Exit 0
+
+let gitTag = githubRef.Substring tagsPrefix.Length
+Console.WriteLine (sprintf "About to start upload of release %s" gitTag)
+
 let snapcraftLoginFileName = Path.Combine(FsxHelper.RootDir.FullName, "snapcraft.login")
 if File.Exists snapcraftLoginFileName then
     Console.WriteLine "snapcraft.login file found, skipping log-in"
