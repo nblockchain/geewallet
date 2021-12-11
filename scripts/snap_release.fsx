8f2122f5de484d1730029a696409bd5e5f6e5ed5
Andres G. Aragoneses <knocte@gmail.com>
2021-12-11 19:45:02 +0800

    scripts: check GitHub's mirror, not canonical mirror
    CI in forks/MRs got broken when we introduced GitLab's
    snap generation [1] because it assumed that env var
    SNAPCRAFT_LOGIN_FILE should be there (as it is, in
    our canonical repo, but it doesn't need to be in forks,
    so CI pipelines of MRs kept failing). I tried to fix
    this here[2] and here[3] but it wasn't enough, because
    a branch in someone's GitLab fork may not be in the
    canonical GitHub mirror of geewallet.
    
    So this time we check local remotes to find the GitHub
    mirror of the fork we're running on, not the canonical
    mirror. This will force MR submitters to setup a GitHub
    mirror from their GitLab fork but it's a good thing, to
    make sure they don't break macOS & windows builds (as
    we don't have those in GitLabCI).
    
    [1] 8b87b15bd7f69860fff2c0b77ea2971f4b3f90cf
    [2] ef9c07b64389f48c5fef5c50a29f57bfe01c4c88
    [3] b9a499a64ad4b19962b8366def0c18d32d110127

--- scripts/snap_release.fsx
+++ scripts/snap_release.fsx
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Net
 open System.Linq
 open System.Diagnostics
 
@@ -34,6 +35,39 @@ type GitProvider =
     | GitHub
     | GitLab
 
+let remotes = Git.CheckRemotes()
+let orgOrUsername, repoName =
+    match remotes.TryFind "origin" with
+    | None -> failwith "Expecting 'origin' remote to be present"
+    | Some remoteUrl ->
+        if remoteUrl.StartsWith "git@" then
+            failwithf "Expecting an https:// based repo URL but got %s" remoteUrl
+        Console.WriteLine remoteUrl
+        // example: https://gitlab-ci-token:[MASKED]@gitlab.com/nblockchain/geewallet.git
+        let uri = Uri remoteUrl
+        let repoName = uri.Segments.Last()
+        let repoName =
+            if repoName.EndsWith ".git" then
+                repoName.Substring(0, repoName.Length - ".git".Length)
+            else
+                repoName
+        Console.WriteLine repoName
+        let orgOrUsername = (uri.Segments.SkipLast 1).Last().TrimEnd '/'
+        Console.WriteLine orgOrUsername
+        orgOrUsername, repoName
+
+let webClient = new WebClient()
+let mirror = sprintf "https://github.com/%s/%s" orgOrUsername repoName
+try
+    webClient.DownloadString mirror
+    |> ignore
+with
+| _ ->
+    Console.Error.WriteLine(
+        sprintf "Some problem while retrieving '%s', did you set up a mirror properly from GitLab to GitHub?"
+            mirror
+    )
+
 let snapFiles = FsxHelper.RootDir.EnumerateFiles().Where(fun file -> file.Name.EndsWith ".snap")
 if not (snapFiles.Any()) then
     Console.Error.WriteLine "No snap package found."
@@ -55,6 +89,23 @@ let gitProvider =
     else
         failwith onlyCiMsg
 
+let snapcraftLoginFileName = Path.Combine(FsxHelper.RootDir.FullName, "snapcraft.login")
+if File.Exists snapcraftLoginFileName then
+    Console.WriteLine "snapcraft.login file found, skipping log-in"
+else
+    match gitProvider with
+    | GitHub ->
+        let snapcraftLoginEnvVarValue = Environment.GetEnvironmentVariable "SNAPCRAFT_LOGIN"
+        if String.IsNullOrEmpty snapcraftLoginEnvVarValue then
+            failwith "SNAPCRAFT_LOGIN-prefixed env var not found; note: manual logging for release has been disabled, only automated CI jobs can upload now"
+        File.WriteAllText(snapcraftLoginFileName, snapcraftLoginEnvVarValue)
+    | _ ->
+        if orgOrUsername = "nblockchain" && repoName = "geewallet" then
+            failwith "In the case of GitLab, the 'snapcraft.login' file should already be here, copied by the docker scripts to the docker container"
+        else
+            // this must be a fork, do nothing
+            ()
+
 Console.WriteLine "Checking if this is a tag commit..."
 
 let gitTag =
@@ -72,7 +123,7 @@ let gitTag =
         if String.IsNullOrEmpty currentBranch then
             failwith "CI_COMMIT_REF_NAME should be available when GitLab: https://docs.gitlab.com/ee/ci/variables/predefined_variables.html"
 
-        GithubActions.MakeSureGithubCIPassed commitHash currentBranch
+        GithubActions.MakeSureGithubCIPassed orgOrUsername repoName commitHash currentBranch
 
         let ciTag = Environment.GetEnvironmentVariable "CI_COMMIT_TAG"
 
@@ -85,19 +136,6 @@ if not (snapFile.FullName.Contains gitTag) then
     failwithf "Git tag (%s) doesn't match version in snap package file name (%s)"
         gitTag
         snapFile.FullName
-
-let snapcraftLoginFileName = Path.Combine(FsxHelper.RootDir.FullName, "snapcraft.login")
-if File.Exists snapcraftLoginFileName then
-    Console.WriteLine "snapcraft.login file found, skipping log-in"
-else
-    match gitProvider with
-    | GitHub ->
-        let snapcraftLoginEnvVarValue = Environment.GetEnvironmentVariable "SNAPCRAFT_LOGIN"
-        if String.IsNullOrEmpty snapcraftLoginEnvVarValue then
-            failwith "SNAPCRAFT_LOGIN-prefixed env var not found; note: manual logging for release has been disabled, only automated CI jobs can upload now"
-        File.WriteAllText(snapcraftLoginFileName, snapcraftLoginEnvVarValue)
-    | _ ->
-        failwith "In the case of GitLab, the 'snapcraft.login' file should already be here, copied by the docker scripts to the docker container"
 
 Console.WriteLine (sprintf "About to start upload of release %s" gitTag)
 
