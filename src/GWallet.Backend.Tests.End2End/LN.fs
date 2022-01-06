c67259bf2487091ce7e375a093d3bbbaef435755
Afshin Arani <afshin@arani.dev>
2022-01-06 14:56:39 +0330

    Backend(LN),Frontend.Console,End2End: fix CheckClosingFinished
    CheckClosingFinished should not consider mempool txs; but it was,
    because the electrum-server API that it was calling under the hood
    (blockchain.scripthash.listunspent) was considering them. Now if Some
    _closingTxHeight happens (instead of None) it means it is confirmed.

--- src/GWallet.Backend.Tests.End2End/LN.fs
+++ src/GWallet.Backend.Tests.End2End/LN.fs
@@ -160,7 +160,7 @@ type LN() =
                 if attempt = 10 then
                     return Error "Closing tx not confirmed after maximum attempts"
                 else
-                    let! txIsConfirmed = Lightning.Network.CheckClosingFinished (clientWallet.ChannelStore.ChannelInfo channelId)
+                    let! txIsConfirmed = Lightning.Network.CheckClosingFinished clientWallet.ChannelStore channelId
                     if txIsConfirmed then
                         return Ok ()
                     else
@@ -511,7 +511,7 @@ type LN() =
             if attempt = 10 then
                 return Error "Closing tx not confirmed after maximum attempts"
             else
-                let! txIsConfirmed = Lightning.Network.CheckClosingFinished (clientWallet.ChannelStore.ChannelInfo channelId)
+                let! txIsConfirmed = Lightning.Network.CheckClosingFinished clientWallet.ChannelStore channelId
                 if txIsConfirmed then
                     return Ok ()
                 else
