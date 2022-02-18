60e328104a716b9ce07119e7e92f04472dfb41fa
Andres G. Aragoneses <knocte@gmail.com>
2022-02-18 20:10:04 +0800

    simp balpage moar

--- src/GWallet.Frontend.XF/BalancesPage.xaml.fs
+++ src/GWallet.Frontend.XF/BalancesPage.xaml.fs
@@ -2,28 +2,12 @@
 
 open System
 open System.Linq
-open System.Threading
 
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 
 open GWallet.Frontend.XF.Controls
 open GWallet.Backend
-open GWallet.Backend.FSharpUtil.UwpHacks
-
-
-// this type allows us to represent the idea that if we have, for example, 3 LTC and an unknown number of ETC (might
-// be because all ETC servers are unresponsive), then it means we have AT LEAST 3LTC; as opposed to when we know for
-// sure all balances of all currencies because all servers are responsive
-type TotalBalance =
-    | ExactBalance of decimal
-    | AtLeastBalance of decimal
-    static member (+) (x: TotalBalance, y: decimal) =
-        match x with
-        | ExactBalance exactBalance -> ExactBalance (exactBalance + y)
-        | AtLeastBalance exactBalance -> AtLeastBalance (exactBalance + y)
-    static member (+) (x: decimal, y: TotalBalance) =
-        y + x
 
 type BalancesPage(normalBalanceStates: seq<BalanceState>)
                       as this =
@@ -31,9 +15,6 @@ type BalancesPage(normalBalanceStates: seq<BalanceState>)
 
     let _ = base.LoadFromXaml(typeof<BalancesPage>)
 
-    let normalAccountsBalanceSets = normalBalanceStates.Select(fun balState -> balState.BalanceSet)
-    let mainLayout = base.FindByName<StackLayout>("mainLayout")
-    let totalFiatAmountLabel = mainLayout.FindByName<Label> "totalFiatAmountLabel"
     let contentLayout = base.FindByName<StackLayout> "contentLayout"
     let normalChartView = base.FindByName<CircleChartView> "normalChartView"
 
@@ -78,29 +59,8 @@ type BalancesPage(normalBalanceStates: seq<BalanceState>)
             )
         chartView.SegmentsSource <- chartSourceList
 
-    let mutable lastRefreshBalancesStamp = DateTime.UtcNow,new CancellationTokenSource()
-
-    // default value of the below field is 'false', just in case there's an incoming payment which we don't want to miss
-    let mutable noImminentIncomingPayment = false
-
-    let mutable balanceRefreshCancelSources = Seq.empty
-
-    let lockObject = Object()
-
     do
         this.Init()
-
-    member private this.LastRefreshBalancesStamp
-        with get() = lock lockObject (fun _ -> lastRefreshBalancesStamp)
-        and set value = lock lockObject (fun _ -> lastRefreshBalancesStamp <- value)
-        
-    member private this.NoImminentIncomingPayment
-        with get() = lock lockObject (fun _ -> noImminentIncomingPayment)
-         and set value = lock lockObject (fun _ -> noImminentIncomingPayment <- value)
-
-    member private this.BalanceRefreshCancelSources
-        with get() = lock lockObject (fun _ -> balanceRefreshCancelSources |> List.ofSeq :> seq<_>)
-         and set value = lock lockObject (fun _ -> balanceRefreshCancelSources <- value)
 
     member this.PopulateBalances (readOnly: bool) (balances: seq<BalanceState>) =
         let activeCurrencyClassId,inactiveCurrencyClassId =
@@ -142,127 +102,14 @@ type BalancesPage(normalBalanceStates: seq<BalanceState>)
 
         contentLayout.BatchCommit()
 
-    member private this.UpdateGlobalBalance (balancesJob: Async<array<BalanceState>>)
-                                            fiatLabel
-                                            (readOnly: bool)
-                                                : Async<Option<bool>> =
-        async {
-            let _,cancelSource = this.LastRefreshBalancesStamp
-            if cancelSource.IsCancellationRequested then
-
-                // as in: we can't(NONE) know the answer to this because we're going to sleep
-                return None
-
-            else
-                let! resolvedBalances = balancesJob
-                let fiatBalances = resolvedBalances.Select(fun balanceState ->
-                                                                     balanceState.FiatAmount)
-                                   |> List.ofSeq
-                Device.BeginInvokeOnMainThread(fun _ ->
-                    RedrawCircleView resolvedBalances
-                )
-                return resolvedBalances.Any(fun balanceState ->
-
-                    // ".IsNone" means: we don't know if there's an incoming payment (absence of info)
-                    // so the whole `".IsNone" || "yes"` means: maybe there's an imminent incoming payment?
-                    // as in "it's false that we know for sure that there's no incoming payment"
-                    balanceState.ImminentIncomingPayment.IsNone ||
-                        Option.exists id balanceState.ImminentIncomingPayment
-
-                // we can(SOME) answer: either there's no incoming payment (false) or that maybe there is (true)
-                ) |> Some
-        }
-
-    member private this.StopTimer() =
-        let _,cancelSource = this.LastRefreshBalancesStamp
-        if not cancelSource.IsCancellationRequested then
-            cancelSource.Cancel()
-            cancelSource.Dispose()
-
-    member private this.ConfigureFiatAmountFrame (readOnly: bool): TapGestureRecognizer =
-        let totalCurrentFiatAmountFrameName,totalOtherFiatAmountFrameName =
-            if readOnly then
-                "totalReadOnlyFiatAmountFrame","totalFiatAmountFrame"
-            else
-                "totalFiatAmountFrame","totalReadOnlyFiatAmountFrame"
-
-        let currentChartViewName,otherChartViewName =
-            if readOnly then
-                "readonlyChartView","normalChartView"
-            else
-                "normalChartView","readonlyChartView"
-
-        let switchingToReadOnly = not readOnly
-
-        let totalCurrentFiatAmountFrame,totalOtherFiatAmountFrame =
-            mainLayout.FindByName<Frame> totalCurrentFiatAmountFrameName,
-            mainLayout.FindByName<Frame> totalOtherFiatAmountFrameName
-
-        let currentChartView,otherChartView =
-            mainLayout.FindByName<CircleChartView> currentChartViewName,
-            mainLayout.FindByName<CircleChartView> otherChartViewName
-
-        let tapGestureRecognizer = TapGestureRecognizer()
-        tapGestureRecognizer.Tapped.Add(fun _ ->
-
-            let shouldNotOpenNewPage =
-                true
-
-            if shouldNotOpenNewPage then
-                Device.BeginInvokeOnMainThread(fun _ ->
-                    totalCurrentFiatAmountFrame.IsVisible <- false
-                    currentChartView.IsVisible <- false
-                    totalOtherFiatAmountFrame.IsVisible <- true
-                    otherChartView.IsVisible <- true
-                )
-                let balancesStatesToPopulate =
-                    normalBalanceStates
-                this.AssignColorLabels switchingToReadOnly
-                this.PopulateBalances switchingToReadOnly balancesStatesToPopulate
-                RedrawCircleView balancesStatesToPopulate
-            else
-                ()
-
-        )
-        totalCurrentFiatAmountFrame.GestureRecognizers.Add tapGestureRecognizer
-        tapGestureRecognizer
-
     member this.PopulateGridInitially () =
-
-        let tapper = this.ConfigureFiatAmountFrame false
-        this.ConfigureFiatAmountFrame true |> ignore
-
         this.PopulateBalances false normalBalanceStates
         RedrawCircleView normalBalanceStates
-
-    member private this.AssignColorLabels (readOnly: bool) =
-        let labels,color =
-                let color = Color.DarkRed
-                totalFiatAmountLabel.TextColor <- color
-                normalAccountsBalanceSets,color
-
-        for balanceSet in labels do
-            balanceSet.Widgets.CryptoLabel.TextColor <- color
-            balanceSet.Widgets.FiatLabel.TextColor <- color
 
     member private this.Init () =
         normalChartView.DefaultImageSource <- FrontendHelpers.GetSizedImageSource "logo" 512
 
-        let tapGestureRecognizer = TapGestureRecognizer()
-        tapGestureRecognizer.Tapped.Subscribe(fun _ ->
-            Uri "http://www.geewallet.com"
-                |> Xamarin.Essentials.Launcher.OpenAsync
-                |> FrontendHelpers.DoubleCheckCompletionNonGeneric
-        ) |> ignore
-        let footerLabel = mainLayout.FindByName<Label> "footerLabel"
-        footerLabel.GestureRecognizers.Add tapGestureRecognizer
-
-        let allNormalAccountFiatBalances =
-            normalBalanceStates.Select(fun balanceState -> balanceState.FiatAmount) |> List.ofSeq
-
         Device.BeginInvokeOnMainThread(fun _ ->
-            this.AssignColorLabels true
-
             this.PopulateGridInitially ()
         )
 
