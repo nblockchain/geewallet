8f019313d49423403b9f8e7441ed8d10d39252c5
Andres G. Aragoneses <knocte@gmail.com>
2022-02-19 11:20:12 +0800

    remove most of frontendhelpers

--- src/GWallet.Frontend.XF/FrontendHelpers.fs
+++ src/GWallet.Frontend.XF/FrontendHelpers.fs
@@ -7,10 +7,7 @@ open Xamarin.Forms
 
 module FrontendHelpers =
 
-    // these days cryptos are not so volatile, so 30mins should be good...
-    let internal TimeSpanToConsiderExchangeRateOutdated = TimeSpan.FromMinutes 30.0
-
-    let private MaybeCrash (canBeCanceled: bool) (ex: Exception) =
+    let private MaybeCrash (ex: Exception) =
         let LastResortBail() =
             Device.PlatformServices.QuitApplication()
 
@@ -26,25 +23,12 @@ module FrontendHelpers =
 
     // when running Task<unit> or Task<T> where we want to ignore the T, we should still make sure there is no exception,
     // & if there is, bring it to the main thread to fail fast, report to Sentry, etc, otherwise it gets ignored
-    let DoubleCheckCompletion<'T> (task: Task<'T>) =
-        task.ContinueWith(fun (t: Task<'T>) ->
-            MaybeCrash false t.Exception
-        , TaskContinuationOptions.OnlyOnFaulted) |> ignore
     let DoubleCheckCompletionNonGeneric (task: Task) =
-        task.ContinueWith(fun (t: Task) ->
-            MaybeCrash false t.Exception
-        , TaskContinuationOptions.OnlyOnFaulted) |> ignore
-
-    let DoubleCheckCompletionAsync<'T> (canBeCanceled: bool) (work: Async<'T>): unit =
-        async {
-            try
-                let! _ = work
-                ()
-            with
-            | ex ->
-                MaybeCrash canBeCanceled ex
-            return ()
-        } |> Async.Start
+        task.ContinueWith(
+            (fun (t: Task) -> MaybeCrash t.Exception),
+            TaskContinuationOptions.OnlyOnFaulted
+        )
+        |> ignore
 
     let SwitchToNewPage (currentPage: Page) (createNewPage: unit -> Page) (navBar: bool): unit =
         Device.BeginInvokeOnMainThread(fun _ ->
@@ -56,37 +40,3 @@ module FrontendHelpers =
             currentPage.Navigation.PushAsync navPage
                 |> DoubleCheckCompletionNonGeneric
         )
-
-    let SwitchToNewPageDiscardingCurrentOne (currentPage: Page) (createNewPage: unit -> Page): unit =
-        Device.BeginInvokeOnMainThread(fun _ ->
-            let newPage = createNewPage ()
-            NavigationPage.SetHasNavigationBar(newPage, false)
-
-            currentPage.Navigation.InsertPageBefore(newPage, currentPage)
-            currentPage.Navigation.PopAsync()
-                |> DoubleCheckCompletion
-        )
-
-    let SwitchToNewPageDiscardingCurrentOneAsync (currentPage: Page) (createNewPage: unit -> Page) =
-        async {
-            let newPage = createNewPage ()
-            NavigationPage.SetHasNavigationBar(newPage, false)
-
-            currentPage.Navigation.InsertPageBefore(newPage, currentPage)
-            let! _ =
-                currentPage.Navigation.PopAsync()
-                |> Async.AwaitTask
-            return ()
-        }
-
-    let ChangeTextAndChangeBack (button: Button) (newText: string) =
-        let initialText = button.Text
-        button.IsEnabled <- false
-        button.Text <- newText
-        Task.Run(fun _ ->
-            Task.Delay(TimeSpan.FromSeconds(2.0)).Wait()
-            Device.BeginInvokeOnMainThread(fun _ ->
-                button.Text <- initialText
-                button.IsEnabled <- true
-            )
-        ) |> DoubleCheckCompletionNonGeneric
