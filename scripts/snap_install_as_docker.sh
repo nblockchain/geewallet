2bbd90efaa4e6c389f463eef417ee10f55d3d2b3
Andres G. Aragoneses <knocte@gmail.com>
2022-02-21 02:29:06 +0800

    remove unconfined apparamor flag?

--- scripts/snap_install_as_docker.sh
+++ scripts/snap_install_as_docker.sh
@@ -111,7 +111,6 @@ $SUDO docker run \
     --tmpfs /tmp \
     --cap-add SYS_ADMIN \
     --device=/dev/fuse \
-    --security-opt apparmor:unconfined \
     --security-opt seccomp:unconfined \
     -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
     -v /lib/modules:/lib/modules:ro \
