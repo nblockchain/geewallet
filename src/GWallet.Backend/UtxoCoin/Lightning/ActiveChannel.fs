308c0c2c6210df4ecc9efd7e2ff1167104643f76
Afshin Arani <afshinarani79@gmail.com>
2022-10-16 11:25:04 +0800

    Backend(LN): add watch tower client
    The main nuget dep added was StreamJsonRpc, which brings a lot
    of sub-dependencies (even many that seem Windows-only, but
    apparently not[1]*).
    
    * Nevertheless, might be better to juse use StreamRpc[2] in the
    future.
    
    [1] https://github.com/microsoft/vs-streamjsonrpc/issues/639
    [2] https://www.nuget.org/packages/StreamRpc

--- src/GWallet.Backend/UtxoCoin/Lightning/ActiveChannel.fs
+++ src/GWallet.Backend/UtxoCoin/Lightning/ActiveChannel.fs
@@ -11,6 +11,7 @@ open ResultUtils.Portability
 open GWallet.Backend
 open GWallet.Backend.FSharpUtil
 open GWallet.Backend.FSharpUtil.UwpHacks
+open GWallet.Backend.UtxoCoin.Lightning.Watcher
 
 type internal LockFundingError =
     | FundingNotConfirmed of FundedChannel * BlockHeightOffset32
@@ -480,6 +481,15 @@ and internal ActiveChannel =
                                                          network
                                                          account
                     breachStore.SaveBreachData breachData
+
+                    do!
+                        TowerClient.Default.CreateAndSendPunishmentTx
+                            perCommitmentSecret
+                            commitments
+                            channelPrivKeys
+                            network
+                            account
+                            true
 
                     connectedChannelAfterRevokeAndAck.SaveToWallet()
                     let activeChannel = { ConnectedChannel = connectedChannelAfterRevokeAndAck }
