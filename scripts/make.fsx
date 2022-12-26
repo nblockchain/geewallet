4209180dc01813d0bdaf69dbe18c2cc0161b1489
Parham Saremi <parhaamsaremi@gmail.com>
2022-12-26 14:33:52 +0330

    scripts/make.fsx: build android sol for macOS
    In the previous commit we built the android solution for macOS
    job in the CI. For simplicity, in this commit we build the
    solution using make command by modifying make.fsx command.

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -31,6 +31,7 @@ let GTK_FRONTEND = "GWallet.Frontend.XF.Gtk"
 let DEFAULT_SOLUTION_FILE = "gwallet.core.sln"
 let LINUX_SOLUTION_FILE = "gwallet.linux.sln"
 let MAC_SOLUTION_FILE = "gwallet.mac.sln"
+let ANDROID_SOLUTION_FILE = "gwallet.android.sln"
 let BACKEND = "GWallet.Backend"
 
 type Frontend =
@@ -189,6 +190,25 @@ let BuildSolution
         Environment.Exit 1
     | _ -> ()
 
+let DotNetBuildSolution 
+    (solutionFileName: string)
+    (binaryConfig: BinaryConfig)
+    (ignoreError: bool)
+    =
+    let configOption = sprintf "-c %s" (binaryConfig.ToString())
+    let buildArgs = (sprintf "build %s %s" configOption solutionFileName)
+    let buildProcess = Process.Execute ({ Command = "dotnet"; Arguments = buildArgs }, Echo.All)
+    match buildProcess.Result with
+    | Error _ ->
+        if not ignoreError then
+            Console.WriteLine()
+            Console.Error.WriteLine "dotnet build failed"
+            PrintNugetVersion() |> ignore
+            Environment.Exit 1
+        else
+            ()
+    | _ -> ()
+
 let JustBuild binaryConfig maybeConstant: Frontend*FileInfo =
     let buildTool = Map.tryFind "BuildTool" buildConfigContents
     if buildTool.IsNone then
@@ -226,6 +246,9 @@ let JustBuild binaryConfig maybeConstant: Frontend*FileInfo =
 
                 //this is because building in release requires code signing keys
                 if binaryConfig = BinaryConfig.Debug then
+                    DotNetBuildSolution ANDROID_SOLUTION_FILE binaryConfig true
+                    DotNetBuildSolution ANDROID_SOLUTION_FILE binaryConfig false
+
                     let solution = MAC_SOLUTION_FILE
 
                     ExplicitRestore solution
