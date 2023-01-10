f1975ca632b5d5168b3efd65bb246b8b1c42f5ee
Andres G. Aragoneses <knocte@gmail.com>
2023-01-10 20:52:18 +0800

    WIP1

--- scripts/snap_build.sh
+++ scripts/snap_build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+sudo apt purge --yes dotnet6
+
 # this is the equivalent of using the 'build-packages' (not stage-packages) section in snapcraft
 # but as we're not using the 'make' plugin, we need to this manually now
 DEBIAN_FRONTEND=noninteractive sudo apt install -y fsharp build-essential pkg-config cli-common-dev mono-devel
