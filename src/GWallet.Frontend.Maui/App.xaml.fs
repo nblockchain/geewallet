5eb9cb85a76d3ef53b09ad22aad5ce3895eecebd
Parham Saremi <parhaamsaremi@gmail.com>
2023-01-15 07:36:10 +0330

    Add Maui base project
    Used the template in [1] for Maui.
    
    Install JDK 11 to fix error "Java SDK 11.0 or above is required
    when using .NET 6 or higher" when building MAUI Android
    frontend [2].
    
    Disable checking of out-of-support workloads in
    Frontend.Maui.fsproj so that project can be built because it
    still uses .NET6 workloads.
    
    [1] https://github.com/fabulous-dev/FSharp.Mobile.Templates/tree/stable-net6.0/templates/Maui
    [2] https://stackoverflow.com/a/77519085/544947

---
+++ src/GWallet.Frontend.Maui/App.xaml.fs
@@ -0,0 +1,10 @@
+﻿namespace GWallet.Frontend.Maui
+
+open Microsoft.Maui.Controls
+open Microsoft.Maui.Controls.Xaml
+
+type App() as this =
+    inherit Application()
+
+    do this.LoadFromXaml typeof<App> |> ignore<App>
+    do this.MainPage <- AppShell()
