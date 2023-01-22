122ab99405bd94198e73c6adc8d1ed60fd6ac8ee
Andres G. Aragoneses <knocte@gmail.com>
2023-01-22 20:41:24 +0800

    Revert "Fixup"
    This reverts commit 16c10ac52acafc5e70807c9243d44449b8bff4c6.

--- src/GWallet.Backend/FSharpUtil.fs
+++ src/GWallet.Backend/FSharpUtil.fs
@@ -94,7 +94,11 @@ module FSharpUtil =
 
         // FIXME: remove once we can stop supporting old versions of FSharpCore
         let SleepSpan (span: TimeSpan) =
+#if LEGACY_FRAMEWORK
             Async.Sleep (int span.TotalMilliseconds)
+#else
+            Async.Sleep span
+#endif
 
         let MixedParallel2 (a: Async<'T1>) (b: Async<'T2>): Async<'T1*'T2> =
             async {
