ec482de07a621e594d3e6de33286f311f08cffcd
webwarrior <reg@webwarrior.ws>
2023-03-01 14:07:36 +0100

    Frontend.MAUI: add WelcomePage2
    Add WelcomePage2 .xaml and .xaml.fs files. Enable navigation
    to WelcomePage2 in MAUI version of WelcomePage. Fix navigation
    by referencing Initialization.fs from Frontend.XF and using
    it to set main page like it is done in Frontend.XF project.
    Use Grid instead of StackLayout so that Maui layout is fixed.
    
    Co-authored-by: Parham <parhaamsaremi@gmail.com>

--- src/GWallet.Frontend.XF/WelcomePage2.xaml.fs
+++ src/GWallet.Frontend.XF/WelcomePage2.xaml.fs
@@ -1,20 +1,35 @@
-﻿namespace GWallet.Frontend.XF
+﻿#if !XAMARIN
+namespace GWallet.Frontend.Maui
+#else
+namespace GWallet.Frontend.XF
+#endif
 
 open System
 open System.Threading.Tasks
 
+#if !XAMARIN
+open Microsoft.Maui.Controls
+open Microsoft.Maui.Controls.Xaml
+open Microsoft.Maui.ApplicationModel
+#else
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 open Xamarin.Essentials
+#endif
 
 open GWallet.Backend
 
+#if !XAMARIN
+// state and masterPrivateKeyGenerationTask are unused for now in MAUI until LoadingPage is ported
+type WelcomePage2(_state: FrontendHelpers.IGlobalAppState, _masterPrivateKeyGenerationTask: Task<array<byte>>) =
+#else
 type WelcomePage2(state: FrontendHelpers.IGlobalAppState, masterPrivateKeyGenerationTask: Task<array<byte>>) =
+#endif
     inherit ContentPage()
 
     let _ = base.LoadFromXaml(typeof<WelcomePage2>)
 
-    let mainLayout = base.FindByName<StackLayout> "mainLayout"
+    let mainLayout = base.FindByName<Grid> "mainLayout"
 
     let password = mainLayout.FindByName<Entry> "passwordEntry"
     let passwordConfirmation = mainLayout.FindByName<Entry> "passwordEntryConfirmation"
@@ -53,12 +68,16 @@ type WelcomePage2(state: FrontendHelpers.IGlobalAppState, masterPrivateKeyGenera
             ToggleInputWidgetsEnabledOrDisabled false
 
             async {
+#if XAMARIN                  
                 let! privateKeyBytes = Async.AwaitTask masterPrivateKeyGenerationTask
                 do! Account.CreateAllAccounts privateKeyBytes password.Text
                 let loadingPage () =
                     LoadingPage (state, false)
                         :> Page
                 FrontendHelpers.SwitchToNewPageDiscardingCurrentOne self loadingPage
+#else        
+                ()
+#endif                
             } |> FrontendHelpers.DoubleCheckCompletionAsync false
 
     member __.OnPasswordTextChanged(_sender: Object, _args: EventArgs) =
