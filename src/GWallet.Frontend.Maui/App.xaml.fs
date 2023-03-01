7f63ec200201b2adf314a6e7957f15729703be8e
webwarrior <reg@webwarrior.ws>
2023-03-01 14:07:36 +0100

    Frontend.MAUI: add WelcomePage2
    Add WelcomePage2 .xaml and .xaml.fs files.
    Enable navigation to WelcomePage2 in MAUI version of
    WelcomePage.
    Fix navigation by referencing Initialization.fs from
    Frontend.XF and using it to set main page like it is done in
    Frontend.XF project.
    Use Grid instead of StackLayout so that Maui layout is fixed.
    
    Co-authored-by: Parham <parhaamsaremi@gmail.com>

--- src/GWallet.Frontend.Maui/App.xaml.fs
+++ src/GWallet.Frontend.Maui/App.xaml.fs
@@ -6,10 +6,9 @@ open Microsoft.Maui.Controls.Xaml
 
 type App() as this =
     inherit Application()
-    let GlobalState = GlobalState()
 
     do this.LoadFromXaml(typeof<App>) |> ignore
-    do this.MainPage <- (WelcomePage GlobalState) :> Page
+    do this.MainPage <- Initialization.LandingPage()
 
 #if GTK
     override _.CreateWindow(activationState) = 
--- src/GWallet.Frontend.XF/WelcomePage.xaml.fs
+++ src/GWallet.Frontend.XF/WelcomePage.xaml.fs
@@ -20,13 +20,8 @@ open Xamarin.Essentials
 
 open GWallet.Backend
 open GWallet.Backend.FSharpUtil.UwpHacks
-// state is unused in Maui for now because it's only used to pass it to
-// WelcomePage2() ctor, which is not created in Maui app yet
-#if !XAMARIN
-type WelcomePage(_state: FrontendHelpers.IGlobalAppState) =
-#else
+
 type WelcomePage(state: FrontendHelpers.IGlobalAppState) =
-#endif
     inherit ContentPage()
 
     let _ = base.LoadFromXaml(typeof<WelcomePage>)
@@ -95,9 +90,6 @@ type WelcomePage(state: FrontendHelpers.IGlobalAppState) =
         else
             None
 
-// ToggleInputWidgetsEnabledOrDisabled is not used since its usage is in another 
-// #if block.
-#if XAMARIN
     let ToggleInputWidgetsEnabledOrDisabled (enabled: bool) =
         let newCreateButtonCaption =
             if enabled then
@@ -113,7 +105,6 @@ type WelcomePage(state: FrontendHelpers.IGlobalAppState) =
             nextButton.IsEnabled <- enabled
             nextButton.Text <- newCreateButtonCaption
         )
-#endif
 
     do
         dobDatePicker.MaximumDate <- DateTime.UtcNow.Date
@@ -142,7 +133,7 @@ type WelcomePage(state: FrontendHelpers.IGlobalAppState) =
                     let! mainThreadSynchContext =
                         Async.AwaitTask <| MainThread.GetMainThreadSynchronizationContextAsync()
                     do! Async.SwitchToContext mainThreadSynchContext
-#if XAMARIN
+
                     let dateTime = dobDatePicker.Date
                     ToggleInputWidgetsEnabledOrDisabled false
                     do! Async.SwitchToThreadPool()
@@ -154,7 +145,6 @@ type WelcomePage(state: FrontendHelpers.IGlobalAppState) =
                         WelcomePage2 (state, masterPrivKeyTask)
                             :> Page
                     do! FrontendHelpers.SwitchToNewPageDiscardingCurrentOneAsync this welcomePage
-#endif
                 }
 
         if dobDatePicker.Date.Date = middleDateEighteenYearsAgo.Date then
