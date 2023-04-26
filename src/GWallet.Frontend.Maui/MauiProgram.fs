82725226691c8a996c0fb964b3db1ed0cd56d399
webwarrior <reg@webwarrior.ws>
2023-04-26 13:10:16 +0330

    Frontend.Maui: add ZXing.Net.Maui library
    Add ZXing.Net.Maui library for barcode gerneration/reading.
    It is successor to ZXing.Net.Mobile, which is used in XF
    project.
    
    Use CrossMaui version of ZXing.Net.Maui libraryon Gtk.
    Reference projects directly instead of using nuget packages.

--- src/GWallet.Frontend.Maui/MauiProgram.fs
+++ src/GWallet.Frontend.Maui/MauiProgram.fs
@@ -10,11 +10,15 @@ open Microsoft.Maui.Controls.Compatibility.Hosting
 open Microsoft.Maui.Controls.Hosting
 open Microsoft.Maui.Hosting
 
+open ZXing.Net.Maui.Controls
+
+
 type MauiProgram =
     static member CreateMauiApp() =
         MauiApp
             .CreateBuilder()
             .UseMauiApp<App>()
+            .UseBarcodeReader()
 #if GTK 
             .UseMauiCompatibility()
 #endif
