b72bb8659a96cac3f465ac679a202467eb54fdab
webwarrior <reg@webwarrior.ws>
2024-01-19 14:19:39 +0800

    Frontend.Console: show progress at balance check
    Show progress when checking balances so that we can know which
    currency is the slow one.
    
    Fixes https://github.com/nblockchain/geewallet/issues/245

--- src/GWallet.Frontend.Console/UserInteraction.fs
+++ src/GWallet.Frontend.Console/UserInteraction.fs
@@ -252,6 +252,9 @@ module UserInteraction =
             //       we don't need to query the fiat value at all (micro-optimization?)
             let! balance,usdValue = FSharpUtil.AsyncExtensions.MixedParallel2 balanceJob usdValueJob
 
+            let progressIteration = sprintf " %A" account.Currency
+            Console.Write progressIteration
+
             return (account,balance,usdValue)
         }
 
@@ -264,6 +267,7 @@ module UserInteraction =
     let private GetAccountBalances (accounts: seq<IAccount>)
                                        : Async<array<IAccount*MaybeCached<decimal>*MaybeCached<decimal>>> =
         let accountAndBalancesToBeQueried = accounts |> Seq.map GetAccountBalanceInner
+        Console.Write "Retrieving balances..."
         Async.Parallel accountAndBalancesToBeQueried
 
     let DisplayAccountStatuses(whichAccount: WhichAccount): Async<seq<string>> =
@@ -342,6 +346,9 @@ module UserInteraction =
                 async {
                     let! accountsWithBalances = GetAccountBalances accounts
                     let statuses, currencyTotals = displayAllAndSumBalance accountsWithBalances 0 Map.empty
+
+                    // All balances are fetched and their currency names printed, put a dot at the end of "Retrieving balances... " line.
+                    Console.Write '.'
 
                     let maybeTotalInUsd, totals = displayTotalAndSumFiatBalance currencyTotals
                     return
