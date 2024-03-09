4488d66059ec7dcebb92dfd4b6d354ad5ca61495
Andres G. Aragoneses <knocte@gmail.com>
2024-03-09 10:45:02 +0100

    CI,scripts: DRY around git submodules population

--- scripts/bump.fsx
+++ scripts/bump.fsx
@@ -260,7 +260,7 @@ let RunUpdateServers () =
 
 
 if not replaceScript.Exists then
-    Console.Error.WriteLine "Script replace.fsx not found, 'fsx' submodule not populated? Please run `git submodule foreach git fetch --all && git submodule sync --recursive && git submodule update --init --recursive`"
+    Console.Error.WriteLine "Script replace.fsx not found, 'fsx' submodule not populated? Please run `populate_gitsubmodules.sh`"
     Environment.Exit 1
 
 GitDiff()
