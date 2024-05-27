d50ea6745156125480147a4e507d705db6825ddf
webwarrior <reg@webwarrior.ws>
2024-05-27 11:59:31 +0200

    Frontend.Maui: fix button padding on Gtk
    Set padding for buttons on Gtk to be consistent with other
    platforms as they have padding around buttons by default.

--- src/GWallet.Frontend.Maui/App.xaml.fs
+++ src/GWallet.Frontend.Maui/App.xaml.fs
@@ -8,6 +8,14 @@ type App() as this =
     inherit Application()
 
     do this.LoadFromXaml typeof<App> |> ignore<App>
+#if GTK
+    // Set style here instead of Styles.xml because OnPlatform property doesn't recognize "Gtk" value
+    do
+        // Set padding for buttons to be consistent with other platforms as they have padding around buttons by default.
+        let style = Style(typeof<Button>)
+        style.Setters.Add(Button.PaddingProperty, 4.0)
+        this.Resources.Add(style)
+#endif
     do this.MainPage <- Initialization.LandingPage()
 
 #if GTK
