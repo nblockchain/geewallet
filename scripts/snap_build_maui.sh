6c51e1431109914f5506d2ce959089fed466ec29
webwarrior <reg@webwarrior.ws>
2024-05-29 11:24:32 +0200

    CI,scripts: refactoring of Maui/Gtk builds
    Extracted installation of gtk workload and adding Maui nuget
    source to separate script to remove code duplication.
    
    Renamed DotNetVersion to DotNetVersionForMauiGtk and made it
    and GtkSharpVersion workflow-level env vars in CI.yml.

--- scripts/snap_build_maui.sh
+++ scripts/snap_build_maui.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Install gtk workload
-wget -O gtksharp.net.sdk.gtk.manifest-$DotnetVersion.nupkg https://globalcdn.nuget.org/packages/gtksharp.net.sdk.gtk.manifest-$DotnetVersion.$GtkSharpVersion.nupkg
-DOTNET_DIR=~/.dotnet
-WORKLOAD_MANIFEST_DIR=$DOTNET_DIR/sdk-manifests/$DotnetVersion/gtksharp.net.sdk.gtk
-mkdir -p $WORKLOAD_MANIFEST_DIR/
-unzip -j gtksharp.net.sdk.gtk.manifest-$DotnetVersion.nupkg "data/*" -d $WORKLOAD_MANIFEST_DIR/
-rm gtksharp.net.sdk.gtk.manifest-$DotnetVersion.nupkg
-# otherwise we get System.UnauthorizedAccessException: Access to the path '/home/runner/.dotnet/sdk-manifests/6.0.300/gtksharp.net.sdk.gtk/WorkloadManifest.json' is denied.
-chmod 764 $WORKLOAD_MANIFEST_DIR/*
-dotnet workload search
-dotnet workload install gtk --skip-manifest-update
-
-#Add Maui Nuget source
-cd dependencies/maui
-dotnet nuget add source --name nuget https://api.nuget.org/v3/index.json
-cd ../..
+# Install gtk workload and add Maui nuget source
+./scripts/prepare_maui_gtk.sh
 
 # Build GWallet MAUI Gtk project
 ./configure.sh
