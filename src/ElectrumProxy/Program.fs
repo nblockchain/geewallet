5bddb4d90876b689014af36d283300aaf6613604
webwarrior <reg@webwarrior.ws>
2024-09-10 12:55:28 +0200

    ElectrumProxy: begin implementing
    Start implementing ElectrumProxy server. Implement
    "server.version" and "server.ping" methods.

--- src/ElectrumProxy/Program.fs
+++ src/ElectrumProxy/Program.fs
@@ -1,2 +1,42 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Program
+
+open System.Net.Sockets
+
+open StreamJsonRpc
+
+
+[<EntryPoint>]
+let main (args: string[]) =
+    let port = int args.[0]
+
+    let listener = new TcpListener(System.Net.IPAddress.Any, port)
+    listener.Start();
+
+    async {
+        while true do
+            use! tcpClient = listener.AcceptTcpClientAsync() |> Async.AwaitTask
+            use networkStream = tcpClient.GetStream()
+
+            use formatter = new SystemTextJsonFormatter()
+            use handler = new NewLineDelimitedMessageHandler(networkStream, networkStream, formatter)
+            formatter.JsonSerializerOptions.PropertyNamingPolicy <- Server.PascalCaseToSnakeCaseNamingPolicy()
+
+            use jsonRpc = new JsonRpc(handler)
+            
+#if DEBUG
+            jsonRpc.TraceSource.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(System.Console.OpenStandardError()))
+            |> ignore
+            jsonRpc.TraceSource.Switch.Level <- System.Diagnostics.SourceLevels.All
+#endif            
+
+            Server.AddMethods jsonRpc
+
+            jsonRpc.Disconnected.Add(fun args -> 
+                eprintfn "Disconnected. Reason=%A; Description=%A; Exception=%A" args.Reason args.Description args.Exception)
+
+            jsonRpc.StartListening()
+            do! jsonRpc.Completion |> Async.AwaitTask
+    }
+    |> Async.RunSynchronously
+
+    0
---
+++ src/ElectrumProxy/Server.fs
@@ -0,0 +1,27 @@
+﻿module Server
+
+open System
+open System.Text
+
+open StreamJsonRpc
+
+type PascalCaseToSnakeCaseNamingPolicy() = 
+    inherit Json.JsonNamingPolicy()
+
+    static let capitalizedWordRegex = RegularExpressions.Regex "[A-Z][a-z0-9]*"
+
+    override self.ConvertName name =
+        let evaluator (regexMatch: RegularExpressions.Match) =
+            let lowercase = regexMatch.Value.ToLower()
+            if regexMatch.Index = 0 then lowercase else "_" + lowercase
+        capitalizedWordRegex.Replace(name, Text.RegularExpressions.MatchEvaluator evaluator)
+
+let supportedProtocolVersion = "0.10"
+
+let AddMethods (jsonRpc: JsonRpc) =
+    jsonRpc.AddLocalRpcMethod(
+        "server.version", 
+        new Func<string, string, string>(fun _clientVersion _protocolVersion -> supportedProtocolVersion))
+    jsonRpc.AddLocalRpcMethod(
+        "server.ping", 
+        new Func<unit>(fun () -> ()))
