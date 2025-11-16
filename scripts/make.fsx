2a55b3a7b494a703cefe57510ed5e03e8864f9cb
Andres G. Aragoneses <knocte@gmail.com>
2025-11-16 19:32:22 +0800

    GitHubCI,Frontend.Console: swap .net6 with .net8
    This is to fix this compiler error when building in Release
    mode:
    
    ```
    Determining projects to restore...
    /usr/share/dotnet/sdk/10.0.100/Sdks/Microsoft.NET.Sdk/targets/Microsoft.NET.EolTargetFrameworks.targets(32,5): error NETSDK1138: The target framework 'net6.0' is out of support and will not receive security updates in the future. Please refer to https://aka.ms/dotnet-core-support for more information about the support policy. [/home/runner/work/geewallet/geewallet/src/GWallet.Frontend.Console/GWallet.Frontend.Console.fsproj]
    ```

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -34,6 +34,9 @@ let UNIX_NAME = "gwallet"
 let DEFAULT_FRONTEND = "GWallet.Frontend.Console"
 let BACKEND = "GWallet.Backend"
 
+// format: X.Y (can't be X.Y.Z here
+let DOTNET_VERSION = "8.0"
+
 type BinaryConfig =
     | Debug
     | Release
@@ -270,7 +273,8 @@ let GetPathToFrontendBinariesDir (binaryConfig: BinaryConfig) =
 #if LEGACY_FRAMEWORK
     Path.Combine (FsxHelper.RootDir.FullName, "src", DEFAULT_FRONTEND, "bin", binaryConfig.ToString())
 #else
-    Path.Combine (FsxHelper.RootDir.FullName, "src", DEFAULT_FRONTEND, "bin", binaryConfig.ToString(), "net6.0")
+    Path.Combine (FsxHelper.RootDir.FullName, "src", DEFAULT_FRONTEND, "bin", binaryConfig.ToString(), 
+                  sprintf "net%s" DOTNET_VERSION)
 #endif
 
 let GetPathToBackend () =
