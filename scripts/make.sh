1144e9cda8d61b7af0d8b1a3549091848ebd504c
Andres G. Aragoneses <knocte@gmail.com>
2025-11-25 14:00:53 +0800

    WIP2

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-BUILD_CONFIG="./scripts/build.config"
-if [ ! -f "$BUILD_CONFIG" ]; then
-    echo "ERROR: configure hasn't been run yet, run ./configure.sh first" >&2 && exit 1
-fi
-source "$BUILD_CONFIG"
 FsxRunnerBin=$FsxRunnerBin FsxRunnerArg=$FsxRunnerArg $FsxRunnerBin $FsxRunnerArg ./scripts/make.fsx "$@"
