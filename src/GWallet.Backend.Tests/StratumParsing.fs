ed408c206d69c536d670ea688b485337f87b2208
Andrés G. Aragoneses <knocte@gmail.com>
2026-09-18 18:04:36 +0800

    Backend.Tests: create new test that reproduces bug
    Bug has been reported here:
    https://github.com/nblockchain/geewallet/issues/316

--- src/GWallet.Backend.Tests/StratumParsing.fs
+++ src/GWallet.Backend.Tests/StratumParsing.fs
@@ -51,3 +51,56 @@ type StratumParsing() =
         )
 
         Assert.That(ex.ErrorCode, Is.EqualTo None)
+
+    [<Test>]
+    member __.``hitting the NRE reported by the user``() =
+        let fakeResponse = "{\"jsonrpc\":\"2.0\",\"method\":\"blockchain.relayfee\"" + String.replicate 511 " " + "\n}"
+
+        let listener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.IPv6Loopback, 0)
+        listener.Server.SetSocketOption(System.Net.Sockets.SocketOptionLevel.IPv6,
+                                        System.Net.Sockets.SocketOptionName.IPv6Only, false)
+        listener.Start()
+        let port = uint32 (listener.LocalEndpoint :?> System.Net.IPEndPoint).Port
+
+        let serverAsync = async {
+            let! client = listener.AcceptTcpClientAsync() |> Async.AwaitTask
+            use stream = client.GetStream()
+            use reader = new System.IO.StreamReader(stream)
+            let! _request = reader.ReadLineAsync() |> Async.AwaitTask
+            use writer = new System.IO.StreamWriter(stream)
+            writer.AutoFlush <- true
+            do! writer.WriteLineAsync(fakeResponse) |> Async.AwaitTask
+        }
+        let _serverTask = serverAsync |> Async.StartAsTask
+        let mutable response: Option<BlockchainScriptHashGetBalanceResult> = None
+
+        let ex =
+            try
+                let jsonRpcClient = JsonRpcTcpClient("localhost", port)
+                let stratumClient = StratumClient(jsonRpcClient)
+                async {
+                    let! res = stratumClient.BlockchainScriptHashGetBalance "someaddress"
+                    response <- Some res
+                }
+                |> Async.RunSynchronously
+                |> ignore
+                None
+            with
+            | exn -> Some exn
+        match ex, response with
+        | Some ex, _ ->
+            let jsonFragment = "blockchain.relayfee"
+            Assert.That(
+                ex.Message.Contains jsonFragment,
+                sprintf
+                    "Exception received (of type %s) didn't contain original JSON fragment '%s'"
+                    (ex.GetType().Name)
+                    jsonFragment
+            )
+        | None, None -> Assert.Fail "Impossible to get no response and no exception"
+        | None, Some res ->
+            Assert.Fail (
+                sprintf
+                    "Should have failed, but we got a response with this Id: %s"
+                    (res.Id.ToString())
+            )
