07e60f26485bd5d16992f43b1714c40b1e36f074
Andrés G. Aragoneses <knocte@gmail.com>
2026-09-22 19:58:25 +0800

    Backend.Tests: refactor electrum-related Tests
    * Moved End2End tests to its own file.
    * Deleted wrongly-named integration tests (they were testing
    against real servers which meant they were always flaky, DOA).

---
+++ src/GWallet.Backend.Tests/ElectrumServer.fs
@@ -0,0 +1,34 @@
+﻿namespace GWallet.Backend.Tests
+
+open System
+open System.Linq
+
+open NUnit.Framework
+
+open GWallet.Backend
+open GWallet.Backend.UtxoCoin
+
+[<TestFixture>]
+type ElectrumServer() =
+
+    [<Test>]
+    member __.``filters electrum BTC servers``() =
+        for electrumServer in ElectrumServerSeedList.DefaultBtcList do
+            Assert.That (electrumServer.ServerInfo.ConnectionType.Encrypted, Is.EqualTo false,
+                sprintf "BTC servers list should be filtered against only-TLS compatible servers, but %s was found"
+                        electrumServer.ServerInfo.NetworkPath)
+
+            Assert.That (electrumServer.ServerInfo.NetworkPath, IsString.WhichDoesNotEndWith ".onion",
+                sprintf "BTC servers list should be filtered against onion servers, but %s was found"
+                        electrumServer.ServerInfo.NetworkPath)
+
+    [<Test>]
+    member __.``filters electrum LTC servers``() =
+        for electrumServer in ElectrumServerSeedList.DefaultLtcList do
+            Assert.That (electrumServer.ServerInfo.ConnectionType.Encrypted, Is.EqualTo false,
+                sprintf "BTC servers list should be filtered against only-TLS compatible servers, but %s was found"
+                        electrumServer.ServerInfo.NetworkPath)
+
+            Assert.That (electrumServer.ServerInfo.NetworkPath, IsString.WhichDoesNotEndWith ".onion",
+                sprintf "BTC servers list should be filtered against onion servers, but %s was found"
+                        electrumServer.ServerInfo.NetworkPath)
